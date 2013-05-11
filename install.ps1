7fdc07834dbaa2dd8137969652a3db24383fb325
Luke Sampson <me@lukesampson.com>
2013-05-11 23:04:47 +1000

    dl and unzip scriptcs

--- install.ps1
+++ install.ps1
@@ -5,28 +5,40 @@ $branch = 'dev' # use this git branch of cscript (dev or master)
 $erroractionpreference='stop' # for try-catch to work
 
 # helpers
-function err($msg) { write-host $msg -b darkred -f white; exit 1 }
-function getenv($name) { [environment]::GetEnvironmentVariable($name, 'User') }
-function setenv($name, $val) { [Environment]::SetEnvironmentVariable($name, $val, 'User') }
+function abort($msg) { write-host $msg -b darkred -f white; exit 1 }
+function getenv($name) { [environment]::getEnvironmentVariable($name, 'User') }
+function setenv($name,$val) { [environment]::setEnvironmentVariable($name,$val,'User') }
+function dl($url,$to) { (new-object system.net.webClient).downloadFile($url,$to) }
+function unzip($path,$to) {
+    $shell = (new-object -com shell.application)
+    $zipfiles = $shell.namespace($path).items()
+    $shell.namespace($to).copyHere($zipFiles, 4) # 4 = don't show progress dialog
+}
 
 $bindir = "$env:localappdata\bin"
 if(-not (test-path $bindir)) { echo "creating $bindir"; mkdir $bindir > $null }
 
-$srcdir = "$bindir\scriptcs"
-if(test-path $srcdir) { err "scriptcs is already installed!" }
+$scoopdir = "$env:localappdata\scoop"
+$srcdir = "$scoopdir\scriptcs\$branch"
+if(test-path $srcdir) { abort "scriptcs is already installed!" }
+mkdir $srcdir > $null
 
-# ensure git is useable
-try { gcm git > $null } catch { err "failed: couldn't find git!" }
+# check for, download roslyn
 
-# clone git repo
-& git clone -b $branch https://github.com/scriptcs/scriptcs.git "$srcdir"
+# download scriptcs
+echo "downloading scriptcs $branch source..."
+dl "https://github.com/scriptcs/scriptcs/archive/$branch.zip" "$srcdir\dl.zip"
+echo "extracting source..."
+unzip "$srcdir\dl.zip" "$srcdir"
+rm "$srcdir\dl.zip"
+exit
 
-# build
-pushd $srcdir
-try { & .\build.cmd } finally { popd }
+# check for, download .net
+
+# build scriptcs
 
 # create bin stubs
-$exe = "$srcdir\src\ScriptCs\bin\Debug\scriptcs.exe"
+$exe = "$srcdir\scriptcs-$branch\src\scriptcs\bin\debug\scriptcs.exe"
 echo "& ""$exe"" `$args" >> "$bindir\scriptcs.ps1"
 
 # ensure ~\appdata\local\bin in user's path
@@ -35,4 +47,4 @@ if((getenv 'path') -notmatch [regex]::escape($bindir)) {
   setenv "path" "$env:path;$bindir"
 }
 
-write-host "done! type scriptcs to run" -b darkgreen -f white
+write-host "done! restart your shell and type scriptcs to run" -b darkgreen -f white
