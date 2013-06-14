57d0e97e2a6af92d8963e2255461dc64d8da6126
Luke Sampson <me@lukesampson.com>
2013-06-14 21:37:09 +1000

    at least it says it installs, but the stub isn't quite right

--- lib/init.ps1
+++ lib/init.ps1
@@ -12,25 +12,8 @@ function success($msg) { write-host $msg -b green -f black; }
 function appdir($name, $version) { "$scoopdir\apps\$name\$version" }
 function fname($path) { split-path $path -leaf }
 function ensure($dir) { if(!(test-path $dir)) { mkdir $dir > $null }; resolve-path $dir }
-function stub($path) {
-  $abs_bindir = ensure $bindir
-  $stub = "$abs_bindir\$(fname $path)"
-
-  echo "`$rawargs = `$myInvocation.line -replace `"^`$([regex]::escape(`$myInvocation.invocationName))\s+`", `"`"" >> "$stub"
-  echo "iex `"$path `$rawargs`"" >> "$stub"
-}
 function friendly_path($path) {
   return "$path" -replace ([regex]::escape($home)), "~"
-}
-function ensure_scoop_in_path { 
-  $userpath = env 'path'
-  $abs_bindir = ensure $bindir
-  if($userpath -notmatch [regex]::escape($abs_bindir)) {
-      # be aggressive and install scoop first in the path
-      echo "adding $(friendly_path $absbindir) to your user path"
-      env 'path' "$abs_bindir;$userpath"  # for future sessions
-      $env:path = "$abs_bindir;$env:path" # for this session
-  }
 }
 function installed($name, $version) { return test-path (appdir $name $version) }
 function assert_not_installed($name, $version) {
@@ -40,4 +23,23 @@ function unzip($path,$to) {
     $shell = (new-object -com shell.application)
     $zipfiles = $shell.namespace("$path").items()
     $shell.namespace("$to").copyHere($zipFiles, 4) # 4 = don't show progress dialog
+}
+
+function stub($path) {
+  $abs_bindir = ensure $bindir
+  $stub = "$abs_bindir\$(fname $path)"
+
+  echo "`$rawargs = `$myInvocation.line -replace `"^`$([regex]::escape(`$myInvocation.invocationName))\s+`", `"`"" >> "$stub"
+  echo "iex `"$path `$rawargs`"" >> "$stub"
+}
+
+function ensure_scoop_in_path { 
+  $userpath = env 'path'
+  $abs_bindir = ensure $bindir
+  if($userpath -notmatch [regex]::escape($abs_bindir)) {
+      # be aggressive and install scoop first in the path
+      echo "adding $(friendly_path $abs_bindir) to your user path"
+      env 'path' "$abs_bindir;$userpath"  # for future sessions
+      $env:path = "$abs_bindir;$env:path" # for this session
+  }
 }
