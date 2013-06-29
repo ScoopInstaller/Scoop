83c6b3f4f9dcbab1de68128573bc10f0715c48a4
Luke Sampson <me@lukesampson.com>
2013-06-29 16:23:12 +1000

    'scoop list' shows versions

--- lib/cmd/list.ps1
+++ lib/cmd/list.ps1
@@ -3,13 +3,17 @@
 # Help: Lists all installed apps
 
 . "$(split-path $myinvocation.mycommand.path)\..\core.ps1"
+. (resolve '..\versions.ps1')
+. (resolve '..\manifest.ps1')
 
 $apps = installed_apps
 
 if($apps) {
     echo "Installed apps:
 "
-    $apps
+    $apps | % {
+        "$_ ($(latest_version $_))"
+    }
     ""
 } else { "there aren't any apps installed" }
 
