5ea0568f269427d049e655719f171144eb0c94e4
Luke Sampson <me@lukesampson.com>
2013-07-03 16:56:02 +1000

    scoop uninstall scoop removes the scoop shim

--- bin/uninstall.ps1
+++ bin/uninstall.ps1
@@ -3,6 +3,10 @@
 . (relpath ..\lib\versions.ps1)
 . (relpath ..\lib\manifest.ps1)
 
+warn 'this will uninstall scoop and all the programs that have been installed with scoop!'
+$yn = read-host 'are you sure? (yN)'
+if($yn -notlike 'y*') { exit }
+
 # run uninstallers if necessary
 installed_apps | % {
     $app = $_
