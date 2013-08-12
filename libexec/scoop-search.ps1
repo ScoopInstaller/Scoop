82f89488693eec781090dd2eddab053634f49a14
Luke Sampson <me@lukesampson.com>
2013-08-12 15:02:53 +1000

    make scoop search match bin files as well as app names

--- libexec/scoop-search.ps1
+++ libexec/scoop-search.ps1
@@ -10,14 +10,26 @@ param($query)
 . (relpath '..\lib\manifest.ps1')
 . (relpath '..\lib\versions.ps1')
 
-function search_bucket($bucket) {
+function bin_match($manifest, $query) {
+    if(!$manifest.bin) { return $false }
+    @($manifest.bin) | % {
+        $fname = strip_ext (split-path $_ -leaf)
+        if($fname -match $query) { return $true }
+    }
+    $false
+}
+
+function search_bucket($bucket, $query) {
     $apps = apps_in_bucket (bucketdir $bucket)
-    if($query) { $apps = $apps | ? { $_ -match $query } }
+    if($query) { $apps = $apps | ? {
+        ($_ -match $query) -or (bin_match (manifest $_) $query)
+
+    } }
     $apps | % { "  $_ ($(latest_version $_ $bucket))"}
 }
 
 @($null) + @(buckets) | % { # $null is main bucket
-    $res = search_bucket($_)
+    $res = search_bucket $_ $query
     if($res) {
         $name = "$_"
         if(!$_) { $name = "main" }
