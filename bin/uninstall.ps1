970ecc75a0a1948c5a57e4cbaaf0e4a6100c23d9
Luke Sampson <me@lukesampson.com>
2013-08-15 14:49:33 +1000

    better uninstall?

--- bin/uninstall.ps1
+++ bin/uninstall.ps1
@@ -9,25 +9,40 @@ if($yn -notlike 'y*') { exit }
 
 # run uninstallers if necessary
 installed_apps | % {
-	$app = $_
-	$version = current_version $app
-	$dir = versiondir $app $version
-	$manifest = installed_manifest $app $version
-	$install = install_info $app $version
-	$architecture = $install.architecture
+    $app = $_
+    $version = current_version $app
+    $dir = versiondir $app $version
+    $manifest = installed_manifest $app $version
+    $install = install_info $app $version
+    $architecture = $install.architecture
 
-	echo "uninstalling $app"
-	run_uninstaller $manifest $architecture $dir
-	rm_env_path $manifest $dir
-	rm_env $manifest
+    echo "uninstalling $app"
+    run_uninstaller $manifest $architecture $dir
+    rm_env_path $manifest $dir
+    rm_env $manifest
 }
 
-if(test-path $scoopdir) {
-	try {
-		rm -r -force $scoopdir -ea stop
-	} catch {
-		abort "couldn't remove $(friendly_path $scoopdir): $_"
-	}
+# try deleting app directories one-by-one except for scoop, in case uninstall fails
+# and we need to run `scoop uninstall scoop` again
+$errors = $false
+gci $appdir | ? name -ne 'scoop' | % {
+    $dir = $_
+    try {
+        rm -r -force $dir -ea stop
+    } catch {
+        $errors = $true
+        warn "couldn't remove $(friendly_path $dir): $_.exception"
+    }
+}
+
+if($errors) {
+    abort "not all apps could be deleted. try again or restart"
+}
+
+try {
+    rm -r -force $scoopdir -ea stop
+} catch {
+    abort "couldn't remove $(friendly_path $scoopdir): $_"
 }
 
 remove_from_path $shimdir
