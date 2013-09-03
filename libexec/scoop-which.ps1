9fcd4215d168030eb88cb5a765a1c58bb70d1c1e
Luke Sampson <me@lukesampson.com>
2013-09-03 11:10:22 +1000

    make 'scoop which' work with global shims

--- libexec/scoop-which.ps1
+++ libexec/scoop-which.ps1
@@ -3,17 +3,18 @@
 # Help: Finds the path to a program that was installed with Scoop
 param($command)
 . "$psscriptroot\..\lib\core.ps1"
-. (relpath '..\lib\help.ps1')
+. "$psscriptroot\..\lib\help.ps1"
 
 if(!$command) { 'ERROR: <command> missing'; my_usage; exit 1 }
 
 try { $gcm = gcm $command -ea stop } catch { }
 if(!$gcm) { [console]::error.writeline("'$command' not found"); exit 3 }
 
-$path = $gcm.path
-$abs_shimdir = "$(resolve-path $(shimdir $false))"
+$path = "$($gcm.path)"
+$usershims = "$(resolve-path $(shimdir $false))"
+$globalshims = "$(resolve-path $(shimdir $true))"
 
-if("$path" -like "$abs_shimdir*") {
+if($path -like "$usershims*" -or $path -like "$globalshims*") {
 	$shimtext = gc $path
 	$shimtext | sls '(?m)^\$path = ''([^'']+)''' | % { $_.matches[0].groups[1].value }
 } else {
