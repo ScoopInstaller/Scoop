640716b2caa99f302ebc02bdeaf7e848f93ed49d
Stéphane Este-Gracias <sestegra@gmail.com>
2014-12-20 15:43:24 +0100

    Add global option to reset command

--- libexec/scoop-reset.ps1
+++ libexec/scoop-reset.ps1
@@ -3,24 +3,32 @@
 # Help: Used to resolve conflicts in favor of a particular app. For example,
 # if you've installed 'python' and 'python27', you can use 'scoop reset' to switch between
 # using one or the other.
-param($app)
-
+#
+# Options:
+#   --global, -g  update a globally installed app
 . "$psscriptroot\..\lib\core.ps1"
 . "$psscriptroot\..\lib\manifest.ps1"
 . "$psscriptroot\..\lib\help.ps1"
 . "$psscriptroot\..\lib\install.ps1"
 . "$psscriptroot\..\lib\versions.ps1"
 
+$opt, $app, $err = getopt $args 'global'
+if($err) { "scoop reset: $err"; exit 1 }
+$global = $opt.g -or $opt.global
+
 if(!$app) { 'ERROR: <app> missing'; my_usage; exit 1 }
+if($global -and !(is_admin)) {
+  'ERROR: you need admin rights to reset global apps'; exit 1
+}
 
-if(!(installed $app)) { abort "$app isn't installed" }
+if(!(installed $app $global)) { abort "$app isn't installed" }
 
-$version = current_version $app
+$version = current_version $app $global
 "resetting $app ($version)"
 
-$dir = resolve-path (versiondir $app $version)
-$manifest = installed_manifest $app $version
+$dir = resolve-path (versiondir $app $version $global)
+$manifest = installed_manifest $app $version $global
 
-create_shims $manifest $dir $false
-env_add_path $manifest $dir
-env_set $manifest $dir
+create_shims $manifest $dir $global
+env_add_path $manifest $dir $global
+env_set $manifest $dir@ $global
