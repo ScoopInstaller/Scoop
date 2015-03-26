7959c1cfe2af792b1565d4ce17b9eeb6e959a092
Luke Sampson <me@lukesampson.com>
2015-03-26 18:33:09 +1000

    Revert "Added support for custom commands"

--- lib/commands.ps1
+++ lib/commands.ps1
@@ -1,7 +1,5 @@
 function command_files {
-	(gci (relpath '..\libexec')) `
-        + (gci "$env:scoop\shims") `
-        | where { $_.name -match 'scoop-.*?\.ps1$' }
+	gci (relpath '..\libexec') | where { $_.name -match 'scoop-.*?\.ps1$' }
 }
 
 function commands {
@@ -12,23 +10,6 @@ function command_name($filename) {
 	$filename.name | sls 'scoop-(.*?)\.ps1$' | % { $_.matches[0].groups[1].value }
 }
 
-function command_path($cmd) {
-    $cmd_path = relpath "..\libexec\scoop-$cmd.ps1"
-
-    # built in commands
-    if (!(Test-Path $cmd_path)) {
-        # get path from shim
-        $shim_path = "$env:scoop\shims\scoop-$cmd.ps1"
-        $line = ((gc $shim_path) | where { $_.startswith('$path') })
-        iex -command "$line"
-        $cmd_path = $path
-    } 
-
-    $cmd_path
-}
-
 function exec($cmd, $arguments) {
-    $cmd_path = command_path $cmd
-
-    & $cmd_path @arguments
+	& (relpath "..\libexec\scoop-$cmd.ps1") @arguments
 }
