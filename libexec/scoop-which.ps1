b103b800457b84d563c5c59f2bf8df95a5c94ac1
Luke Sampson <me@lukesampson.com>
2015-05-11 13:41:53 +1000

    scoop which: handle double-quoted paths in shims (fixes #358)

--- libexec/scoop-which.ps1
+++ libexec/scoop-which.ps1
@@ -16,7 +16,8 @@ $globalshims = fullpath (shimdir $true) # don't resolve: may not exist
 
 if($path -like "$usershims*" -or $path -like "$globalshims*") {
 	$shimtext = gc $path
-	friendly_path ($shimtext | sls '(?m)^\$path = ''([^'']+)''' | % { $_.matches[0].groups[1].value })
+    $exepath = $shimtext | sls '(?m)^\$path = (''|")([^\1]+)\1' | % { $_.matches[0].groups[2].value }
+	friendly_path $exepath
 } else {
 	[console]::error.writeline("not a scoop shim")
 	$path
