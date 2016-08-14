b30afe650df698d86852a1cfac162be2cc4b1455
Luke Sampson <me@lukesampson.com>
2016-08-15 07:10:33 +1000

    don't terminate Powershell session when calling install with invoke-expression (see #980)

--- bin/install.ps1
+++ bin/install.ps1
@@ -13,7 +13,7 @@ iex (new-object net.webclient).downloadstring($core_url)
 if(installed 'scoop') {
     write-host "scoop is already installed. run 'scoop update' to get the latest version." -f red
     # don't abort if invoked with iex——that would close the PS session
-    if($myinvocation.commandorigin -eq 'Internal') { return } else { exit 1 }
+    if($myinvocation.mycommand.commandtype -eq 'Script') { return } else { exit 1 }
 }
 $dir = ensure (versiondir 'scoop' 'current')
 
