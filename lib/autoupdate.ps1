39360036c6d4fd26b603ad87109e1a2cf6c90600
Rémy <git@rrelmy.ch>
2016-11-26 17:11:28 +0100

    First implementation of manifest autoupdate

---
+++ lib/autoupdate.ps1
@@ -0,0 +1,94 @@
+<#
+TODO
+ - add a github release autoupdate type
+ - tests (single arch, without hashes etc.)
+ - clean up
+#>
+
+function substitute([String] $str, [Hashtable] $params) {
+    $params.GetEnumerator() | % {
+        $str = $str.Replace($_.Name, $_.Value);
+    }
+
+    return $str
+}
+
+function check_url([String] $url) {
+    $response = Invoke-WebRequest -Uri $url -Method HEAD
+    if ($response.StatusCode.Equals(200)) { # redirects might be ok
+        return $true
+    }
+
+    return $false
+}
+
+function autoupdate([String] $app, $json, [String] $version)
+{
+    Write-Host -f DarkCyan "Autoupdating $app"
+    $has_changes = $false
+    $has_errors = $false
+
+     $json.architecture | Get-Member -MemberType NoteProperty | % {
+        [Bool]$valid = $true
+        $architecture = $_.Name
+
+        # create new url
+        <#
+        TODO There should be a second option to extract the url from the page
+        #>
+        $template = $json.autoupdate.url.$architecture;
+        $url = substitute $template @{'$version' = $version}
+
+        # check url 
+        if (!(check_url $url)) {
+            $valid = $false
+            Write-Host -f DarkRed "URL $url is not valid"
+        }
+
+        # create hash
+        <#
+        TODO implement more hashing types
+        `extract` Should be able to extract from origin page source (checkver)
+        `download` Last resort, download the real file and hash it
+        #>
+        $hashmode = $json.autoupdate.hash.mode;
+        if ($hashmode -eq "extract") {
+            $hashfile_url = substitute $json.autoupdate.hash.url @{'$version' = $version};
+            $hashfile = (new-object net.webclient).downloadstring($hashfile_url)
+
+            $basename = fname($url)
+            $regex = substitute $json.autoupdate.hash.find @{'$basename' = [regex]::Escape($basename)}
+
+            if ($hashfile -match $regex) {
+                $hash = $matches[1]
+            } else {
+                $valid = $false
+                Write-Error "could no find hash in hashfile"
+            }
+        }
+
+        # write changes to the json object
+        if ($valid) {
+            $has_changes = $true
+            $json.version = $version
+            $json.architecture.$architecture.url = $url
+            $json.architecture.$architecture.hash = $hash
+        } else {
+            $has_errors = $true
+            Write-Host -f DarkRed "Could not update $app $architecture"
+        }
+    }
+
+    if ($has_changes -and !$has_errors) {
+        # write file
+        Write-Host -f DarkGreen "Writing updated $app manifest"
+
+        $path = manifest_path $app
+        <#
+        TODO improve json formatting 
+        #>
+        $json | convertto-json | out-file -filepath $path -encoding utf8
+    } else {
+        Write-Host -f DarkGray "No updates for $app"
+    }
+}
