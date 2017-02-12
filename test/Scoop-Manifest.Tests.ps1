2f40fdde4c81ac0849dcf8506094b689a157e9b9
Richard Kuhnt <r15ch13+git@gmail.com>
2017-02-12 21:37:06 +0100

    Validate manifests against JSON Schema in CI Tests

--- test/Scoop-Manifest.Tests.ps1
+++ test/Scoop-Manifest.Tests.ps1
@@ -3,41 +3,81 @@
 . "$psscriptroot\..\lib\manifest.ps1"
 
 describe "manifest-validation" {
-    $bucketdir = "$psscriptroot\..\bucket\"
-    $manifest_files = gci $bucketdir *.json
+    beforeall {
+        $working_dir = setup_working "manifest"
+        $schema_json = gc "$psscriptroot\..\schema.json" -raw -Encoding UTF8
+        Add-Type -Path "$psscriptroot\..\..\Newtonsoft.Json\lib\net45\Newtonsoft.Json.dll"
+        Add-Type -Path "$psscriptroot\..\..\Newtonsoft.Json.Schema\lib\net45\Newtonsoft.Json.Schema.dll"
+        [System.Collections.Generic.IList[System.String]]$validationErrors = new-object System.Collections.Generic.List[System.String]
+    }
 
-    $manifest_files | % {
-        it "test validity of $_" {
-            $manifest = parse_json $_.fullname
-
-            $url = arch_specific "url" $manifest "32bit"
-            $url | should not match "\$"
-            $url64 = arch_specific "url" $manifest "64bit"
-            $url64 | should not match "\$"
-            if(!$url) {
-                $url = $url64
-            }
-            $url | should not benullorempty
-
-            $extract_dir32 = arch_specific "extract_dir" $manifest "32bit"
-            $extract_dir32 | should not match "\$"
-            $extract_dir64 = arch_specific "extract_dir" $manifest "64bit"
-            $extract_dir64 | should not match "\$"
-
-            $manifest | should not benullorempty
-            $manifest.version | should not benullorempty
+    context "Newtonsoft.Json" {
+        it "Newtonsoft.Json.Linq.JToken is available" {
+            ([System.Management.Automation.PSTypeName]'Newtonsoft.Json.Linq.JToken').Type | should be 'Newtonsoft.Json.Linq.JToken'
+        }
+        it "Newtonsoft.Json.Schema.JSchema is available" {
+            ([System.Management.Automation.PSTypeName]'Newtonsoft.Json.Schema.JSchema').Type | should be 'Newtonsoft.Json.Schema.JSchema'
+        }
+        it "Newtonsoft.Json.Schema.SchemaExtensions is available" {
+            ([System.Management.Automation.PSTypeName]'Newtonsoft.Json.Schema.SchemaExtensions').Type | should be 'Newtonsoft.Json.Schema.SchemaExtensions'
         }
     }
-}
 
-describe "parse_json" {
-    beforeall {
-        $working_dir = setup_working "parse_json"
+    context "parse_json function" {
+        it "fails with invalid json" {
+            { parse_json "$working_dir\broken_wget.json" } | should throw
+        }
     }
 
-    context "json is invalid" {
-        it "fails with invalid json" {
-            { parse_json "$working_dir\wget.json" } | should throw
+    context "schema validation" {
+        it "fails with broken schema" {
+            $json = gc "$working_dir\broken_schema.json" -raw -Encoding UTF8
+            { [Newtonsoft.Json.Schema.JSchema]::Parse($json) } | should throw
+        }
+        it "fails with broken manifest" {
+            $json = gc "$working_dir\broken_wget.json" -raw -Encoding UTF8
+            { [Newtonsoft.Json.Linq.JToken]::Parse($json) } | should throw
+        }
+        it "fails with invalid manifest" {
+            $json = gc "$working_dir\invalid_wget.json" -raw -Encoding UTF8
+            $manifest = [Newtonsoft.Json.Linq.JToken]::Parse($json)
+            $schema = [Newtonsoft.Json.Schema.JSchema]::Parse($schema_json)
+            [Newtonsoft.Json.Schema.SchemaExtensions]::IsValid($manifest, $schema, [ref]$validationErrors)
+            $validationErrors.Count | should be 4
+        }
+    }
+
+    context "manifest validates against the schema" {
+        beforeall {
+            $bucketdir = "$psscriptroot\..\bucket\"
+            $manifest_files = gci $bucketdir *.json
+            $schema = [Newtonsoft.Json.Schema.JSchema]::Parse($schema_json)
+        }
+        beforeeach {
+        }
+        $manifest_files | % {
+            it "$_" {
+                {
+                    $json = gc $_.fullname -raw -Encoding UTF8
+                    $manifest = [Newtonsoft.Json.Linq.JToken]::Parse($json)
+                    [Newtonsoft.Json.Schema.SchemaExtensions]::IsValid($manifest, $schema, [ref]$validationErrors)
+                } | should not throw
+                $validationErrors.Count | should be 0
+
+                $manifest = parse_json $_.fullname
+                $url = arch_specific "url" $manifest "32bit"
+                $url64 = arch_specific "url" $manifest "64bit"
+                if(!$url) {
+                    $url = $url64
+                }
+                $url | should not benullorempty
+            }
+        }
+        aftereach {
+            if ($validationErrors.Count -gt 0) {
+                write-host -f yellow "    [*] $_ " -nonewline
+                write-host -f yellow $([string]::join("`n    [*] $_ ", $validationErrors))
+            }
         }
     }
 }
