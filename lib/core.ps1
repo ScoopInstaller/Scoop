adf0e2cdafb6d16dbbbc1905d379879a96dc5176
Luke Sampson <me@lukesampson.com>
2017-04-21 08:41:43 +1000

    Adjust style, use $env:userprofile for consistency

--- lib/core.ps1
+++ lib/core.ps1
@@ -87,8 +87,7 @@ function fullpath($path) { # should be ~ rooted
 }
 function relpath($path) { "$($myinvocation.psscriptroot)\$path" } # relative to calling script
 function friendly_path($path) {
-    $h = $home.ToString()
-    if(!$h.endswith('\')) { $h += '\' }
+    $h = "$env:userprofile"; if(!$h.endswith('\')) { $h += '\' }
     if($h -eq '\') { return $path }
     return "$path" -replace ([regex]::escape($h)), "~\"
 }
