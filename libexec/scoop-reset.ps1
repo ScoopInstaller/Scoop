7176719501ddb20344e0d0d329b9d80d630386a7
Richard Kuhnt <r15ch13+git@gmail.com>
2018-02-16 14:34:52 +0100

    Improve reset command
    Allow resetting:
    * global apps
    * multiple apps
    * all apps by using * parameter (skips global apps if not admin)

--- libexec/scoop-reset.ps1
+++ libexec/scoop-reset.ps1
@@ -3,49 +3,79 @@
 # Help: Used to resolve conflicts in favor of a particular app. For example,
 # if you've installed 'python' and 'python27', you can use 'scoop reset' to switch between
 # using one or the other.
-param($app)
 
 . "$psscriptroot\..\lib\core.ps1"
 . "$psscriptroot\..\lib\manifest.ps1"
 . "$psscriptroot\..\lib\help.ps1"
+. "$psscriptroot\..\lib\getopt.ps1"
 . "$psscriptroot\..\lib\install.ps1"
 . "$psscriptroot\..\lib\versions.ps1"
 . "$psscriptroot\..\lib\config.ps1"
 . "$psscriptroot\..\lib\shortcuts.ps1"
 
 reset_aliases
+$opt, $apps, $err = getopt $args
+if($err) { "scoop reset: $err"; exit 1 }
 
-if(!$app) { 'ERROR: <app> missing'; my_usage; exit 1 }
+if(!$apps) { error 'ERROR: <app> missing'; my_usage; exit 1 }
 
-$appWithVersion = get_app_with_version $app
-$app            = $appWithVersion.app;
-$version        = $appWithVersion.version;
-
-
-if(!(installed $app)) { abort "'$app' isn't installed" }
-
-if ($version -eq 'latest') {
-    $version = current_version $app
+if($apps -eq '*') {
+    $local = installed_apps $false | % { ,@($_, $false) }
+    $global = installed_apps $true | % { ,@($_, $true) }
+    $apps = @($local) + @($global)
 }
 
-$manifest = installed_manifest $app $version
-# if this is null we know the version they're resetting to
-# is not installed
-if ($manifest -eq $null) {
-    abort "'$app ($version)' isn't installed"
+$apps | ForEach-Object {
+    ($app, $global) = $_
+
+    if(($global -eq $null) -and (installed $app $true)) {
+        # set global flag when running reset command on specific app
+        $global = $true
+    }
+
+    if($app -eq 'scoop') {
+        # skip scoop
+        return
+    }
+
+    $appWithVersion = get_app_with_version $app
+    $app            = $appWithVersion.app;
+    $version        = $appWithVersion.version;
+
+    if(!(installed $app)) {
+        error "'$app' isn't installed"
+        return
+    }
+
+    if ($version -eq 'latest') {
+        $version = current_version $app $global
+    }
+
+    $manifest = installed_manifest $app $version $global
+    # if this is null we know the version they're resetting to
+    # is not installed
+    if ($manifest -eq $null) {
+        error "'$app ($version)' isn't installed"
+        return
+    }
+
+    if($global -and !(is_admin)) {
+        warn "'$app' ($version) is a global app. You need admin rights to reset it. Skipping."
+        return
+    }
+
+    write-host "Resetting $app ($version)."
+
+    $dir = resolve-path (versiondir $app $version $global)
+
+    $install = install_info $app $version $global
+    $architecture = $install.architecture
+
+    $dir = link_current $dir
+    create_shims $manifest $dir $global $architecture
+    create_startmenu_shortcuts $manifest $dir $global $architecture
+    env_add_path $manifest $dir
+    env_set $manifest $dir $global
 }
-
-"Resetting $app ($version)."
-
-$dir = resolve-path (versiondir $app $version)
-
-$install = install_info $app $version
-$architecture = $install.architecture
-
-$dir = link_current $dir
-create_shims $manifest $dir $false $architecture
-create_startmenu_shortcuts $manifest $dir $false
-env_add_path $manifest $dir
-env_set $manifest $dir
 
 exit 0
