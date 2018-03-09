cda4ba36610a503ed9e2db9f299aad80289e1061
yigitemres <yigitemres@users.noreply.github.com>
2018-03-09 23:55:35 +0300

    Added checking installed apps!

--- libexec/scoop-import.ps1
+++ libexec/scoop-import.ps1
@@ -59,43 +59,71 @@ function createAppsList($filePaths) {
 }
 
 function importApps($appslist) {
-    #echo $appslist
+
     $appslist.Split(",") | ForEach-Object {
 
         #echo $_         #For test purpose!
 
-        if ($_ -match '-g') {
-            if ($_ -match '\[http') {
-                if ($_ -match '-32bit') {
-                    #echo $_.Split()[2].TrimStart("[").TrimEnd("]") --global --arch 32bit
-                    scoop install $_.Split()[2].TrimStart("[").TrimEnd("]") --global --arch 32bit
+        if (checkAppsInstalled($_) -eq $True) {
+            if ($_ -match '-g') {
+                if ($_ -match '\[http') {
+                    if ($_ -match '-32bit') {
+                        #echo $_.Split()[2].TrimStart("[").TrimEnd("]") --global --arch 32bit
+                        scoop install $_.Split()[2].TrimStart("[").TrimEnd("]") --global --arch 32bit
+                    }
+                    else {
+                        #echo $_.Split()[2].TrimStart("[").TrimEnd("]") --global
+                        scoop install $_.Split()[2].TrimStart("[").TrimEnd("]") --global
+                    }
                 }
                 else {
-                    #echo $_.Split()[2].TrimStart("[").TrimEnd("]") --global
-                    scoop install $_.Split()[2].TrimStart("[").TrimEnd("]") --global
+                    #echo $_.Split()[0] --global
+                    scoop install $_.Split()[0] --global
                 }
             }
-            else {
-                #echo $_.Split()[0] --global
-                scoop install $_.Split()[0] --global
+            elseif ($_ -match '\[http') {
+                #echo $_.Split()[1].TrimStart("[").TrimEnd("]")
+                scoop install $_.Split()[1].TrimStart("[").TrimEnd("]")
             }
+            elseif ($_ -match '-32bit') {
+                #echo $_.Split()[0] --arch 32bit
+                scoop install $_.Split()[0] --arch 32bit
+            }
+            else {
+                #echo $_
+                scoop install $_
+            }
+        }else{
+            Write-Host($_.Split()[0] + " is already installed!")
         }
-        elseif ($_ -match '\[http') {
-            #echo $_.Split()[1].TrimStart("[").TrimEnd("]")
-            scoop install $_.Split()[1].TrimStart("[").TrimEnd("]")
-        }
-        elseif ($_ -match '-32bit') {
-            #echo $_.Split()[0] --arch 32bit
-            scoop install $_.Split()[0] --arch 32bit
+
+    }
+}
+
+function checkAppsInstalled($appslist) {
+    $installedapps = Get-Content $scoopinstalledlist
+
+    #echo $scoopinstalledlist
+
+    $installedapps | ForEach-Object {
+        $installedappslist += $_.Split()[0] + " "
+    }
+
+    $appslist.Split(",") | ForEach-Object {
+        if ($installedappslist -match $_.Split()[0]) {
+            return $False
         }
         else {
-            #echo $_
-            scoop install $_
+            return $True
         }
     }
 }
 
-
-
 $filePaths = $args
+$scoopinstalledlist = "scoopinstalledlist.txt"
+scoop export > $scoopinstalledlist | Out-Null
+
 importApps(createAppsList($filepaths))
+
+Remove-Item $scoopinstalledlist
+
