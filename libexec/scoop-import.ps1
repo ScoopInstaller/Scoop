bb5d0efa318ce572019034861d0ff1fbb7e7859a
yigitemres <yigitemres@users.noreply.github.com>
2018-03-09 20:40:27 +0300

    Added global and arch keywords! Thx @rasa for advice!

--- libexec/scoop-import.ps1
+++ libexec/scoop-import.ps1
@@ -5,45 +5,75 @@
 # It also supports for multiple file:
 #      scoop import file1.txt file2.txt
 
-function createAppsList($filePaths){
-    if($filePaths){
-        foreach ($file in $filePaths){
+
+function createAppsList($filePaths) {
+    if ($filePaths) {
+        foreach ($file in $filePaths) {
             $ext = [IO.Path]::GetExtension($file)
-            if($ext -eq ".txt")
-            {
-                foreach ($line in (Get-Content $file | Select-Object -Unique))
-                {
-                    if($line -ne "") #Checks empty line!
-                    {
-                        if($line.StartsWith("#") -Or $line.StartsWith(";") -Or $line.StartsWith(" "))
-                        {
+            if ($ext -eq ".txt") {
+                foreach ($line in (Get-Content $file | Select-Object -Unique)) {
+                    if ($line -ne "") {
+                        if ($line.StartsWith("#") -Or $line.StartsWith(";") -Or $line.StartsWith(" ")) {
                             continue
-                        }else{
-                            $appslist += $line.Split()[0] + " "
+                        }
+                        else {
+                            if ($line -like '*global*') {
+                                if ($line -like '*32bit*') {
+                                    $appslist += $line.Split()[0] + " -g -32bit,"
+                                }
+                                else {
+                                    $appslist += $line.Split()[0] + " -g,"
+                                }
+                            }
+                            elseif ($line -like '*32bit*') {
+                                $appslist += $line.Split()[0] + " -32bit,"
+                            }
+                            else {
+                                $appslist += $line.Split()[0] + ","
+                            }
                         }
                     }
                 }
             }
-            else{
+            else {
                 $extError = "Please select .txt file!"
-                Write-Error $extError
+                throw $extError
             }
         }
-    }else{
+    }
+    else {
         $filePathsError = "There is no file to exported!"
-        Write-Error $filePathsError
+        throw $filePathsError
     }
 
-    $uniqueAppsList = $appslist.TrimEnd() | Select-Object -Unique
+    $uniqueAppsList = $appslist.TrimEnd(",") | Select-Object -Unique
 
     return $uniqueAppsList
 }
 
-function importApps($appslist)
-{
-    $appslist.Split() | ForEach-Object{
-        #echo $_ #For test purpose!
-        scoop install $_
+function importApps($appslist) {
+    #echo $appslist
+
+    $appslist.Split(",") | ForEach-Object {
+        #echo $_         #For test purpose!
+        if ($_ -match '-g') {
+            if ($_ -match '-32bit') {
+                #echo $_.Split()[0] --global --arch 32bit
+                scoop install $_.Split()[0] --global --arch 32bit
+            }
+            else {
+                #echo $_.Split()[0] --global
+                scoop install $_.Split()[0] --global
+            }
+        }
+        elseif ($_ -match '-32bit') {
+            #echo $_.Split()[0] -a 32bit
+            scoop install $_.Split()[0] -a 32bit
+        }
+        else {
+            #echo $_
+            scoop install $_
+        }
     }
 }
 
