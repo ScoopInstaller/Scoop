c4d194c7e66a7a976137b8ad7e1781859b29e1b8
tresf <tres.finocchiaro@gmail.com>
2018-03-11 15:43:41 -0400

    Updates

--- libexec/scoop-prefix.ps1
+++ libexec/scoop-prefix.ps1
@@ -9,14 +9,17 @@ param($app)
 
 reset_aliases
 
-if($app) {
-    $app_path = appdir $app
-    if($app_path) {
-        info("$app_path")
-    }
-    else {
-        abort "Could not find app path for '$app'."
-    }
-} else { my_usage }
+if(!$app) { my_usage; exit 1 }
+
+$app_path = versiondir $app 'current' $false
+if(!(Test-Path $app_path)) {
+    $app_path = versiondir $app 'current' $true
+}
+
+if(Test-Path $app_path) {
+    Write-Output $app_path
+} else {
+    abort "Could not find app path for '$app'."
+}
 
 exit 0
