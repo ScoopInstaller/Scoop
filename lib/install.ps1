aab922dc07ac7c9a9a12c7f9626abbd560a2bdb7
Richard Kuhnt <r15ch13+git@gmail.com>
2018-06-25 11:21:47 +0200

    Add aria2 exit code translation

--- lib/install.ps1
+++ lib/install.ps1
@@ -147,6 +147,48 @@ function do_dl($url, $to, $cookies) {
     }
 }
 
+function aria_exit_code($exitcode) {
+    $codes = @{
+        0='All downloads were successful'
+        1='An unknown error occurred'
+        2='Timeout'
+        3='Resource was not found'
+        4='Aria2 saw the specified number of "resource not found" error. See --max-file-not-found option'
+        5='Download aborted because download speed was too slow. See --lowest-speed-limit option'
+        6='Network problem occurred.'
+        7='There were unfinished downloads. This error is only reported if all finished downloads were successful and there were unfinished downloads in a queue when aria2 exited by pressing Ctrl-C by an user or sending TERM or INT signal'
+        8='Remote server did not support resume when resume was required to complete download'
+        9='There was not enough disk space available'
+        10='Piece length was different from one in .aria2 control file. See --allow-piece-length-change option'
+        11='Aria2 was downloading same file at that moment'
+        12='Aria2 was downloading same info hash torrent at that moment'
+        13='File already existed. See --allow-overwrite option'
+        14='Renaming file failed. See --auto-file-renaming option'
+        15='Aria2 could not open existing file'
+        16='Aria2 could not create new file or truncate existing file'
+        17='File I/O error occurred'
+        18='Aria2 could not create directory'
+        19='Name resolution failed'
+        20='Aria2 could not parse Metalink document'
+        21='FTP command failed'
+        22='HTTP response header was bad or unexpected'
+        23='Too many redirects occurred'
+        24='HTTP authorization failed'
+        25='Aria2 could not parse bencoded file (usually ".torrent" file)'
+        26='".torrent" file was corrupted or missing information that aria2 needed'
+        27='Magnet URI was bad'
+        28='Bad/unrecognized option was given or unexpected option argument was given'
+        29='The remote server was unable to handle the request due to a temporary overloading or maintenance'
+        30='Aria2 could not parse JSON-RPC request'
+        31='Reserved. Not used'
+        32='Checksum validation failed'
+    }
+    if($null -eq $codes[$exitcode]) {
+        return 'An unknown error occurred'
+    }
+    return $codes[$exitcode]
+}
+
 function dl_with_cache_aria2($app, $version, $manifest, $architecture, $dir, $cookies = $null, $use_cache = $true, $check_hash = $true) {
     $data = @{}
     $urls = @(url $manifest $architecture)
@@ -217,9 +259,9 @@ function dl_with_cache_aria2($app, $version, $manifest, $architecture, $dir, $co
         }
 
         if($lastexitcode -gt 0) {
-            error 'Download failed!'
-            debug($urlstxt_content)
-            debug($aria2)
+            error "Download failed! (Error $lastexitcode) $(aria_exit_code $lastexitcode)"
+            debug $urlstxt_content
+            debug $aria2
             abort $(new_issue_msg $app $bucket "download via aria2 failed")
         }
 
