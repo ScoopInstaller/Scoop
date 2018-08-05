3201c5714d842a13329d23fd7862b825b2eb4033
Chawye Hsu <chawyehsu@hotmail.com>
2018-08-05 17:15:31 +0800

    Support persisting files without a file extension (#2408)

--- lib/install.ps1
+++ lib/install.ps1
@@ -1003,12 +1003,13 @@ function persist_data($manifest, $original_dir, $persist_dir) {
             write-host "Persisting $source"
 
             # add base paths
-            $source = New-Object System.IO.FileInfo(fullpath "$dir\$source")
-            if(!$source.Extension) {
-                $source = New-Object System.IO.DirectoryInfo($source.FullName)
+            if (is_directory (fullpath "$dir\$source")) {
+                $source = New-Object System.IO.DirectoryInfo(fullpath "$dir\$source")
+            } else {
+                $source = New-Object System.IO.FileInfo(fullpath "$dir\$source")
             }
             $target = New-Object System.IO.FileInfo(fullpath "$persist_dir\$target")
-            if(!$target.Extension) {
+            if(!$target.Extension -and !$source.Exists) {
                 $target = New-Object System.IO.DirectoryInfo($target.FullName)
             }
 
