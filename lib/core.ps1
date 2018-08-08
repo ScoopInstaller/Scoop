bf54a978a1bc2efcde52513a60ec5bcb7bb1a44e
Richard Kuhnt <r15ch13+git@gmail.com>
2018-08-08 11:48:58 +0200

    Add option to indent debug output

--- lib/core.ps1
+++ lib/core.ps1
@@ -49,7 +49,13 @@ function abort($msg, [int] $exit_code=1) { write-host $msg -f red; exit $exit_co
 function error($msg) { write-host "ERROR $msg" -f darkred }
 function warn($msg) {  write-host "WARN  $msg" -f darkyellow }
 function info($msg) {  write-host "INFO  $msg" -f darkgray }
-function debug($msg) { write-host "DEBUG $msg" -f darkcyan }
+function debug($msg, $indent = $false) {
+    if($indent) {
+        write-host "    DEBUG $msg" -f darkcyan
+    } else {
+        write-host "DEBUG $msg" -f darkcyan
+    }
+}
 function success($msg) { write-host $msg -f darkgreen }
 
 function filesize($length) {
