2e186abe7efba4e31fe23cc517875695e7d2f960
Elvyria <valerisafi@gmail.com>
2018-08-30 08:31:41 +0300

    GitHub manifest creation

--- libexec/scoop-create.ps1
+++ libexec/scoop-create.ps1
@@ -4,11 +4,6 @@
 param($url)
 
 function create_manifest($url) {
-    $manifest = new_manifest
-
-    $manifest.url = $url
-
-    $url_parts = $null
     try {
         $url_parts = parse_url $url
     }
@@ -16,15 +11,49 @@ function create_manifest($url) {
         abort "Error: $url is not a valid URL"
     }
 
-    $name = choose_item $url_parts "App name"
-    $name = if ($name.Length -gt 0) {
-        $name
+    $manifest = new_manifest
+
+    if ($url.StartsWith("https://github.com/")) {
+        $owner = $url_parts[0]
+        $name = $url_parts[1]
+        # Getting Info
+        $info = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name") | ConvertFrom-Json
+        $version = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name/releases") | ConvertFrom-Json
+        # In case if user url was junky
+        if ($info.homepage) {
+            $manifest.homepage = $info.homepage
+        }
+        else {
+            $manifest.homepage = $info.html_url
+        }
+        $manifest.description = $info.description
+        if ($info.license) {
+            $manifest.license = $info.license.name
+        }
+        else {
+            $manifest.Remove('license')
+        }
+        $manifest.version = $version[0].tag_name
+        $manifest.checkver = @{ github = $info.html_url }
+        if ($version[0].assets) {
+            $manifest.url = $version.assets[0].browser_download_url
+            $manifest.autoupdate = @{ "url" = $manifest.url.Replace($manifest.version, '$version')}
+        }
+        else {
+            $manifest.url = $info.clone_url
+        }
     }
     else {
+        $manifest.url = $url
+        $name = choose_item $url_parts "App name"
+        $name = if ($name.Length -gt 0) {
+            $name
+        }
+        else {
         file_name ($url_parts | select-object -last 1)
+        }
+        $manifest.version = choose_item $url_parts "Version"
     }
-
-    $manifest.version = choose_item $url_parts "Version"
 
     $manifest | convertto-json | out-file -filepath "$name.json" -encoding utf8
     $manifest_path = join-path $pwd "$name.json"
