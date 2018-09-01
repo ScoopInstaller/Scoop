7466527951adf532206febe69aa952852d1d80dc
Elvyria <valerisafi@gmail.com>
2018-09-01 16:19:57 +0300

    Licenses, tags, bag fixes

--- libexec/scoop-create.ps1
+++ libexec/scoop-create.ps1
@@ -3,6 +3,18 @@
 # Help: Create your own custom app manifest
 param($url)
 
+$manifest = [ordered]@{ "homepage" = "";
+                        "description" = "";
+                        "license" = @{};
+                        "version" = "";
+                        "url" = "";
+                        "hash" = "";
+                        "extract_dir" = "";
+                        "bin" = "";
+                        "depends" = "";
+                        "checkver" = "";
+                        "autoupdate" = @{} }
+
 function create_manifest($url) {
     try {
         $url_parts = parse_url $url
@@ -11,59 +23,9 @@ function create_manifest($url) {
         abort "Error: $url is not a valid URL"
     }
 
-    $manifest = new_manifest
-
     if ($url.StartsWith("https://github.com/")) {
-        $owner = $url_parts[0]
+        github
         $name = $url_parts[1]
-        # Getting Info
-        $info = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name") | ConvertFrom-Json
-        $version = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name/releases") | ConvertFrom-Json
-        # In case if user url was junky
-        if ($info.homepage) {
-            $manifest.homepage = $info.homepage
-        }
-        else {
-            $manifest.homepage = $info.html_url
-        }
-        $manifest.description = $info.description
-        if ($info.license) {
-            $manifest.license = $info.license.name
-        }
-        else {
-            $manifest.Remove('license')
-        }
-        $manifest.version = $version[0].tag_name
-        $manifest.checkver = @{ github = $info.html_url }
-        if ($version[0].assets) {
-            # Trying to guess architecture if it's important
-            foreach ($asset in $version[0].assets) {
-                if ($asset.name -notMatch "linux|macos|osx|arm") {
-                    if ($asset.name -match "x64" -and (!$url_64 -or $asset.name -match "win")) {
-                        $url_64 = $asset.browser_download_url
-                    }
-                    if ($asset.name -match "x86" -and (!$url_32 -or $asset.name -match "win")) {
-                        $url_32 = $asset.browser_download_url
-                    }
-                }
-            }
-            if ($url_64) {
-                $manifest.Remove("url")
-                $manifest.architecture = @{ "64bit" = @{ "url" = $url_64; "hash" = "" } }
-                $manifest.autoupdate = @{ "architecture" = @{ "64bit" = @{ "url" = $url_64.Replace($manifest.version, '$version') } } }
-                if ($url_32) {
-                    $manifest.architecture["32bit"] = @{ "url" = $url_32; "hash" = "" }
-                    $manifest.autoupdate.architecture["32bit"] = @{ "url" = $url_32.Replace($manifest.version, '$version') }
-                }
-            }
-            else {
-                $manifest.url = $version[0].assets[0].browser_download_url
-                $manifest.autoupdate = $manifest.url.Replace($manifest.version, '$version')
-            }
-        }
-        else {
-            $manifest.url = $info.clone_url
-        }
     }
     else {
         # Old Code for default (any url) manifest
@@ -73,7 +35,7 @@ function create_manifest($url) {
             $name
         }
         else {
-        file_name ($url_parts | select-object -last 1)
+            file_name ($url_parts | select-object -last 1)
         }
         $manifest.version = choose_item $url_parts "Version"
     }
@@ -83,19 +45,103 @@ function create_manifest($url) {
     write-host "Created '$manifest_path'."
 }
 
-function new_manifest() {
-    [ordered]@{ "homepage" = "";
-                "description" = "";
-                "license" = "";
-                "version" = "";
-                "url" = "";
-                "hash" = "";
-                "extract_dir" = "";
-                "bin" = "";
-                "depends" = "";
-                "checkver" = "";
-                "autoupdate" = "" }
+function github {
+    $owner = $url_parts[0]
+    $name = $url_parts[1]
+    # Getting Info
+    $info = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name") | ConvertFrom-Json
+    if ($info.homepage) {
+        $manifest.homepage = $info.homepage
+    }
+    else {
+        $manifest.homepage = $info.html_url
+    }
+    $manifest.description = $info.description
+    if ($info.license) {
+        if ($info.license -match "Other") {
+            $manifest.license["url"] = "https://raw.githubusercontent.com/" + $info.full_name + "/" + $info.default_branch + "/LICENSE.txt"
+        }
+        else {
+            $manifest.license["identifier"] = $licenses[$info.license.name]
+        }
+    }
+    else {
+        $manifest.Remove('license')
+    }
+    $manifest.checkver = @{ github = $info.html_url }
+    $releases = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name/releases") | ConvertFrom-Json
+    if ($releases[0].assets) {
+        $manifest.version = $releases[0].tag_name
+        # Trying to guess architecture if it's important
+        foreach ($asset in $releases[0].assets) {
+            if ($asset.name -notMatch "linux|macos|osx|arm") {
+                if ($asset.name -match "x64" -and (!$url_64 -or $asset.name -match "win")) {
+                    $url_64 = $asset.browser_download_url
+                }
+                if ($asset.name -match "x86" -and (!$url_32 -or $asset.name -match "win")) {
+                    $url_32 = $asset.browser_download_url
+                }
+            }
+        }
+        if ($url_64) {
+            $manifest.Remove("url")
+            $manifest.architecture = @{ "64bit" = @{ "url" = $url_64; "hash" = "" } }
+            $manifest.autoupdate["architecture"] = @{ "64bit" = @{ "url" = $url_64.Replace($manifest.version, '$version') } }
+            if ($url_32) {
+                $manifest.architecture["32bit"] = @{ "url" = $url_32; "hash" = "" }
+                $manifest.autoupdate.architecture["32bit"] = @{ "url" = $url_32.Replace($manifest.version, '$version') }
+            }
+        }
+        else {
+            $manifest.url = $releases[0].assets[0].browser_download_url
+            $manifest.autoupdate["url"] = $manifest.url.Replace($manifest.version, '$version')
+        }
+    }
+    else {
+        # If there's no releases try to find tags
+        $tags = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name/tags") | ConvertFrom-Json
+        if ($tags[0]) {
+            $manifest.version = $tags[0].name
+            $manifest.url = "https://github.com/$owner/$name/archive/" + $manifest.version + ".zip"
+            $manifest.autoupdate["url"] = "https://github.com/$owner/$name/archive/`$version.zip"
+        }
+        else {
+            $manifest.url = "https://github.com/$owner/$name/archive/" + $info.default_branch + ".zip"
+        }
+    }
 }
+
+# GitHub names to SPDX identifiers
+$licenses = @{ "Academic Free License v3.0" = "AFL-3.0";
+               "Apache license 2.0" = "Apache-2.0";
+               "Artistic license 2.0" = "Artistic-2.0";
+               "Boost Software License 1.0" = "BSL-1.0";
+               "BSD 2-clause `"Simplified`" license" = "BSD-2-Clause";
+               "BSD 3-clause `"New`" or `"Revised`" license" = "BSD-3-Clause";
+               "BSD 3-clause Clear license" = "BSD-3-Clause-Clear";
+               "Creative Commons Zero v1.0 Universal" = "CC0-1.0";
+               "Creative Commons Attribution 4.0" = "CC-BY-4.0";
+               "Creative Commons Attribution Share Alike 4.0" = "CC-BY-SA-4.0";
+               "Do What The F*ck You Want To Public License" = "WTFPL";
+               "Educational Community License v2.0" = "ECL-2.0";
+               "Eclipse Public License 1.0" = "EPL-1.0";
+               "European Union Public License 1.1" = "EUPL-1.1";
+               "GNU Affero General Public License v3.0" = "AGPL-3.0-only";
+               "GNU General Public License v2.0" = "GPL-2.0-only";
+               "GNU General Public License v3.0" = "GPL-3.0-only";
+               "GNU Lesser General Public License v2.1" = "LGPL-2.1-only";
+               "GNU Lesser General Public License v3.0" = "LGPL-3.0-only";
+               "ISC" = "ISC";
+               "LaTeX Project Public License v1.3c" = "LPPL-1.3c";
+               "Microsoft Public License" = "MS-PL";
+               "MIT" = "MIT";
+               "Mozilla Public License 2.0" = "MPL-2.0";
+               "Open Software License 3.0" = "OSL-3.0";
+               "PostgreSQL License" = "PostgreSQL";
+               "SIL Open Font License 1.1" = "OFL-1.1";
+               "University of Illinois/NCSA Open Source License" = "NCSA";
+               "The Unlicense" = "Unlicense";
+               "zLib License" = "Zlib" }
 
 function file_name($segment) {
     $segment.substring(0, $segment.lastindexof('.'))
