e158d65802b24ca3e34262e51be1eb7a88b446d0
Elvyria <valerisafi@gmail.com>
2018-09-18 10:58:06 +0300

    Extract_dir, bin, styling and a bunch of fixes

--- libexec/scoop-create.ps1
+++ libexec/scoop-create.ps1
@@ -3,9 +3,11 @@
 # Help: Create your own custom app manifest
 param($url)
 
+. "$psscriptroot\..\lib\core.ps1"
 . "$psscriptroot\..\lib\json.ps1"
 . "$psscriptroot\..\lib\install.ps1"
 
+$name
 $manifest = [ordered]@{ "homepage" = "";
                         "description" = "";
                         "license" = @{};
@@ -13,7 +15,7 @@ $manifest = [ordered]@{ "homepage" = "";
                         "url" = "";
                         "hash" = "";
                         "extract_dir" = "";
-                        "bin" = "";
+                        "bin" = @();
                         "depends" = "";
                         "checkver" = "";
                         "autoupdate" = @{} }
@@ -25,24 +27,24 @@ function create_manifest($url) {
         abort "Error: $url is not a valid URL"
     }
 
-    if ($url.StartsWith("https://github.com/")) {
+    if ($url.startsWith("https://github.com/")) {
         github
         $name = $url_parts[1]
     } else {
         # Old Code for default (any url) manifest
         $manifest.url = $url
-        $name = choose_item $url_parts "App name"
+        $name = choose_item $url_parts "App Name"
         $name = if ($name.Length -gt 0) {
             $name
         } else {
-            file_name ($url_parts | select-object -last 1)
+            file_name ($url_parts | Select-Object -last 1)
         }
-        $manifest.version = choose_item $url_parts "Version"
+        $manifest.version = choose_item $url_parts
     }
 
-    $manifest | ConvertToPrettyJson | out-file -filepath "$name.json" -encoding utf8
-    $manifest_path = join-path $pwd "$name.json"
-    write-host "Created '$manifest_path'."
+    $manifest | ConvertToPrettyJson | Out-File -filepath "$name.json" -encoding utf8
+    $manifest_path = Join-Path $pwd "$name.json"
+    Write-Host "Created '$manifest_path'."
 }
 
 function github {
@@ -51,112 +53,148 @@ function github {
 
     $info = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name") | ConvertFrom-Json
 
-    if ($info.homepage) {
-        $manifest.homepage = $info.homepage
-    } else {
-        $manifest.homepage = $info.html_url
-    }
-
+    $manifest.homepage = if ($info.homepage) {$info.homepage} else {$info.html_url}
     $manifest.description = $info.description
-
-    if ($info.license) {
-        if ($info.license -match "Other") {
-            $manifest.license["url"] = "https://raw.githubusercontent.com/" + $info.full_name + "/" + $info.default_branch + "/LICENSE.txt"
-        } else {
-            $manifest.license["identifier"] = $licenses[$info.license.name]
-        }
-    } else {
-        $manifest.Remove('license')
-    }
-
     $manifest.checkver = @{ github = $info.html_url }
 
+    if ($info.license.spdx_id) {
+        $manifest.license["identifier"] = $info.license.spdx_id
+    } else {
+        $manifest.license["url"] = "https://raw.githubusercontent.com/$($info.full_name)/$($info.default_branch)/LICENSE.txt"
+    }
+
     $releases = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name/releases") | ConvertFrom-Json
-    if ($releases[0].assets) {
-        $manifest.version = $releases[0].tag_name
-        # Trying to guess architecture if it's important
-        foreach ($asset in $releases[0].assets) {
-            if ($asset.name -match ".(zip|rar|7z|msi|exe)") {
-                if ($asset.name -match "x64" -and (!$release_64 -or $asset.name -match ".(zip|7z|rar)")) {
-                    $release_64 = $asset
-                }
-                if ($asset.name -match "x86" -and (!$release_32 -or $asset.name -match ".(zip|7z|rar)")) {
-                    $release_32 = $asset
-                }
+    # If last version is prerelease try to find release and let user choose wich one to use
+    if ($releases[0].prerelease -and $releases.length -gt 1) {
+        for ($i = 0; $i -lt $releases.length; $i++) {
+            if (!$releases[$i].prerelease) {
+                $release = $releases[$i]
+                break
             }
         }
-        if ($release_64) {
-            $manifest.Remove("url")
-            $manifest.Remove("hash")
-            $manifest.architecture = @{ "64bit" = @{ "url" = $release_64.browser_download_url; "hash" = "" } }
-            $manifest.architecture["64bit"].hash = calculate_hash $release_64.name $release_64.browser_download_url
-            $manifest.autoupdate["architecture"] = @{ "64bit" = @{ "url" = $release_64.browser_download_url.Replace($manifest.version, '$version') } }
-            if ($release_32) {
-                $manifest.architecture["32bit"] = @{ "url" = $release_32.browser_download_url; "hash" = "" }
-                $manifest.architecture["32bit"].hash = calculate_hash $release_32.name $release_32.browser_download_url
-                $manifest.autoupdate.architecture["32bit"] = @{ "url" = $release_32.browser_download_url.Replace($manifest.version, '$version') }
-            }
-        } else {
-            $manifest.url = $releases[0].assets[0].browser_download_url
-            $manifest.autoupdate["url"] = $manifest.url.Replace($manifest.version, '$version')
-            $manifest.hash = calculate_hash $releases[0].assets[0].name $manifest.url
+        if ($release -and (confirm "Want to use prerelease ?")) {
+            $release = $releases[0]
         }
     } else {
-        # If there's no releases try to find tags
+        $release = $releases[0]
+    }
+
+    if ($release.assets) {
+        $windows_ext = ".(zip|rar|7z|msi|exe)"
+        $prefered_ext = ".(zip|7z|rar)"
+
+        $manifest.version = $release.tag_name
+
+        foreach ($asset in $release.assets) {
+            if ($asset.name -match $windows_ext) {
+                if ($asset.name -match "x64" -and (!$asset_64 -or $asset.name -match $prefered_ext)) {
+                    $asset_64 = $asset
+                }
+                if ($asset.name -match "x86" -and (!$asset_32 -or $asset.name -match $prefered_ext)) {
+                    $asset_32 = $asset
+                }
+            }
+        }
+
+        if (!($asset_32 -or $asset_64)) {
+            if ($release.assets.length -gt 1) {
+                $asset_32 = choose_item $release.assets $null "name"
+            } else {
+                $asset_32 = $release.assets[0]
+            }
+        }
+
+        url $asset_32.browser_download_url $asset_64.browser_download_url
+
+        if ($asset_64) {
+            if (confirm "Download $($asset_64.name) to calculate hash ?") {
+                install_info $asset_64.browser_download_url "64bit"
+            }
+        }
+
+        if ($asset_32) {
+            if (confirm "Download $($asset_32.name) to calculate hash ?") {
+                install_info $asset_32.browser_download_url "32bit"
+            }
+        }
+
+    } else { # If there's no releases try to find tags
         $tags = Invoke-WebRequest -Uri ("https://api.github.com/repos/$owner/$name/tags") | ConvertFrom-Json
         if ($tags[0]) {
             $manifest.version = $tags[0].name
-            $manifest.url = "https://github.com/$owner/$name/archive/" + $manifest.version + ".zip"
-            $manifest.hash = calculate_hash $manifest.version $manifest.url
-            $manifest.autoupdate["url"] = "https://github.com/$owner/$name/archive/`$version.zip"
-        } else {
-            $manifest.url = "https://github.com/$owner/$name/archive/" + $info.default_branch + ".zip"
+            url "https://github.com/$owner/$name/archive/$($manifest.version).zip"
+            if (confirm "Download $($manifest.version) to calculate hash ?") {
+                $manifest.hash = get_hash_for_app $name $null $manifest.version $manifest.url
+                install_info $manifest.url
+            }
         }
     }
 
 }
 
-function calculate_hash($name, $url) {
-    $confirm = Read-Host "Download $name to calculate hash ? (y/N)"
-    if ($skip -or $confirm.ToLower() -eq 'y') {
-        get_hash_for_app $null $null $null $url
+function install_info($url, $architecture) {
+    if ($architecture -eq "64bit" -or $manifest.architecture) {
+        $manifest.architecture[$architecture].hash = get_hash_for_app $name $null $manifest.version $url
+    } else {
+        $manifest.hash = get_hash_for_app $name $null $manifest.version $url
+    }
+    $tmp = "$pwd\_tmp"
+    unzip (cache_path $name $manifest.version $url) $tmp
+    extract_dir $tmp
+    bin $tmp
+    Remove-Item $tmp -Force -Recurse
+}
+
+function url($32bit, $64bit) {
+    if ($64bit) {
+        $manifest.remove("url")
+        $manifest.remove("hash")
+        $manifest.architecture = @{ "64bit" = @{ "url" = $64bit; "hash" =  "" } }
+        $manifest.autoupdate["architecture"] = @{ "64bit" = @{ "url" = $64bit.replace($manifest.version, '$version') } }
+        if ($32bit) {
+            $manifest.architecture["32bit"] = @{ "url" = $asset_32.browser_download_url; "hash" = "" }
+            $manifest.autoupdate.architecture["32bit"] = @{ "url" = $32bit.replace($manifest.version, '$version') }
+        }
+    } else {
+        $manifest.url = $32bit
+        $manifest.autoupdate["url"] = $32bit.replace($manifest.version, '$version')
     }
 }
 
-# GitHub names to SPDX identifiers
-$licenses = @{ "Academic Free License v3.0" = "AFL-3.0";
-               "Apache license 2.0" = "Apache-2.0";
-               "Artistic license 2.0" = "Artistic-2.0";
-               "Boost Software License 1.0" = "BSL-1.0";
-               "BSD 2-clause `"Simplified`" license" = "BSD-2-Clause";
-               "BSD 3-clause `"New`" or `"Revised`" license" = "BSD-3-Clause";
-               "BSD 3-clause Clear license" = "BSD-3-Clause-Clear";
-               "Creative Commons Zero v1.0 Universal" = "CC0-1.0";
-               "Creative Commons Attribution 4.0" = "CC-BY-4.0";
-               "Creative Commons Attribution Share Alike 4.0" = "CC-BY-SA-4.0";
-               "Do What The F*ck You Want To Public License" = "WTFPL";
-               "Educational Community License v2.0" = "ECL-2.0";
-               "Eclipse Public License 1.0" = "EPL-1.0";
-               "European Union Public License 1.1" = "EUPL-1.1";
-               "GNU Affero General Public License v3.0" = "AGPL-3.0-only";
-               "GNU General Public License v2.0" = "GPL-2.0-only";
-               "GNU General Public License v3.0" = "GPL-3.0-only";
-               "GNU Lesser General Public License v2.1" = "LGPL-2.1-only";
-               "GNU Lesser General Public License v3.0" = "LGPL-3.0-only";
-               "ISC" = "ISC";
-               "LaTeX Project Public License v1.3c" = "LPPL-1.3c";
-               "Microsoft Public License" = "MS-PL";
-               "MIT" = "MIT";
-               "Mozilla Public License 2.0" = "MPL-2.0";
-               "Open Software License 3.0" = "OSL-3.0";
-               "PostgreSQL License" = "PostgreSQL";
-               "SIL Open Font License 1.1" = "OFL-1.1";
-               "University of Illinois/NCSA Open Source License" = "NCSA";
-               "The Unlicense" = "Unlicense";
-               "zLib License" = "Zlib" }
+function extract_dir($path) {
+    $dir = Get-ChildItem -Path $path -Directory -Name
+    if ($dir.count -eq 1) {
+        $manifest.extract_dir = "$dir"
+        if ($dir -match $manifest.version) {
+            $manifest.autoupdate["extract_dir"] = $dir.replace($manifest.version, '$version')
+        }
+    } else { $manifest.remove("extract_dir") }
+}
+
+function bin($path) {
+    $bin_ext = @("*.exe", "*.bat")
+    $bin = Get-ChildItem -Path $path -File -Include $bin_ext -Recurse
+
+    if ($manifest.extract_dir) {
+        $bin = $bin -Replace [Regex]::Escape("$path\$($manifest.extract_dir)\")
+    } else {
+        $bin = $bin -Replace [Regex]::Escape("$path\")
+    }
+
+    if ($bin -ne $manifest.bin) {
+        $manifest.bin += $bin
+    } else {
+        $manifest.bin = $bin
+    }
+}
 
 function file_name($segment) {
-    $segment.substring(0, $segment.lastindexof('.'))
+    $segment.substring(0, $segment.lastIndexOf('.'))
+}
+
+function confirm($quote, $command) {
+    $answer = Read-Host $quote "(y/N)"
+    return $answer.toLower() -eq 'y'
 }
 
 function parse_url($url) {
@@ -164,18 +202,26 @@ function parse_url($url) {
     $uri.pathandquery.substring(1).split("/")
 }
 
-function choose_item($list, $query) {
-    for ($i = 0; $i -lt $list.count; $i++) {
+function choose_item($list, $quote, $property) {
+    for ($i = 0; $i -lt $list.length; $i++) {
         $item = $list[$i]
-        write-host "$($i + 1)) $item"
-    }
-    $sel = read-host $query
-
-    if ($sel.trim() -match '^[0-9+]$') {
-        return $list[$sel-1]
+        if ($property) {
+            $item = $item | Select -ExpandProperty $property
+        }
+        Write-Host "[$($i + 1)]: $item"
     }
 
-    $sel
+    $choose =  {
+        $selection = if ($quote) {Read-Host $quote} else {Read-Host}
+        if ($selection -match "\d+" -and ($selection - 1) -lt $list.length) {
+            return $list[$selection - 1]
+        } else {
+            Write-Host "Out of bound. Try again..."
+            & $choose
+        }
+    }
+
+    & $choose
 }
 
 if (!$url) {
