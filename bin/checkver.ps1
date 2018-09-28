758b61c18e52cf9455c9ace2cad4afb7ff855400
Jakub Čábera <cabera.jakub@gmail.com>
2018-09-28 18:47:36 +0200

    Fix indentation

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -20,12 +20,12 @@
     Check all manifest inside default directory.
 #>
 param(
-	[String] $App = '*',
-	[ValidateScript( {Test-Path $_ -Type Container})]
-	[String] $Dir = "$psscriptroot\..\bucket",
-	[Switch] $Update,
-	[Switch] $ForceUpdate,
-	[Switch] $SkipSupported
+    [String] $App = '*',
+    [ValidateScript( {Test-Path $_ -Type Container})]
+    [String] $Dir = "$psscriptroot\..\bucket",
+    [Switch] $Update,
+    [Switch] $ForceUpdate,
+    [Switch] $SkipSupported
 )
 
 . "$psscriptroot\..\lib\core.ps1"
@@ -45,211 +45,211 @@ $Search = $App
 $Queue = @()
 $json = ''
 Get-ChildItem $Dir "$App.json" | ForEach-Object {
-	$json = parse_json "$Dir\$($_.Name)"
-	if ($json.checkver) {
-		$Queue += , @($_.Name, $json)
-	}
+    $json = parse_json "$Dir\$($_.Name)"
+    if ($json.checkver) {
+        $Queue += , @($_.Name, $json)
+    }
 }
 
 # clear any existing events
 Get-Event | ForEach-Object {
-	Remove-Event $_.SourceIdentifier
+    Remove-Event $_.SourceIdentifier
 }
 
 $original = use_any_https_protocol
 
 # start all downloads
 $Queue | ForEach-Object {
-	$name, $json = $_
+    $name, $json = $_
 
-	$substitutions = get_version_substitutions $json.version
+    $substitutions = get_version_substitutions $json.version
 
-	$wc = New-Object Net.Webclient
-	if ($json.checkver.useragent) {
-		$wc.Headers.Add('User-Agent', (substitute $json.checkver.useragent $substitutions))
-	} else {
-		$wc.Headers.Add('User-Agent', (Get-UserAgent))
-	}
-	Register-ObjectEvent $wc downloadstringcompleted -ErrorAction Stop | Out-Null
+    $wc = New-Object Net.Webclient
+    if ($json.checkver.useragent) {
+        $wc.Headers.Add('User-Agent', (substitute $json.checkver.useragent $substitutions))
+    } else {
+        $wc.Headers.Add('User-Agent', (Get-UserAgent))
+    }
+    Register-ObjectEvent $wc downloadstringcompleted -ErrorAction Stop | Out-Null
 
-	$githubRegex = '\/releases\/tag\/(?:v)?([\d.]+)'
+    $githubRegex = '\/releases\/tag\/(?:v)?([\d.]+)'
 
-	$url = $json.homepage
-	if ($json.checkver.url) {
-		$url = $json.checkver.url
-	}
-	$regex = ''
-	$jsonpath = ''
-	$replace = ''
+    $url = $json.homepage
+    if ($json.checkver.url) {
+        $url = $json.checkver.url
+    }
+    $regex = ''
+    $jsonpath = ''
+    $replace = ''
 
-	if ($json.checkver -eq 'github') {
-		if (!$json.homepage.StartsWith('https://github.com/')) {
-			error "$name checkver expects the homepage to be a github repository"
-		}
-		$url = $json.homepage + '/releases/latest'
-		$regex = $githubRegex
-	}
+    if ($json.checkver -eq 'github') {
+        if (!$json.homepage.StartsWith('https://github.com/')) {
+            error "$name checkver expects the homepage to be a github repository"
+        }
+        $url = $json.homepage + '/releases/latest'
+        $regex = $githubRegex
+    }
 
-	if ($json.checkver.github) {
-		$url = $json.checkver.github + '/releases/latest'
-		$regex = $githubRegex
-	}
+    if ($json.checkver.github) {
+        $url = $json.checkver.github + '/releases/latest'
+        $regex = $githubRegex
+    }
 
-	if ($json.checkver.re) {
-		$regex = $json.checkver.re
-	}
-	if ($json.checkver.regex) {
-		$regex = $json.checkver.regex
-	}
+    if ($json.checkver.re) {
+        $regex = $json.checkver.re
+    }
+    if ($json.checkver.regex) {
+        $regex = $json.checkver.regex
+    }
 
-	if ($json.checkver.jp) {
-		$jsonpath = $json.checkver.jp
-	}
-	if ($json.checkver.jsonpath) {
-		$jsonpath = $json.checkver.jsonpath
-	}
+    if ($json.checkver.jp) {
+        $jsonpath = $json.checkver.jp
+    }
+    if ($json.checkver.jsonpath) {
+        $jsonpath = $json.checkver.jsonpath
+    }
 
-	if ($json.checkver.replace -and $json.checkver.replace.GetType() -eq [System.String]) {
-		$replace = $json.checkver.replace
-	}
+    if ($json.checkver.replace -and $json.checkver.replace.GetType() -eq [System.String]) {
+        $replace = $json.checkver.replace
+    }
 
-	if (!$jsonpath -and !$regex) {
-		$regex = $json.checkver
-	}
+    if (!$jsonpath -and !$regex) {
+        $regex = $json.checkver
+    }
 
-	$reverse = $json.checkver.reverse -and $json.checkver.reverse -eq 'true'
+    $reverse = $json.checkver.reverse -and $json.checkver.reverse -eq 'true'
 
-	$url = substitute $url $substitutions
+    $url = substitute $url $substitutions
 
-	$state = New-Object psobject @{
-		app      = (strip_ext $name);
-		url      = $url;
-		regex    = $regex;
-		json     = $json;
-		jsonpath = $jsonpath;
-		reverse  = $reverse;
-		replace  = $replace;
-	}
+    $state = New-Object psobject @{
+        app      = (strip_ext $name);
+        url      = $url;
+        regex    = $regex;
+        json     = $json;
+        jsonpath = $jsonpath;
+        reverse  = $reverse;
+        replace  = $replace;
+    }
 
-	$wc.Headers.Add('Referer', (strip_filename $url))
-	$wc.DownloadStringAsync($url, $state)
+    $wc.Headers.Add('Referer', (strip_filename $url))
+    $wc.DownloadStringAsync($url, $state)
 }
 
 function next($er) {
-	Write-Host "$App`: " -NoNewline
-	Write-Host $er -ForegroundColor DarkRed
+    Write-Host "$App`: " -NoNewline
+    Write-Host $er -ForegroundColor DarkRed
 }
 
 # wait for all to complete
 $in_progress = $Queue.length
 while ($in_progress -gt 0) {
-	$ev = Wait-Event
-	Remove-Event $ev.SourceIdentifier
-	$in_progress--
+    $ev = Wait-Event
+    Remove-Event $ev.SourceIdentifier
+    $in_progress--
 
-	$state = $ev.SourceEventArgs.UserState
-	$app = $state.app
-	$json = $state.json
-	$url = $state.url
-	$regexp = $state.regex
-	$jsonpath = $state.jsonpath
-	$reverse = $state.reverse
-	$replace = $state.replace
-	$expected_ver = $json.version
-	$ver = ''
+    $state = $ev.SourceEventArgs.UserState
+    $app = $state.app
+    $json = $state.json
+    $url = $state.url
+    $regexp = $state.regex
+    $jsonpath = $state.jsonpath
+    $reverse = $state.reverse
+    $replace = $state.replace
+    $expected_ver = $json.version
+    $ver = ''
 
-	$err = $ev.SourceEventArgs.Error
-	$page = $ev.SourceEventArgs.Result
+    $err = $ev.SourceEventArgs.Error
+    $page = $ev.SourceEventArgs.Result
 
-	if ($err) {
-		next "$($err.message)`r`nURL $url is not valid"
-		continue
-	}
+    if ($err) {
+        next "$($err.message)`r`nURL $url is not valid"
+        continue
+    }
 
-	if (!$regex -and $replace) {
-		next "'replace' requires 're' or 'regex'"
-		continue
-	}
+    if (!$regex -and $replace) {
+        next "'replace' requires 're' or 'regex'"
+        continue
+    }
 
-	if ($jsonpath) {
-		$ver = json_path $page $jsonpath
-		if (!$ver) {
-			$ver = json_path_legacy $page $jsonpath
-		}
-		if (!$ver) {
-			next "couldn't find '$jsonpath' in $url"
-			continue
-		}
-	}
+    if ($jsonpath) {
+        $ver = json_path $page $jsonpath
+        if (!$ver) {
+            $ver = json_path_legacy $page $jsonpath
+        }
+        if (!$ver) {
+            next "couldn't find '$jsonpath' in $url"
+            continue
+        }
+    }
 
-	if ($jsonpath -and $regexp) {
-		$page = $ver
-		$ver = ''
-	}
+    if ($jsonpath -and $regexp) {
+        $page = $ver
+        $ver = ''
+    }
 
-	if ($regexp) {
-		$regex = New-Object System.Text.RegularExpressions.Regex($regexp)
-		if ($reverse) {
-			$match = $regex.Matches($page) | Select-Object -Last 1
-		} else {
-			$match = $regex.Matches($page) | Select-Object -First 1
-		}
+    if ($regexp) {
+        $regex = New-Object System.Text.RegularExpressions.Regex($regexp)
+        if ($reverse) {
+            $match = $regex.Matches($page) | Select-Object -Last 1
+        } else {
+            $match = $regex.Matches($page) | Select-Object -First 1
+        }
 
-		if ($match -and $match.Success) {
-			$matchesHashtable = @{}
-			$regex.GetGroupNames() | ForEach-Object { $matchesHashtable.Add($_, $match.Groups[$_].Value) }
-			$ver = $matchesHashtable['1']
-			if ($replace) {
-				$ver = $regex.Replace($match.Value, $replace)
-			}
-			if (!$ver) {
-				$ver = $matchesHashtable['version']
-			}
-		} else {
-			next "couldn't match '$regexp' in $url"
-			continue
-		}
-	}
+        if ($match -and $match.Success) {
+            $matchesHashtable = @{}
+            $regex.GetGroupNames() | ForEach-Object { $matchesHashtable.Add($_, $match.Groups[$_].Value) }
+            $ver = $matchesHashtable['1']
+            if ($replace) {
+                $ver = $regex.Replace($match.Value, $replace)
+            }
+            if (!$ver) {
+                $ver = $matchesHashtable['version']
+            }
+        } else {
+            next "couldn't match '$regexp' in $url"
+            continue
+        }
+    }
 
-	if (!$ver) {
-		next "couldn't find new version in $url"
-		continue
-	}
+    if (!$ver) {
+        next "couldn't find new version in $url"
+        continue
+    }
 
-	# Skip actual only if versions are same and there is no -f
-	if (($ver -eq $expected_ver) -and !$ForceUpdate -and $SkipSupported) { continue }
+    # Skip actual only if versions are same and there is no -f
+    if (($ver -eq $expected_ver) -and !$ForceUpdate -and $SkipSupported) { continue }
 
-	Write-Host "$App`: " -NoNewline
+    Write-Host "$App`: " -NoNewline
 
-	# version hasn't changed (step over if forced update)
-	if ($ver -eq $expected_ver -and !$ForceUpdate) {
-		Write-Host $ver -ForegroundColor DarkGreen
-		continue
-	}
+    # version hasn't changed (step over if forced update)
+    if ($ver -eq $expected_ver -and !$ForceUpdate) {
+        Write-Host $ver -ForegroundColor DarkGreen
+        continue
+    }
 
-	Write-Host $ver -ForegroundColor DarkRed -NoNewline
-	Write-Host " (scoop version is $expected_ver)" -NoNewline
-	$Update_available = (compare_versions $expected_ver $ver) -eq -1
+    Write-Host $ver -ForegroundColor DarkRed -NoNewline
+    Write-Host " (scoop version is $expected_ver)" -NoNewline
+    $Update_available = (compare_versions $expected_ver $ver) -eq -1
 
-	if ($json.autoupdate -and $Update_available) {
-		Write-Host ' autoupdate available' -ForegroundColor Cyan
-	} else {
-		Write-Host ''
-	}
+    if ($json.autoupdate -and $Update_available) {
+        Write-Host ' autoupdate available' -ForegroundColor Cyan
+    } else {
+        Write-Host ''
+    }
 
-	# forcing an update implies updating, right?
-	if ($ForceUpdate) { $Update = $true }
+    # forcing an update implies updating, right?
+    if ($ForceUpdate) { $Update = $true }
 
-	if ($Update -and $json.autoupdate) {
-		if ($ForceUpdate) {
-			Write-Host 'Forcing autoupdate!' -ForegroundColor DarkMagenta
-		}
-		try {
-			autoupdate $App $Dir $json $ver $matchesHashtable
-		} catch {
-			error $_.Exception.Message
-		}
-	}
+    if ($Update -and $json.autoupdate) {
+        if ($ForceUpdate) {
+            Write-Host 'Forcing autoupdate!' -ForegroundColor DarkMagenta
+        }
+        try {
+            autoupdate $App $Dir $json $ver $matchesHashtable
+        } catch {
+            error $_.Exception.Message
+        }
+    }
 }
 
 set_https_protocols $original
