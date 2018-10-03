63deba02be5f0ed101343c6903e0bcf713be2546
Jakub Čábera <cabera.jakub@gmail.com>
2018-10-03 15:42:11 +0200

    [Checkver] Support skipping up to date manifests (#2624)
    - Lint file a bit
    - Add synopsis
    - Add default parameters
    - Replace shortcutted cmdlets / parameters
    - Add schema validation for VSCode
    - Add recommended VSCode extensions
    - Move everything from scoop-sublime-project into settings.json
    - Small progress on #2609 and making developers life easier

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,17 +1,61 @@
-# checks websites for newer versions using an (optional) regular expression defined in the manifest
-# use $dir to specify a manifest directory to check from, otherwise ./bucket is used
+<#
+.SYNOPSIS
+    Check manifest for a newer version.
+.DESCRIPTION
+    Checks websites for newer versions using an (optional) regular expression defined in the manifest.
+.PARAMETER App
+    Manifest name to search.
+    Placeholders are supported.
+.PARAMETER Dir
+    Where to search for manifest(s).
+.PARAMETER Update
+    Update given manifest
+.PARAMETER ForceUpdate
+    Update given manifest(s) even when there is no new version.
+    Useful for hash updates.
+.PARAMETER SkipUpdated
+    Updated manifests will not be shown.
+.EXAMPLE
+    PS BUCKETDIR > .\bin\checkver.ps1
+    Check all manifest inside default directory.
+.EXAMPLE
+    PS BUCKETDIR > .\bin\checkver.ps1 -s
+    Check all manifest inside default directory (list only outdated manifests).
+.EXAMPLE
+    PS BUCKETDIR > .\bin\checkver.ps1 -u
+    Check all manifests and update All outdated manifests.
+.EXAMPLE
+    PS BUCKETDIR > .\bin\checkver.ps1 MAN
+    Check manifest MAN.json inside default directory.
+.EXAMPLE
+    PS BUCKETDIR > .\bin\checkver.ps1 MAN -u
+    Check manifest MAN.json and update, if there is newer vesion.
+.EXAMPLE
+    PS BUCKETDIR > .\bin\checkver.ps1 MAN -f
+    Check manifest MAN.json and update, even if there is no new vesion.
+.EXAMPLE
+    PS BUCKETDIR > .\bin\checkver.ps1 MAN DIR
+    Check manifest MAN.json inside ./DIR directory.
+.EXAMPLE
+    PS BUCKETDIR > .\bin\checkver.ps1 -Dir DIR
+    Check all manifests inside ./DIR directory.
+.EXAMPLE
+        PS BUCKETDIR > .\bin\checkver.ps1 MAN DIR -u
+        Check manifest MAN.json inside ./DIR directory and update if there is newer version.
+#>
 param(
-    [String]$app,
-    [String]$dir,
-    [Switch]$update = $false,
-    [Switch]$forceUpdate = $false
+    [String] $App = '*',
+    [ValidateScript( {
+        if (!(Test-Path $_ -Type Container)) {
+            throw "$_ is not a directory!"
+        }
+        $true
+    })]
+    [String] $Dir = "$psscriptroot\..\bucket",
+    [Switch] $Update,
+    [Switch] $ForceUpdate,
+    [Switch] $SkipUpdated
 )
-
-if (!$app -and $update) {
-    # While developing the feature we only allow specific updates
-    Write-Host "[ERROR] AUTOUPDATE CAN ONLY BE USED WITH A APP SPECIFIED" -f DarkRed
-    exit
-}
 
 . "$psscriptroot\..\lib\core.ps1"
 . "$psscriptroot\..\lib\manifest.ps1"
@@ -23,76 +67,74 @@ if (!$app -and $update) {
 . "$psscriptroot\..\lib\install.ps1" # needed for hash generation
 . "$psscriptroot\..\lib\unix.ps1"
 
-if(!$dir) { $dir = "$psscriptroot\..\bucket" }
-$dir = resolve-path $dir
-
-$search = "*"
-if($app) { $search = $app }
+$Dir = Resolve-Path $Dir
+$Search = $App
 
 # get apps to check
-$queue = @()
-Get-ChildItem $dir "$search.json" | ForEach-Object {
-    $json = parse_json "$dir\$($_.Name)"
-    if($json.checkver) {
-        $queue += ,@($_.Name, $json)
+$Queue = @()
+$json = ''
+Get-ChildItem $Dir "$App.json" | ForEach-Object {
+    $json = parse_json "$Dir\$($_.Name)"
+    if ($json.checkver) {
+        $Queue += , @($_.Name, $json)
     }
 }
 
 # clear any existing events
-get-event | ForEach-Object {
-    remove-event $_.sourceidentifier
+Get-Event | ForEach-Object {
+    Remove-Event $_.SourceIdentifier
 }
 
 $original = use_any_https_protocol
 
 # start all downloads
-$queue | ForEach-Object {
+$Queue | ForEach-Object {
     $name, $json = $_
 
     $substitutions = get_version_substitutions $json.version
 
     $wc = New-Object Net.Webclient
-    if($json.checkver.useragent) {
+    if ($json.checkver.useragent) {
         $wc.Headers.Add('User-Agent', (substitute $json.checkver.useragent $substitutions))
     } else {
         $wc.Headers.Add('User-Agent', (Get-UserAgent))
     }
-    Register-ObjectEvent $wc downloadstringcompleted -ErrorAction stop | Out-Null
+    Register-ObjectEvent $wc downloadstringcompleted -ErrorAction Stop | Out-Null
 
-    $githubRegex = "\/releases\/tag\/(?:v)?([\d.]+)"
+    $githubRegex = '\/releases\/tag\/(?:v)?([\d.]+)'
 
     $url = $json.homepage
-    if($json.checkver.url) {
+    if ($json.checkver.url) {
         $url = $json.checkver.url
     }
-    $regex = ""
-    $jsonpath = ""
-    $replace = ""
+    $regex = ''
+    $jsonpath = ''
+    $replace = ''
 
-    if ($json.checkver -eq "github") {
-        if (!$json.homepage.StartsWith("https://github.com/")) {
+    if ($json.checkver -eq 'github') {
+        if (!$json.homepage.StartsWith('https://github.com/')) {
             error "$name checkver expects the homepage to be a github repository"
         }
-        $url = $json.homepage + "/releases/latest"
+        $url = $json.homepage + '/releases/latest'
         $regex = $githubRegex
     }
 
     if ($json.checkver.github) {
-        $url = $json.checkver.github + "/releases/latest"
+        $url = $json.checkver.github + '/releases/latest'
         $regex = $githubRegex
     }
 
-    if($json.checkver.re) {
+    if ($json.checkver.re) {
         $regex = $json.checkver.re
     }
-    if($json.checkver.regex) {
+    if ($json.checkver.regex) {
         $regex = $json.checkver.regex
     }
 
-    if($json.checkver.jp) {
+    if ($json.checkver.jp) {
         $jsonpath = $json.checkver.jp
     }
-    if($json.checkver.jsonpath) {
+    if ($json.checkver.jsonpath) {
         $jsonpath = $json.checkver.jsonpath
     }
 
@@ -100,136 +142,141 @@ $queue | ForEach-Object {
         $replace = $json.checkver.replace
     }
 
-    if(!$jsonpath -and !$regex) {
+    if (!$jsonpath -and !$regex) {
         $regex = $json.checkver
     }
 
-    $reverse = $json.checkver.reverse -and $json.checkver.reverse -eq "true"
+    $reverse = $json.checkver.reverse -and $json.checkver.reverse -eq 'true'
 
     $url = substitute $url $substitutions
 
-    $state = new-object psobject @{
-        app = (strip_ext $name);
-        url = $url;
-        regex = $regex;
-        json = $json;
+    $state = New-Object psobject @{
+        app      = (strip_ext $name);
+        url      = $url;
+        regex    = $regex;
+        json     = $json;
         jsonpath = $jsonpath;
-        reverse = $reverse;
-        replace = $replace;
+        reverse  = $reverse;
+        replace  = $replace;
     }
 
-    $wc.headers.add('Referer', (strip_filename $url))
-    $wc.downloadstringasync($url, $state)
+    $wc.Headers.Add('Referer', (strip_filename $url))
+    $wc.DownloadStringAsync($url, $state)
+}
+
+function next($er) {
+    Write-Host "$App`: " -NoNewline
+    Write-Host $er -ForegroundColor DarkRed
 }
 
 # wait for all to complete
-$in_progress = $queue.length
-while($in_progress -gt 0) {
-    $ev = wait-event
-    remove-event $ev.sourceidentifier
+$in_progress = $Queue.length
+while ($in_progress -gt 0) {
+    $ev = Wait-Event
+    Remove-Event $ev.SourceIdentifier
     $in_progress--
 
-    $state = $ev.sourceeventargs.userstate
+    $state = $ev.SourceEventArgs.UserState
     $app = $state.app
     $json = $state.json
     $url = $state.url
-    $expected_ver = $json.version
     $regexp = $state.regex
     $jsonpath = $state.jsonpath
     $reverse = $state.reverse
     $replace = $state.replace
-    $ver = ""
+    $expected_ver = $json.version
+    $ver = ''
 
-    $err = $ev.sourceeventargs.error
-    $page = $ev.sourceeventargs.result
+    $err = $ev.SourceEventArgs.Error
+    $page = $ev.SourceEventArgs.Result
 
-    write-host "$app`: " -nonewline
-
-    if($err) {
-        write-host -f darkred $err.message
-        write-host -f darkred "URL $url is not valid"
+    if ($err) {
+        next "$($err.message)`r`nURL $url is not valid"
         continue
     }
 
-    if(!$regex -and $replace) {
-        write-host -f darkred "'replace' requires 're'"
+    if (!$regex -and $replace) {
+        next "'replace' requires 're' or 'regex'"
         continue
     }
 
-    if($jsonpath) {
+    if ($jsonpath) {
         $ver = json_path $page $jsonpath
-        if(!$ver) {
+        if (!$ver) {
             $ver = json_path_legacy $page $jsonpath
         }
-        if(!$ver) {
-            write-host -f darkred "couldn't find '$jsonpath' in $url"
+        if (!$ver) {
+            next "couldn't find '$jsonpath' in $url"
             continue
         }
     }
 
-    if($jsonpath -and $regexp) {
+    if ($jsonpath -and $regexp) {
         $page = $ver
-        $ver = ""
+        $ver = ''
     }
 
-    if($regexp) {
-        $regex = new-object System.Text.RegularExpressions.Regex($regexp)
-        if($reverse) {
-            $match = $regex.matches($page) | select-object -last 1
+    if ($regexp) {
+        $regex = New-Object System.Text.RegularExpressions.Regex($regexp)
+        if ($reverse) {
+            $match = $regex.Matches($page) | Select-Object -Last 1
         } else {
-            $match = $regex.matches($page) | select-object -first 1
+            $match = $regex.Matches($page) | Select-Object -First 1
         }
 
-        if($match -and $match.Success) {
+        if ($match -and $match.Success) {
             $matchesHashtable = @{}
             $regex.GetGroupNames() | ForEach-Object { $matchesHashtable.Add($_, $match.Groups[$_].Value) }
             $ver = $matchesHashtable['1']
             if ($replace) {
-                $ver = $regex.replace($match.Value, $replace)
+                $ver = $regex.Replace($match.Value, $replace)
             }
-            if(!$ver) {
+            if (!$ver) {
                 $ver = $matchesHashtable['version']
             }
         } else {
-            write-host -f darkred "couldn't match '$regexp' in $url"
+            next "couldn't match '$regexp' in $url"
             continue
         }
     }
 
-    if(!$ver) {
-        write-host -f darkred "couldn't find new version in $url"
+    if (!$ver) {
+        next "couldn't find new version in $url"
         continue
     }
 
-    if($ver -eq $expected_ver -and $forceUpdate -eq $false) {
-        # version hasn't changed (step over if forced update)
-        write-host "$ver" -f darkgreen
+    # Skip actual only if versions are same and there is no -f
+    if (($ver -eq $expected_ver) -and !$ForceUpdate -and $SkipUpdated) { continue }
+
+    Write-Host "$App`: " -NoNewline
+
+    # version hasn't changed (step over if forced update)
+    if ($ver -eq $expected_ver -and !$ForceUpdate) {
+        Write-Host $ver -ForegroundColor DarkGreen
         continue
     }
 
-    write-host "$ver" -f darkred -nonewline
-    write-host " (scoop version is $expected_ver)" -NoNewline
+    Write-Host $ver -ForegroundColor DarkRed -NoNewline
+    Write-Host " (scoop version is $expected_ver)" -NoNewline
     $update_available = (compare_versions $expected_ver $ver) -eq -1
 
     if ($json.autoupdate -and $update_available) {
-        Write-Host " autoupdate available" -f Cyan
+        Write-Host ' autoupdate available' -ForegroundColor Cyan
     } else {
-        Write-Host ""
+        Write-Host ''
     }
 
-    if($forceUpdate) {
-        # forcing an update implies updating, right?
-        $update = $true
-    }
+    # forcing an update implies updating, right?
+    if ($ForceUpdate) { $Update = $true }
 
-    if($update -and $json.autoupdate) {
-        if($forceUpdate) {
-            Write-Host "Forcing autoupdate!" -f DarkMagenta
+    if ($Update -and $json.autoupdate) {
+        if ($ForceUpdate) {
+            Write-Host 'Forcing autoupdate!' -ForegroundColor DarkMagenta
         }
         try {
-            autoupdate $app $dir $json $ver $matchesHashtable
+            autoupdate $App $Dir $json $ver $matchesHashtable
         } catch {
-            error $_.exception.message
+            error $_.Exception.Message
         }
     }
 }
