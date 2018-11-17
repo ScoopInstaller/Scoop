36dacba733291159bd8db92a6d2aac92aa3b0c4c
Jakub Čábera <cabera.jakub@gmail.com>
2018-11-17 22:55:00 +0100

    Format first

--- bin/uninstall.ps1
+++ bin/uninstall.ps1
@@ -1,74 +1,85 @@
-param($global)
+<#
+.SYNOPSIS
+    Uninstall ALL scoop applications and scoop itself.
+.PARAMETER global
+    Global applications will be uninstalled.
+#>
+param([bool] $global)
 
-. "$psscriptroot\..\lib\core.ps1"
-. "$psscriptroot\..\lib\install.ps1"
-. "$psscriptroot\..\lib\shortcuts.ps1"
-. "$psscriptroot\..\lib\versions.ps1"
-. "$psscriptroot\..\lib\manifest.ps1"
+. "$PSScriptRoot\..\lib\core.ps1"
+. "$PSScriptRoot\..\lib\install.ps1"
+. "$PSScriptRoot\..\lib\shortcuts.ps1"
+. "$PSScriptRoot\..\lib\versions.ps1"
+. "$PSScriptRoot\..\lib\manifest.ps1"
 
-if($global -and !(is_admin)) {
-    "ERROR: You need admin rights to uninstall globally."; exit 1
+if ($global -and !(is_admin)) {
+	error 'ERROR: You need admin rights to uninstall globally.'
+	exit 1
 }
 
 warn 'This will uninstall Scoop and all the programs that have been installed with Scoop!'
-$yn = read-host 'Are you sure? (yN)'
-if($yn -notlike 'y*') { exit }
+$yn = Read-Host 'Are you sure? (yN)'
+if ($yn -notlike 'y*') { exit }
 
 $errors = $false
+
+# Uninstall given app
 function do_uninstall($app, $global) {
-    $version = current_version $app $global
-    $dir = versiondir $app $version $global
-    $manifest = installed_manifest $app $version $global
-    $install = install_info $app $version $global
-    $architecture = $install.architecture
+	$version = current_version $app $global
+	$dir = versiondir $app $version $global
+	$manifest = installed_manifest $app $version $global
+	$install = install_info $app $version $global
+	$architecture = $install.architecture
 
-    Write-Output "Uninstalling '$app'"
-    run_uninstaller $manifest $architecture $dir
-    rm_shims $manifest $global $architecture
+	Write-Output "Uninstalling '$app'"
+	run_uninstaller $manifest $architecture $dir
+	rm_shims $manifest $global $architecture
 
-    # If a junction was used during install, that will have been used
-    # as the reference directory. Othewise it will just be the version
-    # directory.
-    $refdir = unlink_current (appdir $app $global)
+	# If a junction was used during install, that will have been used
+	# as the reference directory. Othewise it will just be the version
+	# directory.
+	$refdir = unlink_current (appdir $app $global)
 
-    env_rm_path $manifest $refdir $global
-    env_rm $manifest $global
+	env_rm_path $manifest $refdir $global
+	env_rm $manifest $global
 
-    $appdir = appdir $app $global
-    try {
-        Remove-Item -r -force $appdir -ea stop
-    } catch {
-        $errors = $true
-        warn "Couldn't remove $(friendly_path $appdir): $_.exception"
-    }
+	$appdir = appdir $app $global
+	try {
+		Remove-Item $appdir -Recurse -Force -ErrorAction Stop
+	} catch {
+		$errors = $true
+		warn "Couldn't remove $(friendly_path $appdir): $_.Exception"
+	}
 }
+
 function rm_dir($dir) {
-    try {
-        Remove-Item -r -force $dir -ea stop
-    } catch {
-        abort "Couldn't remove $(friendly_path $dir): $_"
-    }
+	try {
+		Remove-Item $dir -Recurse -Force -ErrorAction Stop
+	} catch {
+		abort "Couldn't remove $(friendly_path $dir): $_"
+	}
 }
 
-# run uninstallation for each app if necessary, continuing if there's
+# Run uninstallation for each app if necessary, continuing if there's
 # a problem deleting a directory (which is quite likely)
-if($global) {
-    installed_apps $true | ForEach-Object { # global apps
-        do_uninstall $_ $true
-    }
-}
-installed_apps $false | ForEach-Object { # local apps
-    do_uninstall $_ $false
+if ($global) {
+	installed_apps $true | ForEach-Object { # global apps
+		do_uninstall $_ $true
+	}
 }
 
-if($errors) {
-    abort "Not all apps could be deleted. Try again or restart."
+installed_apps $false | ForEach-Object { # local apps
+	do_uninstall $_ $false
+}
+
+if ($errors) {
+	abort 'Not all apps could be deleted. Try again or restart.'
 }
 
 rm_dir $scoopdir
-if($global) { rm_dir $globaldir }
+if ($global) { rm_dir $globaldir }
 
 remove_from_path (shimdir $false)
-if($global) { remove_from_path (shimdir $true) }
+if ($global) { remove_from_path (shimdir $true) }
 
-success "Scoop has been uninstalled."
+success 'Scoop has been uninstalled.'
