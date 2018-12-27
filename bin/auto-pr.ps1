6f73329fb80fab416d945d2e48025a5aa98b224d
Jakub Čábera <cabera.jakub@gmail.com>
2018-12-27 22:49:06 +0100

    Format

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -26,9 +26,8 @@ param(
     [ValidateScript( {
         if (!($_ -match '^(.*)\/(.*):(.*)$')) {
             throw 'Upstream must be in this format: <user>/<repo>:<branch>'
-        } else {
-            $true
         }
+        $true
     })]
     [String] $Upstream = 'lukesampson/scoop:master',
     [ValidateScript( {
@@ -44,14 +43,14 @@ param(
     [string[]] $SpecialSnowflakes
 )
 
+. "$PSScriptRoot\..\lib\manifest.ps1"
+. "$PSScriptRoot\..\lib\json.ps1"
+. "$PSScriptRoot\..\lib\unix.ps1"
+
 $Dir = Resolve-Path $Dir
 
-. "$psscriptroot\..\lib\manifest.ps1"
-. "$psscriptroot\..\lib\json.ps1"
-. "$psscriptroot\..\lib\unix.ps1"
-
 if ((!$Push -and !$Request) -or $Help) {
-    Write-Host @"
+    Write-Host @'
 Usage: auto-pr.ps1 [OPTION]
 
 Mandatory options:
@@ -62,61 +61,63 @@ Optional options:
   -u,  -upstream                   upstream repository with target branch
                                      only used if -r is set (default: lukesampson/scoop:master)
   -h,  -help
-"@
+'@
     exit 0
 }
 
 if (is_unix) {
     if (!(which hub)) {
-        Write-Host -f yellow "Please install hub ('brew install hub' or visit: https://hub.github.com/)"
+        Write-Host "Please install hub ('brew install hub' or visit: https://hub.github.com/)" -ForegroundColor Yellow
         exit 1
     }
 } else {
     if (!(scoop which hub)) {
-        Write-Host -f yellow "Please install hub 'scoop install hub'"
+        Write-Host "Please install hub 'scoop install hub'" -ForegroundColor Yellow
         exit 1
     }
 }
 
 function execute($cmd) {
-    Write-Host -f Green $cmd
+    Write-Host $cmd -ForegroundColor Green
     $output = Invoke-Expression $cmd
 
     if ($LASTEXITCODE -gt 0) {
         abort "^^^ Error! See above ^^^ (last command: $cmd)"
     }
+
     return $output
 }
 
-function pull_requests($json, [String]$app, [String]$upstream, [String]$manifest) {
+function pull_requests($json, [String] $app, [String] $upstream, [String] $manifest) {
     $version = $json.version
     $homepage = $json.homepage
     $branch = "manifest/$app-$version"
 
-    execute "hub checkout master"
-    Write-Host -f Green "hub rev-parse --verify $branch"
+    execute 'hub checkout master'
+    Write-Host "hub rev-parse --verify $branch" -ForegroundColor Green
     hub rev-parse --verify $branch
 
     if ($LASTEXITCODE -eq 0) {
-        Write-Host -f Yellow "Skipping update $app ($version) ..."
+        Write-Host "Skipping update $app ($version) ..." -ForegroundColor Yellow
         return
     }
 
-    Write-Host -f DarkCyan "Creating update $app ($version) ..."
+    Write-Host "Creating update $app ($version) ..." -ForegroundColor DarkCyan
     execute "hub checkout -b $branch"
     execute "hub add $manifest"
     execute "hub commit -m '${app}: Update to version $version'"
-    Write-Host -f DarkCyan "Pushing update $app ($version) ..."
+    Write-Host "Pushing update $app ($version) ..." -ForegroundColor DarkCyan
     execute "hub push origin $branch"
 
     if ($LASTEXITCODE -gt 0) {
         error "Push failed! (hub push origin $branch)"
-        execute "hub reset"
+        execute 'hub reset'
         return
     }
+
     Start-Sleep 1
-    Write-Host -f DarkCyan "Pull-Request update $app ($version) ..."
-    Write-Host -f green "hub pull-request -m '<msg>' -b '$upstream' -h '$branch'"
+    Write-Host "Pull-Request update $app ($version) ..." -ForegroundColor DarkCyan
+    Write-Host "hub pull-request -m '<msg>' -b '$upstream' -h '$branch'" -ForegroundColor Green
 
     $msg = @"
 $app`: Update to version $version
@@ -124,38 +125,38 @@ $app`: Update to version $version
 Hello lovely humans,
 a new version of [$app]($homepage) is available.
 
-| State | Update :rocket: |
-| :---: | :-------------: |
-| New Version | $version  |
+| State       | Update :rocket: |
+| :---------- | :-------------- |
+| New version | $version        |
 "@
 
     hub pull-request -m "$msg" -b '$upstream' -h '$branch'
     if ($LASTEXITCODE -gt 0) {
-        execute "hub reset"
+        execute 'hub reset'
         abort "Pull Request failed! (hub pull-request -m '${app}: Update to version $version' -b '$upstream' -h '$branch')"
     }
 }
 
-Write-Host -f DarkCyan "Updating ..."
+Write-Host 'Updating ...' -ForegroundColor DarkCyan
 if ($Push) {
-    execute("hub pull origin master")
-    execute "hub checkout master"
+    execute 'hub pull origin master'
+    execute 'hub checkout master'
 } else {
-    execute("hub pull upstream master")
-    execute("hub push origin master")
+    execute 'hub pull upstream master'
+    execute 'hub push origin master'
 }
 
-. "$psscriptroot\checkver.ps1" * -update -dir $Dir
+. "$PSScriptRoot\checkver.ps1" -Dir $Dir -Update
 if ($SpecialSnowflakes) {
-    write-host -f DarkCyan "Forcing update on our special snowflakes: $($SpecialSnowflakes -join ',')"
+    Write-Host "Forcing update on our special snowflakes: $($SpecialSnowflakes -join ',')" -ForegroundColor DarkCyan
     $SpecialSnowflakes -split ',' | ForEach-Object {
-        . "$psscriptroot\checkver.ps1" $_ -update -forceUpdate -dir $Dir
+        . "$PSScriptRoot\checkver.ps1" $_ -Dir $Dir -ForceUpdate
     }
 }
 
 hub diff --name-only | ForEach-Object {
     $manifest = $_
-    if (!$manifest.EndsWith(".json")) {
+    if (!$manifest.EndsWith('.json')) {
         return
     }
 
@@ -168,16 +169,16 @@ hub diff --name-only | ForEach-Object {
     $version = $json.version
 
     if ($Push) {
-        Write-Host -f DarkCyan "Creating update $app ($version) ..."
+        Write-Host "Creating update $app ($version) ..." -ForegroundColor DarkCyan
         execute "hub add $manifest"
 
         # detect if file was staged, because it's not when only LF or CRLF have changed
-        $status = Invoke-Expression "hub status --porcelain -uno"
-        $status = $status | select-object -first 1
+        $status = Invoke-Expression 'hub status --porcelain -uno'
+        $status = $status | Select-Object -First 1
         if ($status -and $status.StartsWith('M  ') -and $status.EndsWith("$app.json")) {
             execute "hub commit -m '${app}: Update to version $version'"
         } else {
-            Write-Host -f Yellow "Skipping $app because only LF/CRLF changes were detected ..."
+            Write-Host "Skipping $app because only LF/CRLF changes were detected ..." -ForegroundColor Yellow
         }
     } else {
         pull_requests $json $app $Upstream $manifest
@@ -185,11 +186,11 @@ hub diff --name-only | ForEach-Object {
 }
 
 if ($Push) {
-    Write-Host -f DarkCyan "Pushing updates ..."
-    execute "hub push origin master"
+    Write-Host 'Pushing updates ...' -ForegroundColor DarkCyan
+    execute 'hub push origin master'
 } else {
-    Write-Host -f DarkCyan "Returning to master branch and removing unstaged files ..."
-    execute "hub checkout -f master"
+    Write-Host 'Returning to master branch and removing unstaged files ...' -ForegroundColor DarkCyan
+    execute 'hub checkout -f master'
 }
 
-execute "hub reset"
+execute 'hub reset'
