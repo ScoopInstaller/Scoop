82acd7e4c770d8932f937a63c3027d7293a18aef
Jakub Čábera <cabera.jakub@gmail.com>
2019-02-02 18:52:08 +0100

    Run checkver

--- bin/update-supportings.ps1
+++ bin/update-supportings.ps1
@@ -5,15 +5,17 @@
 . "$PSScriptRoot\..\lib\config.ps1"
 . "$PSScriptRoot\..\lib\decompress.ps1"
 
-$SUPPORTINGS = Resolve-Path (Get-ChildItem "$PSScriptRoot\..\supporting" -File).FullName
+$SUPPORTINGS = Resolve-Path (Get-ChildItem "$PSScriptRoot\..\supporting\*" -File -Include '*.json').FullName
 
 foreach ($sup in $SUPPORTINGS) {
     $name = ((Split-Path $sup -Leaf) -split '\.')[0]
+    $folder = Split-Path $sup -Parent
+    $dir = "$folder\$name\bin"
 
     Write-Host "Updating $name" -ForegroundColor Magenta
 
+    Invoke-Expression "$PSScriptRoot\checkver.ps1 -App $name -Dir $folder -Update"
     $manifest = parse_json $sup
-    $dir = "$(Split-Path $sup -Parent)\$name\bin"
     if (!(Test-Path $dir)) { New-Item $dir -ItemType Directory | Out-Null }
 
     $fname = dl_urls $name $manifest.version $manifest '' default_architecture $dir $true $true
