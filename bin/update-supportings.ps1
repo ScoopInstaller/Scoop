d0b4ee2a7c8fdb876779d401f6a6cc112f382ff2
Jakub Čábera <cabera.jakub@gmail.com>
2019-02-02 17:36:11 +0100

    Concept of extracting supportings

---
+++ bin/update-supportings.ps1
@@ -0,0 +1,24 @@
+. "$psscriptroot\..\lib\autoupdate.ps1"
+. "$PSScriptRoot\..\lib\core.ps1"
+. "$PSScriptRoot\..\lib\manifest.ps1"
+. "$PSScriptRoot\..\lib\install.ps1"
+. "$PSScriptRoot\..\lib\config.ps1"
+. "$PSScriptRoot\..\lib\decompress.ps1"
+
+$SUPPORTINGS = Resolve-Path (Get-ChildItem "$PSScriptRoot\..\supporting" -File).FullName
+
+foreach ($sup in $SUPPORTINGS) {
+    $name = ((Split-Path $sup -Leaf) -split '\.')[0]
+
+    Write-Host "Updating $name" -ForegroundColor Magenta
+
+    $manifest = parse_json $sup
+    $dir = "$(Split-Path $sup -Parent)\$name\bin"
+    if (!(Tets-Path $dir)) { New-Item $dir -ItemType Directory | Out-Null }
+
+    $fname = dl_urls $name $manifest.version $manifest '' default_architecture $dir $true $true
+    # Pre install es enough now
+    pre_install $manifest $architecture
+
+    Write-Host "$name done" -ForegroundColor Green
+}
