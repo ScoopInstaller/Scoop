6da4ce900086023414d984f5306fdd6fd1d3a86b
Richard Kuhnt <r15ch13+git@gmail.com>
2019-02-03 16:48:20 +0100

    core.ps1: Improve debug() function
    - Enable output by setting $env:SCOOP_DEBUG to true (disabled by default)
    - Converts objects to string output
    - Shows calling position for clickable links in VSCode

--- lib/core.ps1
+++ lib/core.ps1
@@ -60,11 +60,26 @@ function abort($msg, [int] $exit_code=1) { write-host $msg -f red; exit $exit_co
 function error($msg) { write-host "ERROR $msg" -f darkred }
 function warn($msg) {  write-host "WARN  $msg" -f darkyellow }
 function info($msg) {  write-host "INFO  $msg" -f darkgray }
-function debug($msg, $indent = $false) {
-    if($indent) {
-        write-host "    DEBUG $msg" -f darkcyan
+function debug($obj) {
+    if($env:SCOOP_DEBUG -ine 'true') {
+        return
+    }
+
+    $prefix = "DEBUG[$(Get-Date -UFormat %s)]"
+    $param = $MyInvocation.Line.Replace($MyInvocation.InvocationName, '').Trim()
+    $msg = $obj | Out-String -Stream
+
+    if($msg.GetType() -eq [System.Object[]]) {
+        Write-Host "$prefix $param ($($obj.GetType()))" -f DarkCyan -NoNewline
+        Write-Host " -> $($MyInvocation.PSCommandPath):$($MyInvocation.ScriptLineNumber):$($MyInvocation.OffsetInLine)" -f DarkGray
+        $msg | Where-Object { ![String]::IsNullOrWhiteSpace($_) } |
+            Select-Object -Skip 2 | # Skip headers
+            ForEach-Object {
+                Write-Host "$prefix $param.$($_)" -f DarkCyan
+            }
     } else {
-        write-host "DEBUG $msg" -f darkcyan
+        Write-Host "$prefix $param = $($msg.Trim())" -f DarkCyan -NoNewline
+        Write-Host " -> $($MyInvocation.PSCommandPath):$($MyInvocation.ScriptLineNumber):$($MyInvocation.OffsetInLine)" -f DarkGray
     }
 }
 function success($msg) { write-host $msg -f darkgreen }
