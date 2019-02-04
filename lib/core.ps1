8213fa3d694ee740a245793ec3357d214ff0d6dd
Richard Kuhnt <r15ch13+git@gmail.com>
2019-02-04 10:09:25 +0100

    core.ps1: Handle $null values in debug()

--- lib/core.ps1
+++ lib/core.ps1
@@ -69,6 +69,13 @@ function debug($obj) {
     $param = $MyInvocation.Line.Replace($MyInvocation.InvocationName, '').Trim()
     $msg = $obj | Out-String -Stream
 
+    if($null -eq $obj -or $null -eq $msg) {
+        Write-Host "$prefix $param = " -f DarkCyan -NoNewline
+        Write-Host '$null' -f DarkYellow -NoNewline
+        Write-Host " -> $($MyInvocation.PSCommandPath):$($MyInvocation.ScriptLineNumber):$($MyInvocation.OffsetInLine)" -f DarkGray
+        return
+    }
+
     if($msg.GetType() -eq [System.Object[]]) {
         Write-Host "$prefix $param ($($obj.GetType()))" -f DarkCyan -NoNewline
         Write-Host " -> $($MyInvocation.PSCommandPath):$($MyInvocation.ScriptLineNumber):$($MyInvocation.OffsetInLine)" -f DarkGray
