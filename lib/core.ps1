e66f97c655a6748855f97ba030d24541bf8b8410
Jan Piotrowski <piotrowski+github@gmail.com>
2019-02-14 16:15:48 +0100

    Allow uninstall of apps with underscore
    Based on https://github.com/lukesampson/scoop/issues/3128#issuecomment-463662715
    
    closes #3128

--- lib/core.ps1
+++ lib/core.ps1
@@ -610,7 +610,7 @@ function applist($apps, $global) {
 }
 
 function parse_app([string] $app) {
-    if($app -match '(?:(?<bucket>[a-zA-Z0-9-]+)\/)?(?<app>.*.json$|[a-zA-Z0-9-.]+)(?:@(?<version>.*))?') {
+    if($app -match '(?:(?<bucket>[a-zA-Z0-9-]+)\/)?(?<app>.*.json$|[a-zA-Z0-9-_.]+)(?:@(?<version>.*))?') {
         return $matches['app'], $matches['bucket'], $matches['version']
     }
     return $app, $null, $null
