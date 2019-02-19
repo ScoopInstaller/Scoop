bfea003e034a5221b4cbaa5abb170b0f00760460
Jakub Čábera <cabera.jakub@gmail.com>
2019-02-19 18:59:59 +0100

    Correct bucket URL for new issues

--- lib/buckets.ps1
+++ lib/buckets.ps1
@@ -105,19 +105,32 @@ function rm_bucket($name) {
 function new_issue_msg($app, $bucket, $title, $body) {
     $app, $manifest, $bucket, $url = locate $app $bucket
     $url = known_bucket_repo $bucket
-    if($manifest -and $null -eq $url -and $null -eq $bucket) {
+    $bucket_path = "$bucketsdir\$bucket"
+
+    if($manifest -and ($null -eq $url) -and ($null -eq $bucket)) {
         $url = 'https://github.com/lukesampson/scoop'
-    }
-    if(!$url) {
-        return "Please contact the bucket maintainer!"
+    } elseif (Test-path $bucket_path) {
+        Push-Location $bucket_path
+        $remote = git_config --get remote.origin.url
+        # Support ssh and http syntax
+        # git@PROVIDER:USER/REPO.git
+        # https://PROVIDER/USER/REPO.git
+        $remote -match '(@|:\/\/)(?<provider>.+)[:/](?<user>.*)\/(?<repo>.*)(\.git)?$' | Out-Null
+        $url = "https://$($Matches.Provider)/$($Matches.User)/$($Matches.Repo)"
+        Pop-Location
     }
 
-    $title = [System.Web.HttpUtility]::UrlEncode("$app@$($manifest.version): $title")
-    $body = [System.Web.HttpUtility]::UrlEncode($body)
-    $url = $url -replace '^(.*).git$','$1'
-    $url = "$url/issues/new?title=$title"
-    if($body) {
-        $url += "&body=$body"
+    if(!$url) { return 'Please contact the bucket maintainer!' }
+
+    # Print only github repositories
+    if ($url -like '*github*') {
+        $title = [System.Web.HttpUtility]::UrlEncode("$app@$($manifest.version): $title")
+        $body = [System.Web.HttpUtility]::UrlEncode($body)
+        $url = $url -replace '\.git$', ''
+        $url = "$url/issues/new?title=$title"
+        if($body) {
+            $url += "&body=$body"
+        }
     }
 
     $msg = "`nPlease try again or create a new issue by using the following link and paste your console output:"
