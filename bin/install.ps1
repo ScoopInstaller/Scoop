38e346d3801867c9e48fa409ae868d2f703ffcef
Richard Kuhnt <r15ch13+git@gmail.com>
2019-02-19 22:08:11 +0100

    install.ps1: replace raw.github.com with raw.githubusercontent.com
    closes #3142

--- bin/install.ps1
+++ bin/install.ps1
@@ -27,7 +27,7 @@ if([System.Enum]::GetNames([System.Net.SecurityProtocolType]) -notcontains 'Tls1
 }
 
 # get core functions
-$core_url = 'https://raw.github.com/lukesampson/scoop/master/lib/core.ps1'
+$core_url = 'https://raw.githubusercontent.com/lukesampson/scoop/master/lib/core.ps1'
 Write-Output 'Initializing...'
 Invoke-Expression (new-object net.webclient).downloadstring($core_url)
 
