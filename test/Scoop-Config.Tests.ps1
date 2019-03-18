f7a9cd9ceb5e6736c2f0058a5a8c5b58279b8980
Richard Kuhnt <r15ch13+git@gmail.com>
2019-03-18 15:50:31 +0100

    refactor(config): Move configuration handling to core.ps1
    - Migrate ~\.scoop to ~\.config\scoop\config.json
    - Remove hashtable and hashtable_val functions because ConvertFrom-Json is enough
    - Add PowerShell 6 time conversion to tests

--- test/Scoop-Config.Tests.ps1
+++ test/Scoop-Config.Tests.ps1
@@ -1,20 +1,19 @@
 . "$psscriptroot\..\lib\core.ps1"
-. "$psscriptroot\..\lib\config.ps1"
 
 describe "hashtable" -Tag 'Scoop' {
-    $json = '{ "one": 1, "two": [ { "a": "a" }, "b", 2 ], "three": { "four": 4 }, "five": true, "six": false, "seven": "\/Date(1529917395805)\/" }'
+    $json = '{ "one": 1, "two": [ { "a": "a" }, "b", 2 ], "three": { "four": 4 }, "five": true, "six": false, "seven": "\/Date(1529917395805)\/", "eight": "2019-03-18T15:22:09.3930000+01:00" }'
 
     it "converts pscustomobject to hashtable" {
-        $obj = convertfrom-json $json
-        $ht = hashtable $obj
+        $obj = ConvertFrom-Json $json
 
-        $ht.one | should -beexactly 1
-        $ht.two[0].a | should -be "a"
-        $ht.two[1] | should -be "b"
-        $ht.two[2] | should -beexactly 2
-        $ht.three.four | should -beexactly 4
-        $ht.five | should -beexactly $true
-        $ht.six | should -beexactly $false
-        [System.DateTime]::Equals($ht.seven, $(New-Object System.DateTime (2018, 06, 25, 09, 03, 15, 805))) | should -betrue
+        $obj.one | should -beexactly 1
+        $obj.two[0].a | should -be "a"
+        $obj.two[1] | should -be "b"
+        $obj.two[2] | should -beexactly 2
+        $obj.three.four | should -beexactly 4
+        $obj.five | should -beexactly $true
+        $obj.six | should -beexactly $false
+        [System.DateTime]::Equals($obj.seven, $(New-Object System.DateTime (2018, 06, 25, 09, 03, 15, 805))) | should -betrue
+        [System.DateTime]::Equals([System.DateTime]::parse($obj.eight), $(New-Object System.DateTime (2019, 03, 18, 15, 22, 09, 393))) | should -betrue
     }
 }
