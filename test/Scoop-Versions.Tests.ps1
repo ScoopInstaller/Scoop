be617e9f78df4641ca413c5ce05868d9520ecb01
Hsiao-nan Cheung <niheaven@gmail.com>
2019-04-19 00:14:28 +0800

    fix(versions): Refactor `version` and `compare_versions` to `Compare-Version`
    Force install manifest version

--- test/Scoop-Versions.Tests.ps1
+++ test/Scoop-Versions.Tests.ps1
@@ -3,35 +3,39 @@
 
 describe "versions" -Tag 'Scoop' {
     it 'compares versions with integer-string mismatch' {
-        $a = '1.8.9'
-        $b = '1.8.5-1'
-        $res = compare_versions $a $b
-
-        $res | should -be 1
+        Compare-Version '1.8.9' '1.8.5-1' | Should -Be -1
     }
 
     it 'handles plain string version comparison to int version' {
-        $a = 'latest'
-        $b = '20150405'
-        $res = compare_versions $a $b
-
-        $res | should -be 1
+        Compare-Version 'latest' '20150405' | Should -Be -1
     }
 
     it 'handles dashed version components' {
-        $a = '7.0.4-9'
-        $b = '7.0.4-10'
+        Compare-Version '7.0.4-9' '7.0.4-10' | Should -Be 1
+        Compare-Version '7.0.4' '7.0.4-9' | Should -Be 1
+        Compare-Version '7.0.4-9' '7.0.4-8' | Should -Be -1
+    }
 
-        $res = compare_versions $a $b
-
-        $res | should -be -1
+    it 'handle example comparisons' {
+        Compare-Version '1' '1.1' | Should -Be 1
+        Compare-Version '1.0' '1.1' | Should -Be 1
+        Compare-Version '1.1' '1.0' | Should -Be -1
+        Compare-Version '1.1' '1' | Should -Be -1
+        Compare-Version '1.1.1_8' '1.1.1' | Should -Be -1
+        Compare-Version '1.1.1b' '1.1.1a' | Should -Be -1
+        Compare-Version '1.1.1a' '1.1.1b' | Should -Be 1
+        Compare-Version '2019-01-01' '2019-01-02' | Should -Be 1
+        Compare-Version '2019-01-02' '2019-01-01' | Should -Be -1
+        Compare-Version '2018-01-01' '2019-01-01' | Should -Be 1
+        Compare-Version '2019-01-01' '2018-01-01' | Should -Be -1
     }
 
     it 'handles comparsion against en empty string' {
-        compare_versions '7.0.4-9' '' | should -be 1
+        Compare-Version '7.0.4-9' '' | Should -Be -1
     }
 
     it 'handles equal versions' {
-        compare_versions '12.0' '12.0' | should -be 0
+        Compare-Version '12.0' '12.0' | Should -Be 0
+        Compare-Version '7.0.4-9' '7.0.4-9' | Should -Be 0
     }
 }
