e252a910c586b17e129d63991a6807a07342ca22
Jakub Čábera <cabera.jakub@gmail.com>
2019-04-25 23:50:44 +0200

    wording

--- lib/diagnostic.ps1
+++ lib/diagnostic.ps1
@@ -42,7 +42,7 @@ function check_main_bucket {
 function check_long_paths {
     $key = Get-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -ErrorAction SilentlyContinue -Name 'LongPathsEnabled'
     if (!$key -or ($key.LongPathsEnabled -eq 0)) {
-        warn 'LongPaths settings is not enabled.'
+        warn 'LongPaths support is not enabled.'
         Write-Host "You can enable it with running:"
         Write-Host "    Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1"
 
