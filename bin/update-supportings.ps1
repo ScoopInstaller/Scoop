8fc95ae120144d902a4fa22033302a24c070b946
Jakub Čábera <cabera.jakub@gmail.com>
2019-05-01 19:43:20 +0200

    Add Supporting parameter

--- bin/update-supportings.ps1
+++ bin/update-supportings.ps1
@@ -1,13 +1,19 @@
-. "$psscriptroot\..\lib\autoupdate.ps1"
-. "$PSScriptRoot\..\lib\core.ps1"
-. "$PSScriptRoot\..\lib\manifest.ps1"
-. "$PSScriptRoot\..\lib\install.ps1"
+<#
+.SYNOPSIS
+    Update supporting tools to the latest version.
+.PARAMETER Supporting
+    Name of supporting tool to be updated.
+#>
+param([String] $Supporting = '*')
+
 . "$PSScriptRoot\..\lib\config.ps1"
 . "$PSScriptRoot\..\lib\decompress.ps1"
+. "$PSScriptRoot\..\lib\manifest.ps1"
+. "$PSScriptRoot\..\lib\install.ps1"
 
-$SUPPORTINGS = Resolve-Path (Get-ChildItem "$PSScriptRoot\..\supporting\*" -File -Include '*.json').FullName
+$Sups = (Get-ChildItem "$PSScriptRoot\..\supporting\*" -File -Include "$Supporting.json").FullName
 
-foreach ($sup in $SUPPORTINGS) {
+foreach ($sup in $Sups) {
     $name = ((Split-Path $sup -Leaf) -split '\.')[0]
     $folder = Split-Path $sup -Parent
     $dir = "$folder\$name\bin"
