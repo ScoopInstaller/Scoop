e260727af614d19efdaced5945635d42ec43740d
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-05-08 09:25:52 -0400

    remove lib imports from scoop-cleanup.ps1

--- libexec/scoop-cleanup.ps1
+++ libexec/scoop-cleanup.ps1
@@ -9,14 +9,6 @@
 #   -g, --global       Cleanup a globally installed app
 #   -k, --cache        Remove outdated download cache
 
-. "$psscriptroot\..\lib\core.ps1"
-. "$psscriptroot\..\lib\manifest.ps1"
-. "$psscriptroot\..\lib\buckets.ps1"
-. "$psscriptroot\..\lib\versions.ps1"
-. "$psscriptroot\..\lib\getopt.ps1"
-. "$psscriptroot\..\lib\help.ps1"
-. "$psscriptroot\..\lib\install.ps1"
-
 reset_aliases
 
 $opt, $apps, $err = getopt $args 'gk' 'global', 'cache'
