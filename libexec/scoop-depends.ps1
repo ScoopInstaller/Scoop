7a7f9ad4cefa48ba4d81948cabe89a028503fda7
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-05-08 09:28:55 -0400

    remove lib imports from scoop-depends

--- libexec/scoop-depends.ps1
+++ libexec/scoop-depends.ps1
@@ -1,15 +1,6 @@
 # Usage: scoop depends <app>
 # Summary: List dependencies for an app
 
-. "$psscriptroot\..\lib\depends.ps1"
-. "$psscriptroot\..\lib\install.ps1"
-. "$psscriptroot\..\lib\manifest.ps1"
-. "$psscriptroot\..\lib\buckets.ps1"
-. "$psscriptroot\..\lib\getopt.ps1"
-. "$psscriptroot\..\lib\decompress.ps1"
-. "$psscriptroot\..\lib\config.ps1"
-. "$psscriptroot\..\lib\help.ps1"
-
 reset_aliases
 
 $opt, $apps, $err = getopt $args 'a:' 'arch='
