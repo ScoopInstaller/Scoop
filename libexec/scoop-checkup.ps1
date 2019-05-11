f578cd0767d95cdfa1a3f109ecfc270374add53b
Richard Kuhnt <r15ch13+git@gmail.com>
2019-05-11 22:15:23 +0200

    feature(checkup): Add warnings to 'scoop checkup' for missing unpackers

--- libexec/scoop-checkup.ps1
+++ libexec/scoop-checkup.ps1
@@ -13,6 +13,30 @@ $issues += !(check_windows_defender $true)
 $issues += !(check_main_bucket)
 $issues += !(check_long_paths)
 
+if (!(Test-HelperInstalled -Helper 7zip)) {
+    error "'7-Zip' is not installed! It's required for unpacking most programs. Please Run 'scoop install 7zip' or 'scoop install 7zip-zstd'."
+    $issues++
+}
+
+if (!(Test-HelperInstalled -Helper Lessmsi)) {
+    error "'lessmsi' is not installed! It's required for unpacking programs with MSI packages. Please run 'scoop install lessmsi'."
+    $issues++
+}
+
+if (!(Test-HelperInstalled -Helper Innounp)) {
+    error "'Inno Setup Unpacker' is not installed! It's required for unpacking InnoSetup files. Please run 'scoop install innounp'."
+    $issues++
+}
+
+if (!(Test-HelperInstalled -Helper Dark)) {
+    error "'dark' is not installed! It's require for unpacking installers created with the WiX Toolset. Please run 'scoop install dark' or 'scoop install wixtoolset'."
+    $issues++
+}
+
+if (!(Test-HelperInstalled -Helper Aria2)) {
+    warn "'aria2' is not installed! This can improve download speeds. You may run 'scoop install aria2'."
+}
+
 $globaldir = New-Object System.IO.DriveInfo($globaldir)
 if($globaldir.DriveFormat -ne 'NTFS') {
     error "Scoop requires an NTFS volume to work! Please point `$env:SCOOP_GLOBAL or 'globalPath' variable in '~/.config/scoop/config.json' to another Drive."
