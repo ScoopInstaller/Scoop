d10207fe303596cc4662bff5c9e20d5e923f7a52
luckynum7 <huomian@outlook.com>
2019-05-15 07:58:33 +0800

    Fix config file path in comment
    config file moved by r15ch13@f7a9cd9ceb5e6736c2f0058a5a8c5b58279b8980

--- libexec/scoop-config.ps1
+++ libexec/scoop-config.ps1
@@ -1,6 +1,6 @@
 # Usage: scoop config [rm] name [value]
 # Summary: Get or set configuration values
-# Help: The scoop configuration file is saved at ~/.scoop.
+# Help: The scoop configuration file is saved at ~/.config/scoop/config.json.
 #
 # To get a configuration setting:
 #
