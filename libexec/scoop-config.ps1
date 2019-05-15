ad6198eeb4e4d5d5934a4c3b8a8b817f2f1da453
Mark Huo <luckynum7@users.noreply.github.com>
2019-05-16 00:08:08 +0800

    fix(config): show correct output when removing a config value (#3462)

--- libexec/scoop-config.ps1
+++ libexec/scoop-config.ps1
@@ -1,6 +1,6 @@
 # Usage: scoop config [rm] name [value]
 # Summary: Get or set configuration values
-# Help: The scoop configuration file is saved at ~/.scoop.
+# Help: The scoop configuration file is saved at ~/.config/scoop/config.json.
 #
 # To get a configuration setting:
 #
@@ -37,7 +37,7 @@ if(!$name) { my_usage; exit 1 }
 
 if($name -like 'rm') {
     set_config $value $null | Out-Null
-    Write-Output "'$name' has been removed"
+    Write-Output "'$value' has been removed"
 } elseif($null -ne $value) {
     set_config $name $value | Out-Null
     Write-Output "'$name' has been set to '$value'"
