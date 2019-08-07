e453418a3d8abcced99cd90d1439c42219c72ad4
Jakub Čábera <cabera.jakub@gmail.com>
2019-08-07 12:28:57 +0200

    spaces

--- lib/ManifestHelpers.psm1
+++ lib/ManifestHelpers.psm1
@@ -1,5 +1,5 @@
 function Test-Persistence {
-	<#
+    <#
     .SYNOPSIS
         Persistence check helper.
     .DESCRIPTION
@@ -12,31 +12,29 @@ function Test-Persistence {
         Custom scriptblock to run when file is not persisted.
         https://github.com/lukesampson/scoop-extras/blob/a84b257fd9636d02295b48c3fd32826487ca9bd3/bucket/ditto.json#L25-L33
     #>
-	param(
-		[CmdletBinding(DefaultParameterSetName = 'Content')]
-		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-		[String[]] $File,
-        [Parameter(ParameterSetName = 'Content')]
-		[Object[]] $Content,
-        [Parameter(ParameterSetName = 'Execution')]
-		[ScriptBlock] $Execution
-	)
+    param(
+        [CmdletBinding(DefaultParameterSetName = 'Content')]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [String[]] $File,
+        [Object[]] $Content,
+        [ScriptBlock] $Execution
+    )
 
-	for ($ind = 0; $ind -lt $File.Count; ++$ind) {
-		$f = $File[$ind]
-		$cont = $Content[$ind] # $null when there is none specified on this index
-		# PWSH has UTF-8 without BOM while, <=5 is using UTF-8 with bom
-		$enc = if ($PSVersionTable.PSVersion.Major -ge 6) { 'UTF-8' } else { 'ASCII' }
+    for ($ind = 0; $ind -lt $File.Count; ++$ind) {
+        $f = $File[$ind]
+        $cont = $Content[$ind] # $null when there is none specified on this index
+        # PWSH has UTF-8 without BOM while, <=5 is using UTF-8 with bom
+        $enc = if ($PSVersionTable.PSVersion.Major -ge 6) { 'UTF-8' } else { 'ASCII' }
 
-		if (-not (Test-Path (Join-Path $persist_dir $f))) {
-			if ($Execution) {
-				& $Execution
-			} else {
-				Set-Content (Join-Path $dir $f) -ItemType File -Value $cont -Encoding $enc | Out-Null
-				New-Item -Path $dir -Name $f -ItemType File -Value $cont | Out-Null
-			}
-		}
-	}
+        if (-not (Test-Path (Join-Path $persist_dir $f))) {
+            if ($Execution) {
+                & $Execution
+            } else {
+                Set-Content (Join-Path $dir $f) -ItemType File -Value $cont -Encoding $enc | Out-Null
+                New-Item -Path $dir -Name $f -ItemType File -Value $cont | Out-Null
+            }
+        }
+    }
 }
 
 Export-ModuleMember -Function Test-Persistence
