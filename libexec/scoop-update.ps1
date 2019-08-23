518a93ed54496163274c6de089ba6c612b62e2bd
Jakub Čábera <cabera.jakub@gmail.com>
2019-08-23 19:41:16 +0200

    Update libexec/scoop-update.ps1
    Co-Authored-By: Richard Kuhnt <r15ch13+git@gmail.com>

--- libexec/scoop-update.ps1
+++ libexec/scoop-update.ps1
@@ -240,7 +240,7 @@ function update($app, $global, $quiet = $false, $independent, $suggested, $use_c
     #region Workaround for #2952
     # Split-path for getting $env:SCOOP\apps\<app> and not current or specific version
     if (Get-Process | Where-Object { $_.Path -like "$(Split-Path $dir)\*" }) {
-        error "Aplication is still running. Close all instances and try again."
+        error "Application is still running. Close all instances and try again."
         return
     }
     #endregion Workaround for #2952
