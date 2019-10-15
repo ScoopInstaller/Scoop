09f44a404dbabb45bc4609a8d9f7923d10fc511f
Hsiao-nan Cheung <niheaven@gmail.com>
2019-10-15 13:53:14 +0800

    fix(versions): Refactor `compare_versions`

--- test/Scoop-Versions.Tests.ps1
+++ test/Scoop-Versions.Tests.ps1
@@ -2,36 +2,44 @@
 . "$psscriptroot\..\lib\versions.ps1"
 
 describe "versions" -Tag 'Scoop' {
-    it 'compares versions with integer-string mismatch' {
-        $a = '1.8.9'
-        $b = '1.8.5-1'
-        $res = compare_versions $a $b
-
-        $res | should -be 1
+    it 'handles versions with integer-string mismatch' {
+        Compare-Version '1' '1.1' | Should -Be 1
+        Compare-Version '1.0' '1.1' | Should -Be 1
+        Compare-Version '1.9.8' '1.10.0' | Should -Be 1
+        Compare-Version '1.1' '1.0' | Should -Be -1
+        Compare-Version '1.1' '1' | Should -Be -1
+        Compare-Version '1.10.0' '1.9.8' | Should -Be -1
+        Compare-Version '1.1.1_8' '1.1.1' | Should -Be -1
+        Compare-Version '1.1.1b' '1.1.1a' | Should -Be -1
+        Compare-Version '1.1.1a' '1.1.1b' | Should -Be 1
     }
 
     it 'handles plain string version comparison to int version' {
-        $a = 'latest'
-        $b = '20150405'
-        $res = compare_versions $a $b
-
-        $res | should -be 1
+        Compare-Version 'latest' '20150405' | Should -Be -1
+        Compare-Version '0.5alpha' '0.5' | Should -Be 1
+        Compare-Version '0.5' '0.5Beta' | Should -Be -1
     }
 
     it 'handles dashed version components' {
-        $a = '7.0.4-9'
-        $b = '7.0.4-10'
-
-        $res = compare_versions $a $b
-
-        $res | should -be -1
+        Compare-Version '1.8.9' '1.8.5-1' | Should -Be -1
+        Compare-Version '7.0.4-9' '7.0.4-10' | Should -Be 1
+        Compare-Version '7.0.4' '7.0.4-9' | Should -Be 1
+        Compare-Version '7.0.4-beta9' '7.0.4' | Should -Be 1
+        Compare-Version '7.0.4-9' '7.0.4-8' | Should -Be -1
+        Compare-Version '7.0.4' '7.0.4-beta9' | Should -Be -1
+        Compare-Version '2019-01-01' '2019-01-02' | Should -Be 1
+        Compare-Version '2019-01-02' '2019-01-01' | Should -Be -1
+        Compare-Version '2018-01-01' '2019-01-01' | Should -Be 1
+        Compare-Version '2019-01-01' '2018-01-01' | Should -Be -1
     }
 
     it 'handles comparsion against en empty string' {
-        compare_versions '7.0.4-9' '' | should -be 1
+        Compare-Version '7.0.4-9' '' | Should -Be -1
     }
 
     it 'handles equal versions' {
-        compare_versions '12.0' '12.0' | should -be 0
+        Compare-Version '12.0' '12.0' | Should -Be 0
+        Compare-Version '7.0.4-9' '7.0.4-9' | Should -Be 0
+        Compare-Version 'nightly-20190801' 'nightly' | Should -Be 0
     }
 }
