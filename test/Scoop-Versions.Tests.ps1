d14bcb34f577159797cb127d09177ecff3d960a1
Hsiao-nan Cheung <niheaven@gmail.com>
2019-11-18 18:29:57 +0800

    Using single quotes in test

--- test/Scoop-Versions.Tests.ps1
+++ test/Scoop-Versions.Tests.ps1
@@ -1,55 +1,55 @@
 . "$psscriptroot\Scoop-TestLib.ps1"
 . "$psscriptroot\..\lib\versions.ps1"
 
-Describe "versions comparison" -Tag 'Scoop' {
+Describe 'versions comparison' -Tag 'Scoop' {
     Context 'semver compliant versions' {
         It 'handles major.minor.patch progressing' {
-            Compare-Version "0.1.0" "0.1.1" | Should -Be 1
-            Compare-Version "0.1.1" "0.2.0" | Should -Be 1
-            Compare-Version "0.2.0" "1.0.0" | Should -Be 1
+            Compare-Version '0.1.0' '0.1.1' | Should -Be 1
+            Compare-Version '0.1.1' '0.2.0' | Should -Be 1
+            Compare-Version '0.2.0' '1.0.0' | Should -Be 1
         }
 
         It 'handles pre-release versioning progression' {
-            Compare-Version "0.4.0" "0.5.0-alpha.1" | Should -Be 1
-            Compare-Version "0.5.0-alpha.1" "0.5.0-alpha.2" | Should -Be 1
-            Compare-Version "0.5.0-alpha.2" "0.5.0-alpha.10" | Should -Be 1
-            Compare-Version "0.5.0-alpha.10" "0.5.0-beta" | Should -Be 1
-            Compare-Version "0.5.0-beta" "0.5.0-alpha.10" | Should -Be -1
-            Compare-Version "0.5.0-beta" "0.5.0-beta.0" | Should -Be 1
+            Compare-Version '0.4.0' '0.5.0-alpha.1' | Should -Be 1
+            Compare-Version '0.5.0-alpha.1' '0.5.0-alpha.2' | Should -Be 1
+            Compare-Version '0.5.0-alpha.2' '0.5.0-alpha.10' | Should -Be 1
+            Compare-Version '0.5.0-alpha.10' '0.5.0-beta' | Should -Be 1
+            Compare-Version '0.5.0-beta' '0.5.0-alpha.10' | Should -Be -1
+            Compare-Version '0.5.0-beta' '0.5.0-beta.0' | Should -Be 1
         }
 
         It 'handles the pre-release tags in an alphabetic order' {
-            Compare-Version "0.5.0-rc.1" "0.5.0-z" | Should -Be 1
-            Compare-Version "0.5.0-rc.1" "0.5.0-howdy" | Should -Be -1
-            Compare-Version "0.5.0-howdy" "0.5.0-rc.1" | Should -Be 1
+            Compare-Version '0.5.0-rc.1' '0.5.0-z' | Should -Be 1
+            Compare-Version '0.5.0-rc.1' '0.5.0-howdy' | Should -Be -1
+            Compare-Version '0.5.0-howdy' '0.5.0-rc.1' | Should -Be 1
         }
     }
 
     Context 'semver semi-compliant versions' {
         It 'handles Windows-styled major.minor.patch.build progression' {
-            Compare-Version "0.0.0.0" "0.0.0.1" | Should -Be 1
-            Compare-Version "0.0.0.1" "0.0.0.2" | Should -Be 1
-            Compare-Version "0.0.0.2" "0.0.1.0" | Should -Be 1
-            Compare-Version "0.0.1.0" "0.0.1.1" | Should -Be 1
-            Compare-Version "0.0.1.1" "0.0.1.2" | Should -Be 1
-            Compare-Version "0.0.1.2" "0.0.2.0" | Should -Be 1
-            Compare-Version "0.0.2.0" "0.1.0.0" | Should -Be 1
-            Compare-Version "0.1.0.0" "0.1.0.1" | Should -Be 1
-            Compare-Version "0.1.0.1" "0.1.0.2" | Should -Be 1
-            Compare-Version "0.1.0.2" "0.1.1.0" | Should -Be 1
-            Compare-Version "0.1.1.0" "0.1.1.1" | Should -Be 1
-            Compare-Version "0.1.1.1" "0.1.1.2" | Should -Be 1
-            Compare-Version "0.1.1.2" "0.2.0.0" | Should -Be 1
-            Compare-Version "0.2.0.0" "1.0.0.0" | Should -Be 1
+            Compare-Version '0.0.0.0' '0.0.0.1' | Should -Be 1
+            Compare-Version '0.0.0.1' '0.0.0.2' | Should -Be 1
+            Compare-Version '0.0.0.2' '0.0.1.0' | Should -Be 1
+            Compare-Version '0.0.1.0' '0.0.1.1' | Should -Be 1
+            Compare-Version '0.0.1.1' '0.0.1.2' | Should -Be 1
+            Compare-Version '0.0.1.2' '0.0.2.0' | Should -Be 1
+            Compare-Version '0.0.2.0' '0.1.0.0' | Should -Be 1
+            Compare-Version '0.1.0.0' '0.1.0.1' | Should -Be 1
+            Compare-Version '0.1.0.1' '0.1.0.2' | Should -Be 1
+            Compare-Version '0.1.0.2' '0.1.1.0' | Should -Be 1
+            Compare-Version '0.1.1.0' '0.1.1.1' | Should -Be 1
+            Compare-Version '0.1.1.1' '0.1.1.2' | Should -Be 1
+            Compare-Version '0.1.1.2' '0.2.0.0' | Should -Be 1
+            Compare-Version '0.2.0.0' '1.0.0.0' | Should -Be 1
         }
 
         It 'handles partial semver version differences' {
-            Compare-Version "1" "1.1" | Should -Be 1
-            Compare-Version "1" "1.0" | Should -Be 1
-            Compare-Version "1.1.0.0" "1.1" | Should -Be -1
-            Compare-Version "1.4" "1.3.0" | Should -Be -1
-            Compare-Version "1.4" "1.3.255.255" | Should -Be -1
-            Compare-Version "1.4" "1.4.4" | Should -Be 1
+            Compare-Version '1' '1.1' | Should -Be 1
+            Compare-Version '1' '1.0' | Should -Be 1
+            Compare-Version '1.1.0.0' '1.1' | Should -Be -1
+            Compare-Version '1.4' '1.3.0' | Should -Be -1
+            Compare-Version '1.4' '1.3.255.255' | Should -Be -1
+            Compare-Version '1.4' '1.4.4' | Should -Be 1
             Compare-Version '1.1.1_8' '1.1.1' | Should -Be -1
             Compare-Version '1.1.1b' '1.1.1a' | Should -Be -1
             Compare-Version '1.1.1a' '1.1.1b' | Should -Be 1
@@ -65,11 +65,11 @@ Describe "versions comparison" -Tag 'Scoop' {
             Compare-Version '2019-01-01' '2018-01-01' | Should -Be -1
         }
         It 'handles post-release tagging ("+")' {
-            Compare-Version "1" "1+hotfix.0" | Should -Be 1
-            Compare-Version "1.0.0" "1.0.0+hotfix.0" | Should -Be 1
-            Compare-Version "1.0.0+hotfix.0" "1.0.0+hotfix.1" | Should -Be 1
-            Compare-Version "1.0.0+hotfix.1" "1.0.1" | Should -Be 1
-            Compare-Version "1.0.0+1.1" "1.0.0+1" | Should -Be -1
+            Compare-Version '1' '1+hotfix.0' | Should -Be 1
+            Compare-Version '1.0.0' '1.0.0+hotfix.0' | Should -Be 1
+            Compare-Version '1.0.0+hotfix.0' '1.0.0+hotfix.1' | Should -Be 1
+            Compare-Version '1.0.0+hotfix.1' '1.0.1' | Should -Be 1
+            Compare-Version '1.0.0+1.1' '1.0.0+1' | Should -Be -1
         }
     }
 
