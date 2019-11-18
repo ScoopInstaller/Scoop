cde2800d05bb1459b883d19578e7acdba5a12b93
linsui <linsui555@gmail.com>
2019-11-18 13:46:27 +0800

    support symlink in movedir

--- lib/core.ps1
+++ lib/core.ps1
@@ -496,7 +496,7 @@ function movedir($from, $to) {
 
     $proc = New-Object System.Diagnostics.Process
     $proc.StartInfo.FileName = 'robocopy.exe'
-    $proc.StartInfo.Arguments = "`"$from`" `"$to`" /e /move"
+    $proc.StartInfo.Arguments = "`"$from`" `"$to`" /e /move /sl"
     $proc.StartInfo.RedirectStandardOutput = $true
     $proc.StartInfo.RedirectStandardError = $true
     $proc.StartInfo.UseShellExecute = $false
