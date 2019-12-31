df648ec174a84e98466786a93e68a71cfef7e569
Jakub Čábera <cabera.jakub@gmail.com>
2019-12-31 15:10:55 +0100

    Cleanup

--- lib/ManifestHelpers.ps1
+++ lib/ManifestHelpers.ps1
@@ -1,11 +1,13 @@
+#region Persists
 function Test-Persistence {
     <#
     .SYNOPSIS
         Persistence check helper for files.
     .DESCRIPTION
-        This will save some lines to not always write `if (-not (Test-Path $persist_dir\$file)) { New-item | Out-Null }` inside manifests.
+        This will save some lines to not always write `if (-not (Test-Path "$persist_dir\$file")) { New-item "$dir\$file" | Out-Null }` inside manifests.
     .PARAMETER File
         File to be checked.
+        Do not prefix with $dir. All files are already checked against $dir.
     .PARAMETER Content
         If file does not exists it will be created with this value. Value should be array of strings or string.
     .PARAMETER Execution
@@ -25,9 +27,7 @@ function Test-Persistence {
         for ($ind = 0; $ind -lt $File.Count; ++$ind) {
             $currentFile = $File[$ind]
 
-            if (-not (Join-Path $persist_dir $currentFile | Test-Path -Type Leaf)) {
-                if (-not $quiet) { warn "File $currentFile do not exists. Creating." }
-
+            if (-not (Join-Path $persist_dir $currentFile | Test-Path -PathType Leaf)) {
                 if ($Execution) {
                     & $Execution
                 } else {
@@ -50,13 +50,15 @@ function Test-Persistence {
         }
     }
 }
+#endregion Persists
 
 function Remove-AppDirItem {
     <#
     .SYNOPSIS
-        Remove given item from applications directory. Wildcards are supported.
+        Remove given item from applications directory.
+        Wildcards are supported.
     .PARAMETER Item
-        Item to be removed.
+        Specify item for removing from $dir.
     #>
     [CmdletBinding(SupportsShouldProcess = $true)]
     param(
@@ -70,144 +72,74 @@ function Remove-AppDirItem {
     process { foreach ($it in $Item) { Get-ChildItem $dir $it | Remove-Item -Force -Recurse } }
 }
 
+function Edit-File {
+    <#
+    .SYNOPSIS
+        Find and replace text in given file.
+    .PARAMETER File
+        Specify file, which will be loaded.
+        File could be passed as full path (used for changing files outside $dir) or just relative path to $dir.
+    .PARAMETER Find
+        Specify the string to be replaced.
+    .PARAMETER Replace
+        Specify the string for replacing all occurrences.
+        Empty string is default => Found string will be removed.
+    .PARAMETER Regex
+        Specify if regular expression should be used instead of simple match.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [String] $File,
+        [Parameter(Mandatory = $true)]
+        [String[]] $Find,
+        [String[]] $Replace,
+        [Switch] $Regex
+    )
+
+    begin {
+        # Use file from $dir
+        if (Join-Path $dir $File | Test-Path -PathType Leaf) { $File = Join-Path $dir $File }
+        if (-not (Test-Path $File)) {
+            error "File $File does not exist."
+            return
+        }
+    }
+
+    process {
+        $content = Get-Content $File
+        for ($i = 0; $i -lt $Find.Count; ++$i) {
+            $toFind = $Find[$i]
+            if (-not $Replace -or ($null -eq $Replace[$i])) {
+                $toReplace = ''
+            } else {
+                $toReplace = $Replace[$i]
+            }
+
+            if ($Regex) {
+                $content = $content -replace $toFind, $toReplace
+            } else {
+                $content = $content.Replace($toFind, $toReplace)
+            }
+        }
+
+        Set-Content -LiteralPath $File -Value $content -Encoding Ascii -Force
+    }
+}
+
 function New-JavaShortcutWrapper {
     <#
     .SYNOPSIS
         Create new shim-like batch file wrapper to spawn jar files within start menu (using shortcut).
-    .PARAMETER Filename
-        Filename of jar executable. Without .jar extension!
+    .PARAMETER FileName
+        Jar executable filename without .jar extension.
     #>
     [CmdletBinding(SupportsShouldProcess = $true)]
-    param([Parameter(Mandatory = $true, ValueFromPipeline = $true)] [String[]] $Filename)
+    param([Parameter(Mandatory = $true, ValueFromPipeline = $true)] [String[]] $FileName)
 
     process {
-        foreach ($f in $Filename) {
+        foreach ($f in $FileName) {
             Set-Content -LiteralPath (Join-Path $dir "$f.bat") -Value "@start javaw.exe -jar `"%~dp0$f.jar`" %*" -Encoding Ascii -Force
         }
     }
 }
-
-function Assert-Administrator {
-    <#
-    .SYNOPSIS
-        Test if current user have administrator privileges needed for proper installation / uninstallation.
-    #>
-    if (-not (is_admin)) {
-        error 'Administrator privileges are required for installation'
-        # TODO:
-        abort
-    }
-}
-
-function Assert-WindowsVersion {
-    <#
-    .SYNOPSIS
-        Test if Windows version meets application requirements.
-    #>
-    param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [Alias('Version')]
-        [String]
-        $RequiredVersion
-    )
-
-    if ((Compare-Version ([System.Environment]::OSVersion.Version.ToString()) $RequiredVersion) -eq -1) {
-        error "Application requires at least Windows version $RequiredVersion"
-        # TODO:
-        abort
-    }
-}
-
-#region dotNet Framework
-function Get-InstalledDotNetFrameworkVersion {
-    <#
-    .SYNOPSIS
-        List all installed .NET Framework versions.
-    .OUTPUTS
-        Array of installed versions.
-    #>
-    [CmdletBinding()]
-    [OutputType([String[]])]
-
-    $base = 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP'
-
-    $versions = @()
-    '2.0.50727', '3.0', '3.5', '4\Full' | ForEach-Object {
-        $versions += Get-ItemProperty "$base\v$_" 'Version' | Select-Object -ExpandProperty 'Version'
-    }
-
-    return $versions
-}
-
-function Get-LatestDotNetFrameworkVersion {
-    <#
-    .SYNOPSIS
-        Get highest/latest installed version of .NET Framework.
-    #>
-    [CmdletBinding()]
-    [OutputType([String])]
-    param() # For some reason there has to be empty param block
-
-    return Get-InstalledDotNetFrameworkVersion | Sort-Object | Select-Object -Last 1
-}
-
-function Convert-DotNetFrameworkVersion {
-    <#
-    .SYNOPSIS
-        Convert .NET Framework version into comparable release key.
-        https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
-    .PARAMETER Version
-        Version of .NET Framework.
-    #>
-    [CmdletBinding()]
-    [OutputType([String])]
-    param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $Version
-    )
-
-    $key = switch -Wildcard ($Version) {
-        '4.5.1*' { 378675; break; }
-        '4.5.2*' { 379893; break; }
-        '4.5*' { 378389; break; }
-        '4.6.1*' { 394254; break; }
-        '4.6.2*' { 394802; break; }
-        '4.6*' { 393295; break; }
-        '4.7.1*' { 461308; break; }
-        '4.7.2*' { 461808; break; }
-        '4.7*' { 460798; break; }
-        '4.8*' { 528040; break; }
-        default { $null }
-    }
-
-    return $key
-}
-
-function Assert-DotNetFramework {
-    <#
-    .SYNOPSIS
-        Test if required .NET Framework version is installed.
-    .PARAMETER RequiredVersion
-        Required version of .NET Framework.
-    #>
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [ValidateNotNullOrEmpty()]
-        [Alias('Version')]
-        [String]
-        $RequiredVersion
-    )
-
-    $latest = Get-LatestDotNetFrameworkVersion | Convert-DotNetFrameworkVersion
-    $required = Convert-DotNetFrameworkVersion $RequiredVersion
-
-    if ($latest -lt $required) {
-        error "Application requires at least .NET Framework $RequiredVersion"
-        # TODO:
-        abort
-    }
-}
-#endregion dotNet Framework
