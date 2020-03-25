b893c86eaf8b39d47c9bb2b5f28dcd00d5476c4c
L-Trump <37738631+L-Trump@users.noreply.github.com>
2020-03-25 11:30:13 +0800

    Provide two ways to set encoding and cookie
    use parameter Cookies,Encode or $json.checkver.cookie&$json.checkver.encode

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -15,6 +15,12 @@
     Useful for hash updates.
 .PARAMETER SkipUpdated
     Updated manifests will not be shown.
+.PARAMETER Cookie
+    Set Cookies
+.PARAMETER UserAgent
+    Set User-Agent
+.PARAMETER Encode
+    Set Encoding Type
 .EXAMPLE
     PS BUCKETROOT > .\bin\checkver.ps1
     Check all manifest inside default directory.
@@ -60,6 +66,9 @@ param(
     [Switch] $Update,
     [Switch] $ForceUpdate,
     [Switch] $SkipUpdated,
+    [String] $Cookie,
+    [String] $UserAgent,
+    [String] $Encode
     [String] $Version = ''
 )
 
@@ -100,7 +109,18 @@ $Queue | ForEach-Object {
     if ($json.checkver.useragent) {
         $wc.Headers.Add('User-Agent', (substitute $json.checkver.useragent $substitutions))
     } else {
-        $wc.Headers.Add('User-Agent', (Get-UserAgent))
+        if ($UserAgent){
+            $wc.Headers.Add('User-Agent', (substitute $UserAgent $substitutions))
+        } else {
+            $wc.Headers.Add('User-Agent', (Get-UserAgent))
+        }
+    }
+    if ($json.checkver.cookie) {
+        $wc.Headers.Add('cookie', $json.checkver.cookie)
+    } else {
+        if ($Cookie) {
+            $wc.Headers.Add('cookie', $Cookie)
+        }
     }
     Register-ObjectEvent $wc downloadstringcompleted -ErrorAction Stop | Out-Null
 
@@ -127,7 +147,15 @@ $Queue | ForEach-Object {
         $url = $json.checkver.github + '/releases/latest'
         $regex = $githubRegex
     }
-
+    
+    if ($json.checkver.encode) {
+        $wc.Encoding = [System.Text.Encoding]::GetEncoding($json.checkver.encode)
+    } else {
+        if($Encode) {
+            $wc.Encoding = [System.Text.Encoding]::GetEncoding($Encode)
+        }
+    }
+    
     if ($json.checkver.re) {
         $regex = $json.checkver.re
     }
@@ -144,7 +172,6 @@ $Queue | ForEach-Object {
     if ($json.checkver.xpath) {
         $xpath = $json.checkver.xpath
     }
-
     if ($json.checkver.replace -and $json.checkver.replace.GetType() -eq [System.String]) {
         $replace = $json.checkver.replace
     }
@@ -167,7 +194,6 @@ $Queue | ForEach-Object {
         reverse  = $reverse;
         replace  = $replace;
     }
-
     $wc.Headers.Add('Referer', (strip_filename $url))
     $wc.DownloadStringAsync($url, $state)
 }
