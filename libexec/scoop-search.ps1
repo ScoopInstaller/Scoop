ee91c1316ab6913324ad509c3766c7eff632dca2
Neonailol <neonailol@gmail.com>
2020-05-14 17:47:19 +0300

    Implemented verbose search

--- libexec/scoop-search.ps1
+++ libexec/scoop-search.ps1
@@ -4,11 +4,23 @@
 #
 # If used with [query], shows app names that match the query.
 # Without [query], shows all the available apps.
-param($query)
+# Options:
+#   -v, --verbose   Show extended application info
+#   -k, --known     Search also by all known buckets
+
+param(
+  [String]$query,
+  [Switch]$verbose = $false,
+  [Switch]$known = $false
+)
+
 . "$psscriptroot\..\lib\core.ps1"
 . "$psscriptroot\..\lib\buckets.ps1"
 . "$psscriptroot\..\lib\manifest.ps1"
 . "$psscriptroot\..\lib\versions.ps1"
+. "$psscriptroot\..\lib\depends.ps1"
+. "$psscriptroot\..\lib\help.ps1"
+. "$psscriptroot\..\lib\install.ps1"
 
 reset_aliases
 
@@ -107,12 +119,120 @@ Get-LocalBucket | ForEach-Object {
             $item = "    $($_.name) ($($_.version))"
             if($_.bin) { $item += " --> includes '$($_.bin)'" }
             $item
+            if($verbose) {
+                $app, $bucket, $null = parse_app $_.name
+                $status = app_status $app $global
+                $manifest, $bucket = find_manifest $app $bucket
+                if ($manifest) {
+                    $install = install_info $app $status.version $global
+                    $version_output = $manifest.version
+                    if (!$manifest_file) {
+                        $manifest_file = manifest_path $app $bucket
+                    }
+
+                    $dir = versiondir $app 'current' $global
+                    $original_dir = versiondir $app $manifest.version $global
+                    $persist_dir = persistdir $app $global
+
+                    if($status.installed) {
+                        $manifest_file = manifest_path $app $install.bucket
+                        if ($install.url) {
+                            $manifest_file = $install.url
+                        }
+                        if($status.version -eq $manifest.version) {
+                            $version_output = $status.version
+                        } else {
+                            $version_output = "        $($status.version) (Update to $($manifest.version) available)"
+                        }
+                    }
+
+                    Write-Output "        Name: $app"
+                    if ($manifest.description) {
+                        Write-Output "        Description: $($manifest.description)"
+                    }
+                    Write-Output "        Version: $version_output"
+                    Write-Output "        Website: $($manifest.homepage)"
+                    # Show license
+                    if ($manifest.license) {
+                        $license = $manifest.license
+                        if ($manifest.license.identifier -and $manifest.license.url) {
+                            $license = "$($manifest.license.identifier) ($($manifest.license.url))"
+                        } elseif ($manifest.license -match '^((ht)|f)tps?://') {
+                            $license = "$($manifest.license)"
+                        } elseif ($manifest.license -match '[|,]') {
+                            $licurl = $manifest.license.Split("|,") | ForEach-Object {"https://spdx.org/licenses/$_.html"}
+                            $license = "$($manifest.license) ($($licurl -join ', '))"
+                        } else {
+                            $license = "$($manifest.license) (https://spdx.org/licenses/$($manifest.license).html)"
+                        }
+                        Write-Output "        License: $license"
+                    }
+
+                    # Manifest file
+                    Write-Output "        Manifest:`n          $manifest_file"
+
+                    if($status.installed) {
+                        # Show installed versions
+                        Write-Output "        Installed:"
+                        $versions = versions $app $global
+                        $versions | ForEach-Object {
+                            $dir = versiondir $app $_ $global
+                            if($global) { $dir += " *global*" }
+                            Write-Output "          $dir"
+                        }
+                    } else {
+                        Write-Output "        Installed: No"
+                    }
+
+                    $binaries = @(arch_specific 'bin' $manifest $install.architecture)
+                    if($binaries) {
+                        $binary_output = "Binaries:`n         "
+                        $binaries | ForEach-Object {
+                            if($_ -is [System.Array]) {
+                                $binary_output += " $($_[1]).exe"
+                            } else {
+                                $binary_output += " $_"
+                            }
+                        }
+                        Write-Output "        $binary_output"
+                    }
+
+                    if($manifest.env_set -or $manifest.env_add_path) {
+                        if($status.installed) {
+                            Write-Output "        Environment:"
+                        } else {
+                            Write-Output "        Environment: (simulated)"
+                        }
+                    }
+                    if($manifest.env_set) {
+                        $manifest.env_set | Get-Member -member noteproperty | ForEach-Object {
+                            $value = env $_.name $global
+                            if(!$value) {
+                                $value = format $manifest.env_set.$($_.name) @{ "dir" = $dir }
+                            }
+                            Write-Output "          $($_.name)=$value"
+                        }
+                    }
+                    if($manifest.env_add_path) {
+                        $manifest.env_add_path | Where-Object { $_ } | ForEach-Object {
+                            if($_ -eq '.') {
+                                Write-Output "          PATH=%PATH%;$dir"
+                            } else {
+                                Write-Output "          PATH=%PATH%;$dir\$_"
+                            }
+                        }
+                    }
+
+                    # Show notes
+                    show_notes $manifest $dir $original_dir $persist_dir
+                }
+            }
         }
         ""
     }
 }
 
-if (!$local_results -and !(github_ratelimit_reached)) {
+if (!(github_ratelimit_reached) -and (!$local_results -or $known)) {
     $remote_results = search_remotes $query
     if(!$remote_results) { [console]::error.writeline("No matches found."); exit 1 }
     $remote_results
