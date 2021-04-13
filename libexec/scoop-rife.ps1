25be11d4e7cf7fc6ab8c2873616aca883fc73f39
HUMORCE <HUMORCE@outlook.com>
2021-04-13 10:58:44 +0000

    feat: Add 'scoop-rife' command

---
+++ libexec/scoop-rife.ps1
@@ -0,0 +1,35 @@
+# Usage: scoop rife <app> [options]
+# Summary: Reveal app in file explorer
+#
+# Options:
+#   -h, --help      Show help for this command.
+
+param($app)
+
+'help', 'Versions' | ForEach-Object {
+    . (Join-Path $PSScriptRoot "..\lib\$_.ps1")
+}
+
+if (!$app) { Stop-ScoopExecution -Message 'Parameter <app> missing' -Usage (my_usage) }
+
+if ($app) {
+    $global = installed $app $true
+    $status = app_status $app $global
+}
+
+if ($status.installed) {
+    $dir = versiondir $app $_ $global
+    $versions = Get-InstalledVersion -AppName $app -Global:$global
+    if ($versions.getType().Name -eq 'String') {
+        $dir += $versions
+    } else {
+        $dir += $versions[-1]
+    }
+    # Deprecated
+    # $dir += '\current'
+    Start-Process $dir
+} else {
+    Stop-ScoopExecution -Message "'$app' isn't installed."
+}
+
+exit 0
