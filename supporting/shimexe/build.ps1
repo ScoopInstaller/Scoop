ef3bf14547198376a4c13c8e526a224f49287526
Hsiao-nan Cheung <niheaven@gmail.com>
2021-11-13 21:42:00 +0800

    feat(supporting): Update Json to 12.0.3, Json.Schema to 3.0.14 (#3352)

--- supporting/shimexe/build.ps1
+++ supporting/shimexe/build.ps1
@@ -1,21 +1,21 @@
 Param([Switch]$Fast)
-Push-Location $psscriptroot
-. "$psscriptroot\..\..\lib\install.ps1"
+Push-Location $PSScriptRoot
+. "$PSScriptRoot\..\..\lib\install.ps1"
 
-if(!$Fast) {
+if (!$Fast) {
     Write-Host "Install dependencies ..."
-    Invoke-Expression "$psscriptroot\install.ps1"
+    Invoke-Expression "$PSScriptRoot\install.ps1"
 }
 
-$output = "$psscriptroot\bin"
+$output = "$PSScriptRoot\bin"
 Write-Output 'Compiling shim.cs ...'
-& "$psscriptroot\packages\Microsoft.Net.Compilers\tools\csc.exe" /deterministic /platform:anycpu /nologo /optimize /target:exe /out:"$output\shim.exe" shim.cs
+& "$PSScriptRoot\packages\Microsoft.Net.Compilers.Toolset\tasks\net472\csc.exe" -deterministic -platform:anycpu -nologo -optimize -target:exe -out:"$output\shim.exe" shim.cs
 
 Write-Output 'Computing checksums ...'
-Remove-Item "$psscriptroot\bin\checksum.sha256" -ErrorAction Ignore
-Remove-Item "$psscriptroot\bin\checksum.sha512" -ErrorAction Ignore
-Get-ChildItem "$psscriptroot\bin\*" -Include *.exe,*.dll | ForEach-Object {
-    "$(compute_hash $_ 'sha256') *$($_.Name)" | Out-File "$psscriptroot\bin\checksum.sha256" -Append -Encoding oem
-    "$(compute_hash $_ 'sha512') *$($_.Name)" | Out-File "$psscriptroot\bin\checksum.sha512" -Append -Encoding oem
+Remove-Item "$PSScriptRoot\bin\checksum.sha256" -ErrorAction Ignore
+Remove-Item "$PSScriptRoot\bin\checksum.sha512" -ErrorAction Ignore
+Get-ChildItem "$PSScriptRoot\bin\*" -Include *.exe, *.dll | ForEach-Object {
+    "$(compute_hash $_ 'sha256') *$($_.Name)" | Out-File "$PSScriptRoot\bin\checksum.sha256" -Append -Encoding oem
+    "$(compute_hash $_ 'sha512') *$($_.Name)" | Out-File "$PSScriptRoot\bin\checksum.sha512" -Append -Encoding oem
 }
 Pop-Location
