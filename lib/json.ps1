c55c1e1911d2db5a922bc3b361076fbbb324d8bd
Pratik Chowdhury <pratikc@live.co.uk>
2021-11-19 22:11:17 -0800

    Update lib/json.ps1
    Co-authored-by: Hsiao-nan Cheung <niheaven@gmail.com>

--- lib/json.ps1
+++ lib/json.ps1
@@ -108,7 +108,7 @@ function json_path([String] $json, [String] $jsonpath, [Hashtable] $substitution
             # Return versions in reverse order
             $result = [System.Linq.Enumerable]::Reverse($result)
         }
-        if ($returnsingle) {
+        if ($single) {
             # Extract First value
             $result = [System.Linq.Enumerable]::First($result)
             # Convert first value to string
