968e38bad8bd9cf7d1bf33bd0ba4c8b32d3db2f6
Rashil Gandhi <rashil2000@gmail.com>
2021-11-29 22:41:27 +0530

    f

--- libexec/scoop-which.ps1
+++ libexec/scoop-which.ps1
@@ -21,9 +21,11 @@ $globalshims = fullpath (shimdir $true) # don't resolve: may not exist
 
 if($path -like "$usershims*" -or $path -like "$globalshims*") {
     $exepath = if ($path.endswith(".exe")) {
-        (Get-Content "$(join-path ([system.io.fileinfo]$path).DirectoryName ([system.io.fileinfo]$path).BaseName).shim").split(' ') | Select-Object -Index 2
+        (Get-Content "$(join-path ([system.io.fileinfo]$path).DirectoryName ([system.io.fileinfo]$path).BaseName).shim" | Select-Object -First 1).replace('path = ', '')
     } elseif ($path.endswith(".ps1")) {
         (Get-Content $path | Where-Object { $_.startswith('$path') }).split(' ') | Select-Object -Last 1 | Invoke-Expression
+    } else {
+        # need to handle .cmd files
     }
 
     if(![system.io.path]::ispathrooted($exepath)) {
