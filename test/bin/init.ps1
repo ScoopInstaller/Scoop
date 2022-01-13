b0d22331547439e0e476a6d14322cbb301298885
Chawye Hsu <chawyehsu@hotmail.com>
2022-01-13 22:58:39 +0800

    refactor(ci,tests): Support both AppVeyor and GitHub Actions
    `test\bin\init.ps1`: Refactor to support both AppVeyor and GitHub
    Actions. The Zstd helper will only be downloaded and installed on
    the AppVeyor CI. On GitHub Actions it's built-in on the CI.
    
    `test\bin\test.ps1`: Refactor to support both AppVeyor and GitHub
    Actions. Only on AppVeyor CI the TestResults will be uploaded.
    Exclusion rules of tests are only applied when tests are called
    from the Core itself on CI, skipped when tests are called either
    from the external bucket or not on CI.
    
    `test\Scoop-Manifest.Test.ps1`: Manifests test and validation have
    been merged into `test\Import-Bucket-Tests.ps1`, since the main
    bucket has been separated from the Core and there is no need to do
    manifest test in the Core repo.
    
    Signed-off-by: Chawye Hsu <chawyehsu@hotmail.com>

--- test/bin/init.ps1
+++ test/bin/init.ps1
@@ -1,20 +1,6 @@
+#Requires -Version 5.0
 Write-Host "PowerShell: $($PSVersionTable.PSVersion)"
 Write-Host (7z.exe | Select-String -Pattern '7-Zip').ToString()
-Write-Host 'Install dependencies ...'
+Write-Host 'Install testsuite dependencies ...'
 Install-Module -Repository PSGallery -Scope CurrentUser -Force -Name Pester -RequiredVersion 4.10.1 -SkipPublisherCheck
 Install-Module -Repository PSGallery -Scope CurrentUser -Force -Name PSScriptAnalyzer, BuildHelpers
-
-if ($env:CI -eq $true) {
-    Write-Host "Load 'BuildHelpers' environment variables ..."
-    Set-BuildEnvironment -Force
-}
-
-$buildVariables = ( Get-ChildItem -Path 'Env:' ).Where( { $_.Name -match '^(?:BH|CI(?:_|$)|APPVEYOR)' } )
-$buildVariables += ( Get-Variable -Name 'CI_*' -Scope 'Script' )
-$details = $buildVariables |
-    Where-Object -FilterScript { $_.Name -notmatch 'EMAIL' } |
-    Sort-Object -Property 'Name' |
-    Format-Table -AutoSize -Property 'Name', 'Value' |
-    Out-String
-Write-Host 'CI variables:'
-Write-Host $details -ForegroundColor DarkGray
