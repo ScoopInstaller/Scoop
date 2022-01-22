cb73004ba2e8b3c5e6bd989393528009cc941753
PaulWCRI <pauls@wcrimail.com>
2022-01-22 12:39:26 -0600

    normalize spacing

--- lib/json.ps1
+++ lib/json.ps1
@@ -179,7 +179,7 @@ function normalize_values([psobject] $json) {
             }
         }
 		
-		# Convert single value array into string
+        # Convert single value array into string
         if (($_.Value -is [Array]) -and $_.IsSettable) {
             # Array contains only 1 element String or Array
             if ($_.Value.Count -eq 1) {
