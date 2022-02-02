89c00a6257584edb66c32d67927fd7232b6b842e
HUMORCE <HUMORCE@outlook.com>
2022-02-02 17:22:13 +0000

    friendly_path

--- libexec/scoop-bucket.ps1
+++ libexec/scoop-bucket.ps1
@@ -41,9 +41,8 @@ function list_buckets {
         if (Test-Path (Join-Path $source '.git')) {
             $updated = git_cmd -C "`"$source`"" log --format='%ch' -n 1
             $source = git_cmd -C "`"$source`"" config remote.origin.url
-        }
-        if ($source.StartsWith((Get-PSProvider 'FileSystem').Home)) {
-            $source = $source.Replace((Get-PSProvider 'FileSystem').Home, '~')
+        } else {
+            $source = friendly_path $source
         }
 
         $buckets += New-Object PSObject -Property @{
