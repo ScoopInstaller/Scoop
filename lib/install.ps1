ae445512e258cc1497ef81bbcc9ffc5430853ff7
Hsiao-nan Cheung <niheaven@gmail.com>
2022-02-11 15:02:27 +0800

    fix(test-running-process): Fix wrong output

--- lib/install.ps1
+++ lib/install.ps1
@@ -1217,11 +1217,15 @@ function test_running_process($app, $global) {
     $processdir = appdir $app $global | Convert-Path
     $running_processes = Get-Process | Where-Object { $_.Path -like "$processdir\*" }
 
-    if ($running_processes -and !(get_config 'ignore_running_processes')) {
-        error "Application `"$app`" is still running. Close all instances and try again."
-        return $true
+    if ($running_processes) {
+        if (get_config 'ignore_running_processes') {
+            warn "Application `"$app`" is still running. Scoop is configured to ignore this condition."
+            return $false
+        } else {
+            error "Application `"$app`" is still running. Close all instances and try again."
+            return $true
+        }
     } else {
-        warn "Application `"$app`" is still running. Scoop is configured to ignore this condition."
         return $false
     }
 }
