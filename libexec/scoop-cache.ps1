f02f0699aea0b47a78c8e996f2d4e21c97eb89c3
Rashil Gandhi <rashil2000@gmail.com>
2022-02-15 15:48:13 +0530

    Add support for multiple apps

--- libexec/scoop-cache.ps1
+++ libexec/scoop-cache.ps1
@@ -1,4 +1,4 @@
-# Usage: scoop cache show|rm [app]
+# Usage: scoop cache show|rm [app(s)]
 # Summary: Show or clear the download cache
 # Help: Scoop caches downloads so you don't need to download the same files
 # when you uninstall and re-install the same version of an app.
@@ -10,7 +10,7 @@
 #
 # To clear everything in your cache, use:
 #     scoop cache rm *
-param($cmd, $app)
+param($cmd)
 
 . "$PSScriptRoot\..\lib\help.ps1"
 
@@ -22,31 +22,38 @@ function cacheinfo($file) {
 }
 
 function cacheshow($app) {
-    $files = @(Get-ChildItem "$cachedir" | Where-Object { $_.name -match "^$app" })
+    if (!$app) { $app = '*' }
+    else { $app = '(' + ($app -join '|') + ')' }
+    $files = @(Get-ChildItem "$cachedir" | Where-Object { $_.name.Split('#')[0] -match "^$app$" })
     $total_length = ($files | Measure-Object length -sum).sum -as [double]
 
     $files | ForEach-Object { cacheinfo $_ } | Select-Object Name, Version, Length, URL
 
-    Write-Host "Total: $($files.length) $(pluralize $files.length 'file' 'files'), $(filesize $total_length)"
+    Write-Host "Total: $($files.length) $(pluralize $files.length 'file' 'files'), $(filesize $total_length)" -ForegroundColor Yellow
+}
+
+function cacheremove($app) {
+    if (!$app) { 'ERROR: <app(s)> missing'; my_usage; exit 1 }
+    elseif ($app -ne '*') { $app = '(' + ($app -join '|') + ')' }
+    $files = @(Get-ChildItem "$cachedir" | Where-Object { $_.name.Split('#')[0] -match "^$app$" })
+    $total_length = ($files | Measure-Object length -sum).sum -as [double]
+
+    $files | ForEach-Object {
+        $curr = cacheinfo $_
+        Write-Host "Removing $(if ($curr.URL) { $curr.URL } else { $curr.Name })..."
+        Remove-Item $_.FullName
+        if(Test-Path "$cachedir\$($curr.Name).txt") {
+            Remove-Item "$cachedir\$($curr.Name).txt"
+        }
+    }
+
+    Write-Host "Deleted: $($files.length) $(pluralize $files.length 'file' 'files'), $(filesize $total_length)" -ForegroundColor Yellow
 }
 
 switch($cmd) {
-    'rm' {
-        if(!$app) { 'ERROR: <app> missing'; my_usage; exit 1 }
-        Remove-Item "$cachedir\$app#*"
-        if(test-path("$cachedir\$app.txt")) {
-            Remove-Item "$cachedir\$app.txt"
-        }
-    }
-    'show' {
-        cacheshow $app
-    }
-    '' {
-        cacheshow
-    }
-    default {
-        my_usage
-    }
+    {$_ -in $null, 'show'}  { cacheshow   $args }
+    {$_ -in 'rm', 'remove'} { cacheremove $args }
+    default                 { my_usage }
 }
 
 exit 0
