d6140ed03f5b63867440b2eb187cd423f7c699d8
Hsiao-nan Cheung <niheaven@gmail.com>
2022-02-21 14:05:07 +0800

    fix(shim-add): Add shim target args as command args

--- libexec/scoop-shim.ps1
+++ libexec/scoop-shim.ps1
@@ -1,18 +1,18 @@
-# Usage: scoop shim <subcommand> [<shim_names>] [<command_path>]
+# Usage: scoop shim <subcommand> [<shim_names>] [<command_path> [<args>...]] [-g|--global]
 # Summary: Manipulate Scoop shims
 # Help: Manipulate Scoop shims: add, remove/rm, list, info, alter, etc.
 #
 # To add a costom shim, use the 'add' subcommand:
 #
-#     scoop shim add <shim_name> <command_path>
+#     scoop shim add <shim_name> <command_path> [<args>...]
 #
 # To remove a shim, use the 'remove' or 'rm' subcommand (CAUTION: this could remove shims added by app manifest):
 #
 #     scoop shim remove <shim_names>
 #
-# To list all shims or a matching shim, use the 'list' subcommand:
+# To list all shims or matching shims, use the 'list' subcommand:
 #
-#     scoop shim list [<shim_name>]
+#     scoop shim list [<shim_names>]
 #
 # To show a shim's information, use the 'info' subcommand:
 #
@@ -25,18 +25,10 @@
 # Options:
 #   -g, --global       Add/Remove/Info/Alter global shim(s)
 
-param($SubCommand)
+param($SubCommand, $ShimName)
 
 . "$PSScriptRoot\..\lib\help.ps1"
-. "$PSScriptRoot\..\lib\getopt.ps1" # for getopt
 . "$PSScriptRoot\..\lib\install.ps1" # for rm_shim
-
-$opt, $addArgs, $err = getopt $Args 'g' 'global'
-if ($err) { abort "scoop shim: $err" 1 }
-$global = $opt.g -or $opt.global
-$globalTag = if ($global) { 'global' } else { 'local' }
-
-$shimName = $addArgs[0]
 
 if ($SubCommand -notin @('add', 'remove', 'rm', 'list', 'info', 'alter')) {
     'ERROR: <subcommand> must be one of: add, remove/rm, list, info, alter'
@@ -44,16 +36,49 @@ if ($SubCommand -notin @('add', 'remove', 'rm', 'list', 'info', 'alter')) {
     exit 1
 }
 
-if (!$shimName -and $SubCommand -ne 'list') {
+if ($SubCommand -ne 'list' -and !$ShimName) {
     "ERROR: <shim_name> must be specified for subcommand '$SubCommand'"
     my_usage
     exit 1
 }
 
-if ($SubCommand -eq 'add' -and $null -eq $addArgs[1]) {
-    "ERROR: <command_path> must be specified for subcommand 'add'"
-    my_usage
-    exit 1
+if ($Args) {
+    $global = $Args[-1] -eq '-g' -or $Args[-1] -eq '--global'
+    switch ($SubCommand) {
+        'add' {
+            if ($Args[0] -like '-*') {
+                "ERROR: <command_path> must be specified for subcommand 'add'"
+                my_usage
+                exit 1
+            } else {
+                if (($Args -join ' ') -match "^'(.*?)'(.*?)( -g| --global)?$") {
+                    $commandPath = $Matches[1]
+                    $commandArgs = $Matches[2]
+                } else {
+                    $commandPath = $Args[0]
+                    $commandArgs = $Args[1..($Args.Length - $(if ($global) { 2 } else { 1 }))]
+                }
+            }
+        }
+        { $_ -in 'remove', 'rm' } {
+            if ($Args.Length -gt 1) {
+                $ShimName = @($ShimName) + $Args[0..($Args.Length - $(if ($global) { 2 } else { 1 }))]
+            } elseif ($Args.Length -eq 1 -and !$global) {
+                $ShimName = @($ShimName) + $Args
+            }
+        }
+        'list' {
+            $ShimName = (@($ShimName) + $Args) -join '|'
+        }
+        default {
+            # For 'info' and 'alter'
+            if ($Args.Length -ne 1 -or $Args[0] -notin @('-g', '--global')) {
+                "ERROR: Option $Args not recognized."
+                my_usage
+                exit 1
+            }
+        }
+    }
 }
 
 if (-not (Get-FormatData ScoopShims)) {
@@ -78,9 +103,9 @@ function Get-ShimInfo($ShimPath) {
     [PSCustomObject]$info
 }
 
-function Get-ShimPath($shimName, $Global) {
+function Get-ShimPath($ShimName, $Global) {
     '.shim', '.ps1' | ForEach-Object {
-        $shimPath = Join-Path (shimdir $Global) "$shimName$_"
+        $shimPath = Join-Path (shimdir $Global) "$ShimName$_"
         if (Test-Path $shimPath) {
             return $shimPath
         }
@@ -103,14 +128,6 @@ function Get-ShimTarget($ShimPath) {
 
 switch ($SubCommand) {
     'add' {
-        $commandPath = $addArgs[1]
-        if ($commandPath -match "^[`"'](.*?)[`"']\s*(.*)$") {
-            $commandPath = $Matches[1]
-            $commandArgs = $Matches[2]
-        } elseif ($commandPath -match '^([^\s]*)\s*(.*)$') {
-            $commandPath = $Matches[1]
-            $commandArgs = $Matches[2]
-        }
         if ($commandPath -notmatch '[\\/]') {
             $shortPath = $commandPath
             $commandPath = Get-ShimTarget (Get-ShimPath $shortPath $global)
@@ -122,17 +139,17 @@ switch ($SubCommand) {
             }
         }
         if ($commandPath -and (Test-Path $commandPath)) {
-            Write-Host "Adding $globalTag shim " -NoNewline
+            Write-Host "Adding $(if ($global) { 'global' } else { 'local' }) shim " -NoNewline
             Write-Host $shimName -ForegroundColor Cyan -NoNewline
             Write-Host '...'
             shim $commandPath $global $shimName $commandArgs
         } else {
-            abort "ERROR: '$($addArgs[1])' does not exist" 3
+            abort "ERROR: '$($Args[0])' does not exist" 3
         }
     }
     { $_ -in 'remove', 'rm' } {
         $failed = @()
-        $addArgs | ForEach-Object {
+        $ShimName | ForEach-Object {
             if (Get-ShimPath $_ $global) {
                 rm_shim $_ (shimdir $global)
             } else {
@@ -147,54 +164,54 @@ switch ($SubCommand) {
     }
     'list' {
         $shims = Get-ChildItem -Path $localShimDir -Recurse -Include '*.shim', '*.ps1' |
-            Where-Object { !$shimName -or ($_.BaseName -match $shimName) } |
+            Where-Object { !$ShimName -or ($_.BaseName -match $ShimName) } |
             Select-Object -ExpandProperty FullName
         if (Test-Path $globalShimDir) {
             $shims += Get-ChildItem -Path $globalShimDir -Recurse -Include '*.shim', '*.ps1' |
-                Where-Object { !$shimName -or ($_.BaseName -match $shimName) } |
+                Where-Object { !$ShimName -or ($_.BaseName -match $ShimName) } |
                 Select-Object -ExpandProperty FullName
         }
         $shims.ForEach({ Get-ShimInfo $_ }) | Add-Member -TypeName 'ScoopShims' -PassThru
     }
     'info' {
-        $shimPath = Get-ShimPath $shimName $global
+        $shimPath = Get-ShimPath $ShimName $global
         if ($shimPath) {
             Get-ShimInfo $shimPath
         } else {
             Write-Host "$(if ($global) { 'Global' } else { 'Local' }) shim not found: " -NoNewline
-            Write-Host $shimName -ForegroundColor Cyan
-            if (Get-ShimPath $shimName (!$global)) {
+            Write-Host $ShimName -ForegroundColor Cyan
+            if (Get-ShimPath $ShimName (!$global)) {
                 Write-Host "But a $(if ($global) { 'local' } else {'global' }) shim exists, " -NoNewline
-                Write-Host "run 'scoop shim info $shimName$(if (!$global) { ' -global' })' to show its info"
+                Write-Host "run 'scoop shim info $ShimName$(if (!$global) { ' --global' })' to show its info"
                 exit 2
             }
             exit 3
         }
     }
     'alter' {
-        $shimPath = Get-ShimPath $shimName $global
+        $shimPath = Get-ShimPath $ShimName $global
         if ($shimPath) {
             $shimInfo = Get-ShimInfo $shimPath
             if ($null -eq $shimInfo.Alternatives) {
                 Write-Host 'No alternatives of ' -NoNewline
-                Write-Host $shimName -ForegroundColor Cyan -NoNewline
+                Write-Host $ShimName -ForegroundColor Cyan -NoNewline
                 Write-Host ' found.'
                 exit 2
             }
             $shimInfo.Alternatives = $shimInfo.Alternatives.Split(' ')
             [System.Management.Automation.Host.ChoiceDescription[]]$altApps = 1..$shimInfo.Alternatives.Length | ForEach-Object {
-                New-Object System.Management.Automation.Host.ChoiceDescription "&$($_)`b$($shimInfo.Alternatives[$_ - 1])", "Sets '$shimName' shim from $($shimInfo.Alternatives[$_ - 1])."
+                New-Object System.Management.Automation.Host.ChoiceDescription "&$($_)`b$($shimInfo.Alternatives[$_ - 1])", "Sets '$ShimName' shim from $($shimInfo.Alternatives[$_ - 1])."
             }
-            $selected = $Host.UI.PromptForChoice("Alternatives of '$shimName' command", "Please choose one that provides '$shimName' as default:", $altApps, 0)
+            $selected = $Host.UI.PromptForChoice("Alternatives of '$ShimName' command", "Please choose one that provides '$ShimName' as default:", $altApps, 0)
             if ($selected -eq 0) {
-                Write-Host $shimName -ForegroundColor Cyan -NoNewline
+                Write-Host $ShimName -ForegroundColor Cyan -NoNewline
                 Write-Host ' is already from ' -NoNewline
                 Write-Host $shimInfo.Source -ForegroundColor DarkYellow -NoNewline
                 Write-Host ', nothing changed.'
             } else {
                 $newApp = $shimInfo.Alternatives[$selected]
                 Write-Host 'Use ' -NoNewline
-                Write-Host $shimName -ForegroundColor Cyan -NoNewline
+                Write-Host $ShimName -ForegroundColor Cyan -NoNewline
                 Write-Host ' from ' -NoNewline
                 Write-Host $newApp -ForegroundColor DarkYellow -NoNewline
                 Write-Host ' as default...' -NoNewline
@@ -213,10 +230,10 @@ switch ($SubCommand) {
             }
         } else {
             Write-Host "$(if ($global) { 'Global' } else { 'Local' }) shim not found: " -NoNewline
-            Write-Host $shimName -ForegroundColor Cyan
-            if (Get-ShimPath $shimName (!$global)) {
+            Write-Host $ShimName -ForegroundColor Cyan
+            if (Get-ShimPath $ShimName (!$global)) {
                 Write-Host "But a $(if ($global) { 'local' } else {'global' }) shim exists, " -NoNewline
-                Write-Host "run 'scoop shim alter $shimName$(if (!$global) { ' -global' })' to alternate its source"
+                Write-Host "run 'scoop shim alter $ShimName$(if (!$global) { ' --global' })' to alternate its source"
                 exit 2
             }
             exit 3
