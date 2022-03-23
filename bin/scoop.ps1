c6349d408d651fd79d1871103c1926376bc11b00
Hsiao-nan Cheung <niheaven@gmail.com>
2022-03-23 15:56:55 +0800

    refactor(scoop): Refactor 'scoop' command

--- bin/scoop.ps1
+++ bin/scoop.ps1
@@ -1,5 +1,5 @@
 #Requires -Version 5
-param($cmd)
+param($SubCommand)
 
 Set-StrictMode -Off
 
@@ -14,25 +14,33 @@ Get-ChildItem Function: | Where-Object -Property Name -In -Value $aliases | ForE
     Set-Alias -Name $_.Name -Value Local:$($_.Name) -Scope Script
 }
 
-$commands = commands
-if ('--version' -contains $cmd -or (!$cmd -and $Args -eq '-v')) {
-    Write-Host 'Current Scoop version:'
-    Invoke-Expression "git -C '$(versiondir 'scoop' 'current')' --no-pager log --oneline HEAD -n 1"
-    Write-Host ''
+switch ($SubCommand) {
+    ({ $SubCommand -in @($null, '--help', '/?') }) {
+        exec 'help'
+    }
+    ({ $SubCommand -eq '--version' -or (!$SubCommand -and $Args -eq '-v') }) {
+        Write-Host 'Current Scoop version:'
+        Invoke-Expression "git -C '$(versiondir 'scoop' 'current')' --no-pager log --oneline HEAD -n 1"
+        Write-Host ''
 
-    Get-LocalBucket | ForEach-Object {
-        $bucketLoc = Find-BucketDirectory $_ -Root
-        if (Test-Path (Join-Path $bucketLoc '.git')) {
-            Write-Host "'$_' bucket:"
-            Invoke-Expression "git -C '$bucketLoc' --no-pager log --oneline HEAD -n 1"
-            Write-Host ''
+        Get-LocalBucket | ForEach-Object {
+            $bucketLoc = Find-BucketDirectory $_ -Root
+            if (Test-Path (Join-Path $bucketLoc '.git')) {
+                Write-Host "'$_' bucket:"
+                Invoke-Expression "git -C '$bucketLoc' --no-pager log --oneline HEAD -n 1"
+                Write-Host ''
+            }
         }
     }
-} elseif (@($null, '--help', '/?') -contains $cmd -or $Args[0] -eq '-h') {
-    exec 'help' "$cmd"
-} elseif ($commands -contains $cmd) {
-    exec $cmd $Args
-} else {
-    "scoop: '$cmd' isn't a scoop command. See 'scoop help'."
-    exit 1
+    ({ $SubCommand -in (commands) }) {
+        if ($Args -in @('-h', '--help', '/?')) {
+            exec 'help' @($SubCommand)
+        } else {
+            exec $SubCommand $Args
+        }
+    }
+    default {
+        "scoop: '$SubCommand' isn't a scoop command. See 'scoop help'."
+        exit 1
+    }
 }
