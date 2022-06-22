db765bc073387cd0adbc6a03f597a015ec3978ba
Rashil Gandhi <rashil2000@gmail.com>
2022-06-23 03:58:33 +0530

    optimize

--- libexec/scoop-export.ps1
+++ libexec/scoop-export.ps1
@@ -8,15 +8,15 @@
 $export = @{}
 
 if ($args[0] -eq '-c' -or $args[0] -eq '--config') {
-    $export.config = & "$PSScriptRoot\scoop-config.ps1" 6>$null
+    $export.config = $scoopConfig
     # Remove machine-specific properties
     foreach ($prop in 'lastUpdate', 'rootPath', 'globalPath', 'cachePath') {
         $export.config.PSObject.Properties.Remove($prop)
     }
 }
 
-$export.buckets = @(& "$PSScriptRoot\scoop-bucket.ps1" list 6>$null)
-$export.apps    = @(& "$PSScriptRoot\scoop-list.ps1"        6>$null)
+$export.buckets = list_buckets
+$export.apps = @(& "$PSScriptRoot\scoop-list.ps1" 6>$null)
 
 $export | ConvertToPrettyJSON
 
