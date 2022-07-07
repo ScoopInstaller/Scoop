8834b69dd07e61e67ff81ba234eb635579a013f1
zStruCat <98123484+zStruCat@users.noreply.github.com>
2022-07-07 08:38:07 +0800

    Update lib/buckets.ps1
    Make warning clearer
    
    Co-authored-by: Rashil Gandhi <46838874+rashil2000@users.noreply.github.com>

--- lib/buckets.ps1
+++ lib/buckets.ps1
@@ -120,8 +120,7 @@ function add_bucket($name, $repo) {
 
     $dir = Find-BucketDirectory $name -Root
     if (Test-Path $dir) {
-        warn "The '$name' bucket already exists locally. It won't be added again."
-        info "If you want to add the '$name' bucket. You must first use 'scoop bucket rm $name' to remove the already-locally-added namesake."
+        warn "The '$name' bucket already exists. To add this bucket again, first remove it by running 'scoop bucket rm $name'."
         return 2
     }
 
