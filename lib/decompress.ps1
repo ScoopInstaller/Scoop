597c7d0c9ecbd200cf4f90d8e1c7a357fb9dcc77
Hsiao-nan Cheung <niheaven@gmail.com>
2022-10-14 14:16:10 +0800

    Separate PR

--- lib/decompress.ps1
+++ lib/decompress.ps1
@@ -240,7 +240,7 @@ function Expand-ZipArchive {
         $OriDestinationPath = $DestinationPath
         $DestinationPath = "$DestinationPath\_tmp"
     }
-    Microsoft.PowerShell.Archive\Expand-Archive -Path $Path -DestinationPath $DestinationPath -Force
+    Expand-Archive -Path $Path -DestinationPath $DestinationPath -Force
     if ($ExtractDir) {
         movedir "$DestinationPath\$ExtractDir" $OriDestinationPath | Out-Null
         Remove-Item $DestinationPath -Recurse -Force
