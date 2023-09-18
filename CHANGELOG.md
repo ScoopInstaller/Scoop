806debc613b3e2c0a8ad2a98ec5c68961bc474d6
ltguillaume <ltguillaume@users.noreply.github.com>
2023-09-18 18:01:01 +0200

    Do not uninstall the entire app when updating, add `pre_update` To prevent user data loss, don't trigger `pre_uninstall`, `post_uninstall` and `uninstaller`. The `pre_update` hook will be triggered instead of `post_uninstall`.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## [v0.3.2](https://github.com/ScoopInstaller/Scoop/compare/v0.3.1...v0.3.2) - 2023-09-18
+
+### Features
+
+- **update:** To prevent user data loss, do not uninstall the entire app when updating, i.e. don't trigger `pre_uninstall`, `post_uninstall` and `uninstaller`
+- **update:** Add support for `pre_update` (instead of `post_uninstall` in the update process)
+
 ## [v0.3.1](https://github.com/ScoopInstaller/Scoop/compare/v0.3.0...v0.3.1) - 2022-11-15
 
 ### Features
--- libexec/scoop-update.ps1
+++ libexec/scoop-update.ps1
@@ -258,16 +258,12 @@ function update($app, $global, $quiet = $false, $independent, $suggested, $use_c
     $dir = versiondir $app $old_version $global
     $persist_dir = persistdir $app $global
 
-    Invoke-HookScript -HookType 'pre_uninstall' -Manifest $old_manifest -Arch $architecture
-
     #region Workaround for #2952
     if (test_running_process $app $global) {
         return
     }
     #endregion Workaround for #2952
 
-    Write-Host "Uninstalling '$app' ($old_version)"
-    run_uninstaller $old_manifest $architecture $dir
     rm_shims $app $old_manifest $global $architecture
     env_rm_path $old_manifest $dir $global $architecture
     env_rm $old_manifest $global $architecture
@@ -291,7 +287,7 @@ function update($app, $global, $quiet = $false, $independent, $suggested, $use_c
         }
     }
 
-    Invoke-HookScript -HookType 'post_uninstall' -Manifest $old_manifest -Arch $architecture
+    Invoke-HookScript -HookType 'pre_update' -Manifest $old_manifest -Arch $architecture
 
     if ($bucket) {
         # add bucket name it was installed from
