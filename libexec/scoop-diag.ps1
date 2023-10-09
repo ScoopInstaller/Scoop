631b71c581a36139a69746f4f9fb55a4cb353ab9
Richard Kuhnt <r15ch13+git@gmail.com>
2023-10-09 18:11:41 +0200

    Add colored output

--- libexec/scoop-diag.ps1
+++ libexec/scoop-diag.ps1
@@ -12,81 +12,138 @@ function Show-Value {
         [String]
         $Value,
         [Switch]
-        $Redacted
+        $Redacted,
+        [Switch]
+        $Color
     )
 
-    if([String]::IsNullOrEmpty($Value)) {
+    if ([String]::IsNullOrEmpty($Value)) {
         return
     }
 
-    if($Redacted) {
-        $Value = '<redacted>'
+    $Red = "`e[31m"
+    $Green = "`e[32m"
+    $Yellow = "`e[33m"
+    $Cyan = "`e[36m"
+    $End = "`e[0m"
+    if (!$Color) {
+        $Red, $Green, $Yellow, $Cyan, $End = '', '', '', '', ''
     }
 
-    $Value = "$Value".Replace($env:USERPROFILE, '%USERPROFILE%')
-    $Value = "$Value".Replace($env:USERNAME, '<username>')
+    if ($Redacted) {
+        $Value = "$Red<redacted>$End"
+    }
+
+    $Value = "$Value".Replace($env:USERPROFILE, "$Green`$env:USERPROFILE$End")
+    $Value = "$Value".Replace($env:USERNAME, "$Green<username>$End")
 
     $Name = $Name.PadRight(12, ' ')
 
-    Write-Output "$Name = $Value"
+    if ($Value -eq $True) {
+        $Value = "$Green$Value$End"
+    } elseif ($Value -eq $False) {
+        $Value = "$Yellow$Value$End"
+    }
+    Write-Output "$Cyan$Name$End = $Value"
 }
 
-$redactedConfigValues = @(
-    'virustotal_api_key'
-    'private_hosts'
-    'gh_token'
-    'proxy'
-    'analytics_id'
-    'alias'
-)
+function Show-Header {
+    [CmdletBinding()]
+    [OutputType([String])]
+    param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Value,
+        [Switch]
+        $Color
+    )
 
-Write-Output "`n"
-Write-Output '```ini'
-
-Write-Output "[PowerShell]"
-Show-Value -Name 'Path' -Value ([System.Diagnostics.Process]::GetCurrentProcess().MainModule.FileName)
-Show-Value -Name 'Version' -Value $PSversionTable.PSVersion.ToString()
-Show-Value -Name 'Edition' -Value $PSversionTable.PSEdition
-Show-Value -Name 'Architecture' -Value (Get-DefaultArchitecture)
-Show-Value -Name 'RunAsAdmin' -Value (is_admin)
-$parent = [System.Diagnostics.Process]::GetCurrentProcess()
-while($parent.MainModule.ModuleName -ieq 'pwsh.exe' -or $parent.MainModule.ModuleName -ieq 'powershell.exe') {
-    $parent = $parent.Parent
-}
-Show-Value -Name 'Parent' -Value $parent.MainModule.ModuleName
-
-Write-Output "[Helpers]"
-Show-Value -Name 'GitPath' -Value (Get-HelperPath -Helper Git)
-Show-Value -Name 'GitVersion' -Value (Invoke-Git -Path $PSScriptRoot -ArgumentList 'version')
-Show-Value -Name 'Zip' -Value (Test-HelperInstalled -Helper '7zip')
-Show-Value -Name 'Lessmsi' -Value (Test-HelperInstalled -Helper 'Lessmsi')
-Show-Value -Name 'Innounp' -Value (Test-HelperInstalled -Helper 'Innounp')
-Show-Value -Name 'Dark' -Value (Test-HelperInstalled -Helper 'Dark')
-Show-Value -Name 'Aria2' -Value (Test-HelperInstalled -Helper 'Aria2')
-Show-Value -Name 'Zstd' -Value (Test-HelperInstalled -Helper 'Zstd')
-
-Write-Output "[Environment]"
-Show-Value -Name 'SCOOP' -Value $env:SCOOP
-Show-Value -Name 'SCOOP_GLOBAL' -Value $env:SCOOP_GLOBAL
-Show-Value -Name 'SCOOP_CACHE' -Value $env:SCOOP_CACHE
-Show-Value -Name 'HTTPS_PROXY' -Value $env:HTTPS_PROXY -Redacted
-Show-Value -Name 'HTTP_PROXY' -Value $env:HTTP_PROXY -Redacted
-
-Write-Output "[Scoop]"
-Show-Value -Name 'Outdated' -Value (is_scoop_outdated)
-Show-Value -Name 'OnHold' -Value (Test-ScoopCoreOnHold)
-Show-Value -Name 'Config' -Value $configFile
-Show-Value -Name 'CoreRoot' -Value $coreRoot
-Show-Value -Name 'ScoopDir' -Value $scoopdir
-Show-Value -Name 'CacheDir' -Value $cachedir
-Show-Value -Name 'GlobalDir' -Value $globaldir
-
-Write-Output "[Config]"
-$scoopConfig.PSObject.Properties | ForEach-Object {
-    Show-Value -Name $_.Name -Value $_.Value -Redacted:($redactedConfigValues.Contains($_.Name))
+    if ($Color) {
+        Write-Output "`e[35m[$Value]`e[0m"
+    } else {
+        Write-Output "[$Value]"
+    }
 }
 
-Write-Output '```'
-Write-Output "`n"
+function Get-ParentProcess {
+    $parent = [System.Diagnostics.Process]::GetCurrentProcess()
+    while ($parent.MainModule.ModuleName -ieq 'pwsh.exe' -or $parent.MainModule.ModuleName -ieq 'powershell.exe') {
+        $parent = $parent.Parent
+    }
+    return $parent.MainModule.ModuleName
+}
+
+function Show-Diag {
+    [CmdletBinding()]
+    [OutputType([String])]
+    param(
+        [Switch]
+        $Markdown,
+        [Switch]
+        $Color
+    )
+
+    $redactedConfigValues = @(
+        'virustotal_api_key'
+        'private_hosts'
+        'gh_token'
+        'proxy'
+        'analytics_id'
+        'alias'
+    )
+
+    if ($Markdown) {
+        Write-Output "`n"
+        Write-Output '```ini'
+    }
+
+    Show-Header -Color:$Color -Value 'PowerShell'
+    Show-Value -Color:$Color -Name 'Path' -Value ([System.Diagnostics.Process]::GetCurrentProcess().MainModule.FileName)
+    Show-Value -Color:$Color -Name 'Version' -Value $PSversionTable.PSVersion.ToString()
+    Show-Value -Color:$Color -Name 'Edition' -Value $PSversionTable.PSEdition
+    Show-Value -Color:$Color -Name 'Architecture' -Value (Get-DefaultArchitecture)
+    Show-Value -Color:$Color -Name 'RunAsAdmin' -Value (is_admin)
+    $parent = Get-ParentProcess
+    Show-Value -Color:$Color -Name 'Parent' -Value $parent
+
+    Show-Header -Color:$Color -Value 'Helpers'
+    Show-Value -Color:$Color -Name 'GitPath' -Value (Get-HelperPath -Helper Git)
+    Show-Value -Color:$Color -Name 'GitVersion' -Value (Invoke-Git -Path $PSScriptRoot -ArgumentList 'version')
+    Show-Value -Color:$Color -Name 'Zip' -Value (Test-HelperInstalled -Helper '7zip')
+    Show-Value -Color:$Color -Name 'Lessmsi' -Value (Test-HelperInstalled -Helper 'Lessmsi')
+    Show-Value -Color:$Color -Name 'Innounp' -Value (Test-HelperInstalled -Helper 'Innounp')
+    Show-Value -Color:$Color -Name 'Dark' -Value (Test-HelperInstalled -Helper 'Dark')
+    Show-Value -Color:$Color -Name 'Aria2' -Value (Test-HelperInstalled -Helper 'Aria2')
+    Show-Value -Color:$Color -Name 'Zstd' -Value (Test-HelperInstalled -Helper 'Zstd')
+
+    Show-Header -Color:$Color -Value 'Environment'
+    Show-Value -Color:$Color -Name 'SCOOP' -Value $env:SCOOP
+    Show-Value -Color:$Color -Name 'SCOOP_GLOBAL' -Value $env:SCOOP_GLOBAL
+    Show-Value -Color:$Color -Name 'SCOOP_CACHE' -Value $env:SCOOP_CACHE
+    Show-Value -Color:$Color -Name 'HTTPS_PROXY' -Value $env:HTTPS_PROXY -Redacted
+    Show-Value -Color:$Color -Name 'HTTP_PROXY' -Value $env:HTTP_PROXY -Redacted
+
+    Show-Header -Color:$Color -Value 'Scoop'
+    Show-Value -Color:$Color -Name 'Outdated' -Value (is_scoop_outdated)
+    Show-Value -Color:$Color -Name 'OnHold' -Value (Test-ScoopCoreOnHold)
+    Show-Value -Color:$Color -Name 'Config' -Value $configFile
+    Show-Value -Color:$Color -Name 'CoreRoot' -Value $coreRoot
+    Show-Value -Color:$Color -Name 'ScoopDir' -Value $scoopdir
+    Show-Value -Color:$Color -Name 'CacheDir' -Value $cachedir
+    Show-Value -Color:$Color -Name 'GlobalDir' -Value $globaldir
+
+    Show-Header -Color:$Color -Value 'Config'
+    $scoopConfig.PSObject.Properties | ForEach-Object {
+        Show-Value -Color:$Color -Name $_.Name -Value $_.Value -Redacted:($redactedConfigValues.Contains($_.Name))
+    }
+
+    if ($Markdown) {
+        Write-Output '```'
+        Write-Output "`n"
+    }
+}
+
+Show-Diag -Markdown -Color
 
 exit 0
