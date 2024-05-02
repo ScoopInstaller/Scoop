774e21e7b85c6a85d4530834214b5e07858adcd5
Capella87 <capella87@outlook.com>
2024-05-02 21:37:24 +0900

    fix(system): `$dir` parameter passing to `Split-PathLikeEnvVar` at `strip_path` with `-Pattern` switch

--- lib/system.ps1
+++ lib/system.ps1
@@ -162,7 +162,10 @@ function env($name, $global, $val) {
 
 function strip_path($orig_path, $dir) {
     Show-DeprecatedWarning $MyInvocation 'Split-PathLikeEnvVar'
-    Split-PathLikeEnvVar -Name $dir -Path $orig_path
+    if ($dir -is [string]) {
+        $dir = @($dir)
+    }
+    Split-PathLikeEnvVar -Pattern $dir -Path $orig_path
 }
 
 function add_first_in_path($dir, $global) {
