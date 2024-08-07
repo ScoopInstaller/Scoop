041ecba1bf11690456302182f55596a7c24dc01d
Olav Rønnestad Birkeland <6450056+o-l-a-v@users.noreply.github.com>
2024-08-07 18:02:49 +0200

    Correct typo

--- lib/decompress.ps1
+++ lib/decompress.ps1
@@ -134,7 +134,7 @@ function Expand-7zipArchive {
     }
     if ($Removal) {
         if (($Path -replace '.*\.([^\.]*)$', '$1') -eq '001') {
-            # Remove splited 7-zip archive parts
+            # Remove splitted 7-zip archive parts
             Get-ChildItem "$($Path -replace '\.[^\.]*$', '').???" | Remove-Item -Force
         } elseif (($Path -replace '.*\.part(\d+)\.rar$', '$1')[-1] -eq '1') {
             # Remove splitted RAR archive parts
