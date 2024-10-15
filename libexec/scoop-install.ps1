3f5133d3c5dabeaba2b7eb92bca8fb1df104276e
Nathan Baulch <nathan.baulch@gmail.com>
2024-10-15 14:15:27 +1100

    Fix typos

--- libexec/scoop-install.ps1
+++ libexec/scoop-install.ps1
@@ -122,7 +122,7 @@ if ($resolved.Count -lt $specific_versions.Count) {
 }
 $apps = @((@($specific_versions_paths) + $difference) | Where-Object { $_ } | Select-Object -Unique)
 
-# remember which were explictly requested so that we can
+# remember which were explicitly requested so that we can
 # differentiate after dependencies are added
 $explicit_apps = $apps
 
