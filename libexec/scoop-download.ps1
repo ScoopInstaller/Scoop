5faf77364981c153c0a424b00d31d91dd025e53e
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2025-06-11 16:57:45 +0800

    fix nightly_version invalid error

--- libexec/scoop-download.ps1
+++ libexec/scoop-download.ps1
@@ -24,6 +24,7 @@
 . "$PSScriptRoot\..\lib\autoupdate.ps1" # 'generate_user_manifest' (indirectly)
 . "$PSScriptRoot\..\lib\manifest.ps1" # 'generate_user_manifest' 'Get-Manifest'
 . "$PSScriptRoot\..\lib\download.ps1"
+. "$PSScriptRoot\..\lib\install.ps1" # 'nightly_version'
 if (get_config USE_SQLITE_CACHE) {
     . "$PSScriptRoot\..\lib\database.ps1"
 }
