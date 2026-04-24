b9b130986897308ca65425813812f02775afae62
Nacai <111849193+B67687@users.noreply.github.com>
2026-04-24 16:16:48 +0800

    fix(config): correct scoop_repo help URL

--- libexec/scoop-config.ps1
+++ libexec/scoop-config.ps1
@@ -33,7 +33,7 @@
 # no_junction: $true|$false
 #       The 'current' version alias will not be used. Shims and shortcuts will point to specific version instead.
 #
-# scoop_repo: http://github.com/ScoopInstaller/Scoop
+# scoop_repo: https://github.com/ScoopInstaller/Scoop
 #       Git repository containining scoop source code.
 #       This configuration is useful for custom forks.
 #
