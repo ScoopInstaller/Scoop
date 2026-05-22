645b6c32aa5405a744766f51c43da6dd3913ad3c
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2026-05-22 16:55:49 +0800

    Correct download results

--- libexec/scoop-download.ps1
+++ libexec/scoop-download.ps1
@@ -134,6 +134,7 @@ foreach ($curr_app in $apps) {
                     }
                     if ($err.Contains('No-hash apps')) {
                         error "If you want to allow no-hash apps, run 'scoop config allow_no_hash true', or use the parameter '--skip-hash-check' in a single download."
+                        $dl_failure = $true
                         continue
                     }
                     error (new_issue_msg $app $bucket "hash check failed")
