916f94f82ab4f40dcb98a6a1d81b98909bfa4e33
Hsiao-nan Cheung <niheaven@gmail.com>
2026-09-14 02:28:28 +0800

    fix(scoop-update): align status pin label with re-resolution

--- libexec/scoop-update.ps1
+++ libexec/scoop-update.ps1
@@ -443,7 +443,7 @@ if (-not ($apps -or $all)) {
             # version is resolved (and warned) by update(), not guessed here
             $ver = Select-CurrentVersion -AppName $app -Global:$global
             $pin = install_info $app $ver $global
-            $is_pin = $force -and !$pin.bucket -and $pin.url
+            $is_pin = $force -and !$pin.bucket -and ($pin.url -eq (usermanifest $app))
             if ($status.installed -and ($force -or $status.outdated)) {
                 if (!$status.hold) {
                     $outdated += applist $app $global
